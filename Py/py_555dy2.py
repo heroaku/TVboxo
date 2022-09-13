9944b3c0deff490d6095a16fb031f64cd9115cff
heroaku <108630267+heroaku@users.noreply.github.com>
2022-09-13 17:11:40 -0600

    Update py_555dy2.py

--- Py/py_555dy2.py
+++ Py/py_555dy2.py
@@ -9,7 +9,7 @@ import base64
 
 class Spider(Spider):  # 元类 默认的元类 type
 	def getName(self):
-		return "Voflix"
+		return "555电影"
 	def init(self,extend=""):
 		print("============{0}============".format(extend))
 		pass
@@ -37,16 +37,16 @@ class Spider(Spider):  # 元类 默认的元类 type
 			result['filters'] = self.config['filter']
 		return result
 	def homeVideoContent(self):
-		rsp = self.fetch("https://www.voflix.com/",headers=self.header)
+		rsp = self.fetch("https://555dy1.com/",headers=self.header)
 		root = self.html(rsp.text)
-		vodList = root.xpath("//div[@class='module']/div[contains(@class,'tab-list')]//a")
+		vodList = root.xpath("//div[@class='module-items module-poster-items-base ']/a")
 		videos = []
 		for vod in vodList:
 			name = vod.xpath("./@title")[0]
-			pic = vod.xpath(".//img/@data-original")[0]
+			pic = vod.xpath(".//div[@class='module-item-pic']/img/@data-original")[0]
 			mark = vod.xpath(".//div[@class='module-item-note']/text()")[0]
 			sid = vod.xpath("./@href")[0]
-			sid = self.regStr(sid,"/detail/(\\S+).html")
+			sid = self.regStr(sid,"/voddetail/(\\S+).html")
 			videos.append({
 				"vod_id":sid,
 				"vod_name":name,
