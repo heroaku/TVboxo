0efe8a4101c66f7841479269d3fa09ea046844a9
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-10 16:02:34 -0600

    Update jstest.js

--- Js/jstest.js
+++ Js/jstest.js
@@ -1,6 +1,6 @@
 muban.首图2.二级.tabs = '.nav-tabs.dpplay&&li';
 var rule = Object.assign(muban.首图2,{
-    title:'007影视',
+    title:'欧乐',
     host:'https://olevod.tv/index.php',
     url:'/vod/show/id/fyclass/page/fypage.html',
     searchUrl:'vod/search/wd/**.html',
