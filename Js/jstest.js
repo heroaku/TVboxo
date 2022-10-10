1da294d5a5a9b79cbf5c3b4cc084a19ed92212cb
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-10 17:37:45 -0600

    Update jstest.js

--- Js/jstest.js
+++ Js/jstest.js
@@ -1,6 +1,6 @@
 var rule = Object.assign(muban.mxpro,{
 title:'段友影视',
-host:'https://shangjihuoke.com/',
+host:'https://shangjihuoke.com',
 url:'/index.php/vod/show/id/fyclass/page/fypage.html',
 searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
 class_parse:'.navbar-items li:gt(2):lt(8);a&&Text;a&&href;.*/(.*?).html',
