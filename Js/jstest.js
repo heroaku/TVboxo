f72e488f27ed1c1949594f3b9ea85776d6a2a450
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-12 17:46:54 -0600

    Update jstest.js

--- Js/jstest.js
+++ Js/jstest.js
@@ -1,7 +1,10 @@
-muban.首图2.二级.tabs = '.nav-tabs.dpplay&&li';
+muban.首图2.二级.tabs = '.stui-pannel__head.bottom-line.active.clearfix h3';
+muban.首图2.二级.content = '.stui-content__desc&&Text';
 var rule = Object.assign(muban.首图2,{
-    title:'007影视',
-    host:'https://www.007ts.me',
-    url:'/channel/fyclass-fypage.html',
-    searchUrl:'/search/**----------fypage---.html',
+title:'秋霞',
+host:'https://www.qiuxiayb.com',
+url:'/vodtype/fyclass-fypage/',
+class_parse:'.stui-header__menu li;a&&Text;a&&href;/type/(.*?)/',
+searchUrl:'/search/**----------fypage---.html',
+搜索:'ul.stui-vodlist&&li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
 });
