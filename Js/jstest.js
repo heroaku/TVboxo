33e31d5b9982e6461754107151833da72f615d78
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-12 17:50:15 -0600

    Update jstest.js

--- Js/jstest.js
+++ Js/jstest.js
@@ -3,7 +3,7 @@ muban.首图2.二级.content = '.stui-content__desc&&Text';
 var rule = Object.assign(muban.首图2,{
 title:'秋霞',
 host:'https://www.qiuxiayb.com',
-url:'/vodtype/fyclass-fypage/',
+url:'/vodtype/fyclass-fypage.html',
 class_parse:'.stui-header__menu li;a&&Text;a&&href;/type/(.*?)/',
 searchUrl:'/search/**----------fypage---.html',
 搜索:'ul.stui-vodlist&&li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
