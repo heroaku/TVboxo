6bc1e78f106ba0e3561414930c743202f3f657fb
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-12 18:17:24 -0600

    Update jstest.js

--- Js/jstest.js
+++ Js/jstest.js
@@ -1,10 +1,26 @@
 muban.首图2.二级.tabs = '.stui-pannel__head.bottom-line.active.clearfix h3';
 muban.首图2.二级.content = '.stui-content__desc&&Text';
 var rule = Object.assign(muban.首图2,{
-title:'秋霞',
-host:'https://www.qiuxiayb.com',
-url:'/vodtype/fyclass-fypage.html',
-class_parse:'.stui-header__menu li;a&&Text;a&&href;.*/(.*?).html',
-searchUrl:'/search/**----------fypage---.html',
-搜索:'ul.stui-vodlist&&li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
+    title:'凌云',
+    host:'http://www.lingyun.in',
+    // homeUrl:'/',
+    url:'/channel/fyclass-fypage.html',
+    searchUrl:'/search.html?wd=**',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent':'MOBILE_UA',
+        // "Cookie": "searchneed=ok"
+    },
+    class_parse:'.menu_bottom ul li;a&&Text;a&&href;.*/(.*?).html',
+    cate_exclude:'解析|动态',
+    play_parse:true,
+    lazy:'',
+    limit:6,
+    推荐:'.indexShowBox;ul&&li;a&&title;img&&data-src;.s1&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    一级:'.pic-list&&li;a&&title;img&&data-src;.s1&&Text;a&&href',
+    二级:{"title":"h1&&Text;.content-rt&&p:eq(0)&&Text","img":".img&&img&&data-src","desc":".content-rt&&p:eq(1)&&Text;.content-rt&&p:eq(2)&&Text;.content-rt&&p:eq(3)&&Text;.content-rt&&p:eq(4)&&Text;.content-rt&&p:eq(5)&&Text","content":".zkjj_a&&Text","tabs":".py-tabs&&option","lists":".player:eq(#id) li"},
+    搜索:'.sr_lists&&ul&&li;h3&&Text;img&&data-src;.int&&p:eq(0)&&Text;a&&href',
 });
