3d27ae81f8962ae0488fc9c0cc7d416ae36e0b2a
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-14 13:25:26 -0600

    Update jstest.js

--- Js/jstest.js
+++ Js/jstest.js
@@ -1,25 +1,26 @@
-var rule={
-    title:'ddys',
-    host:'https://ddys.tv',
-    // homeUrl:'/',
-    url:'https://ddys.tv/category/fyclass/page/fypage/',
-    searchUrl:'/search.html?wd=**',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-        'User-Agent':'MOBILE_UA',
-        // "Cookie": "searchneed=ok"
+//已知问题:
+// 相较于其他壳,pluto player 无法正常嗅探的"分类"或"影片"较多
+// 推荐页面: pluto正常, 其他壳异常
+// 无法搜索
+
+var rule = {
+    title:'短视频',
+    host:'http://www.sharenice.net',
+    url:'/fyclass?page=fypage',
+    //searchUrl:'/video/search?search_key=**',
+    searchable:0,
+    quickSearch:0,
+    headers:{
+        'User-Agent':'PC_UA'
     },
-    class_name:'电影&剧集&动画',
-    class_url:'movie&airing&anime',
-    cate_exclude:'解析|动态',
+    timeout:5000,
+    class_name:'抖音&快手&微视&火山&场库&体育&美拍&秒拍&全民&梨&好兔&美女&正能量&搞笑&社会&游戏&娱乐&旅游&萌系&生活&音乐&美食&明星&动漫&影视&时尚',
+    class_url:'douyin&kuaishou&weishi&huoshan&changku&tags/t-5L2T6IKy.html&meipai&miaopai&quanmin&lishipin&haotu&tags/t-576O5aWz.html&tags/t-5q2j6IO96YeP.html&tags/t-5pCe56yR.html&tags/t-56S+5Lya.html&tags/t-5ri45oiP.html&tags/t-5aix5LmQ.html&tags/t-5peF5ri4.html&tags/t-6JCM57O7.html&tags/t-55Sf5rS7.html&tags/t-6Z+z5LmQ.html&tags/t-576O6aOf.html&tags/t-5piO5pif.html&tags/t-5Yqo5ryr.html&tags/t-5b2x6KeG.html&tags/t-5pe25bCa.html',
     play_parse:true,
     lazy:'',
     limit:6,
-    //推荐:'.indexShowBox;ul&&li;a&&title;img&&data-src;.s1&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'.post-box-list&&article;a:eq(-1)&&Text;.post-box-image&&style;a:eq(0)&&Text;a:eq(-1)&&href',
-    二级:{"title":"h1&&Text;.content-rt&&p:eq(0)&&Text","img":".img&&img&&data-src","desc":".content-rt&&p:eq(1)&&Text;.content-rt&&p:eq(2)&&Text;.content-rt&&p:eq(3)&&Text;.content-rt&&p:eq(4)&&Text;.content-rt&&p:eq(5)&&Text","content":".zkjj_a&&Text","tabs":".py-tabs&&option","lists":".player:eq(#id) li"},
-    搜索:'.sr_lists&&ul&&li;h3&&Text;img&&data-src;.int&&p:eq(0)&&Text;a&&href',
+    double:false,
+    推荐:'*',
+    一级:'.item-box&&ul&&li;a&&title;img&&data-original;;a&&href',
+    二级:'*',
 }
