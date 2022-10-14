5544d3384b42c650bfe4a66e65043e5a2c1388e5
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-14 15:41:51 -0600

    Update MXONE.js

--- Js/MXONE.js
+++ Js/MXONE.js
@@ -1,8 +1,18 @@
-var rule = Object.assign(muban.mxpro,{
-	title:'MXONE',
-    host:'https://www.jpys.me',
-    url:'/vodshow/fyclass--------fypage---.html',
-    searchUrl:'/vodsearch/**----------fypage---.html',
-    class_parse:'.navbar-items li:gt(2):lt(8);a&&Text;a&&href;.*/(.*?).html',	
-//class_parse:'.navbar-items;a&&Text;a&&href;.*/(.*?).html',
-});
+var rule={
+title:'MXONE',
+host:'https://www.jpys.me',
+url:'/vodshow/fyclass--------fypage---.html',
+searchUrl:'/vodsearch/**----------fypage---.html',
+searchable:2,//是否启用全局搜索,
+quickSearch:0,//是否启用快速搜索,
+filterable:0,//是否启用分类筛选,
+class_parse:'.navbar-items li:gt(2):lt(8);a&&Text;a&&href;.*/(.*?).html',
+play_parse:true,
+lazy:'',
+limit:6,
+推荐:'.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+double:true, // 推荐内容是否双层定位
+一级:'.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+二级:{"title":"h1&&Text;.tag-link&&Text","img":".module-item-pic&&img&&data-src","desc":".video-info-items:eq(0)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(3)&&Text","content":".vod_content&&Text","tabs":".module-tab-item","lists":".module-player-list:eq(#id)&&.scroll-content&&a"},
+搜索:'.module-items .module-search-item;a&&title;img&&data-src;.video-serial&&Text;a&&href',
+},
