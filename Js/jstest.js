e088ed6a841688d7bb2248663ddeaf2dd7bf35c5
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-14 16:26:57 -0600

    Update jstest.js

--- Js/jstest.js
+++ Js/jstest.js
@@ -1,22 +1,23 @@
-
-var rule={
-    title:'RP影视',
-    host:'https://mac.rp123.top',
-    url:'/vodshow/fyclass--------fypage---/',
-    searchUrl:'/vodsearch/**----------fypage---/',
-    searchable:2,
-    quickSearch:0,
-    filterable:0,
-    headers:{'User-Agent':'MOBILE_UA',},
-    class_parse:'.navbar-items&&ul li:gt(0):lt(8);a&&Text;a&&href;/(\\d+).html',
-    //class_name:'电影&电视剧&综艺&动漫&纪录片&4K专区',
-    //class_url:'1&2&3&4&35&40',
- play_parse:true,
+var rule = {
+    title:'素白白',
+    host:'https://www.subaibaiys.com',
+    url:'/fyclass/page/fypage',
+    searchUrl:'/grabble?q=**',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{
+        'User-Agent':'UC_UA',
+        // "Cookie": ""
+    },
+    class_parse:'.navlist&&li;a&&Text;a&&href;com/(.*)',
+	cate_exclude:'公告留言',
+    play_parse:true,
     lazy:'',
     limit:6,
-    推荐:'.tab-list.active;a.module-poster-item.module-item;.module-poster-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+    推荐:'.mi_cont&&ul;li;h3&&Text;img&&data-original;.jidi&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:'.module-poster-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-    二级:{"title":"h1&&Text;.module-info-tag&&Text","img":".lazyload&&data-original","desc":".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text","content":".module-info-introduction&&Text","tabs":".module-tab-item.tab-item","lists":".module-play-list:eq(#id) a"},
-    搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
+    一级:'.mi_cont&&ul&&li;h3&&Text;img&&data-original;.jidi&&Text;a&&href',
+    二级:{"title":"h1&&Text;.moviedteail_list&&li&&Text","img":".dyimg&&img&&src","desc":".moviedteail_list&&li:eq(1)&&Text;.moviedteail_list&&li:eq(2)&&Text;.moviedteail_list&&li:eq(3)&&Text;.moviedteail_list&&li:eq(4)&&Text","content":".yp_context&&Text","tabs":".mi_paly_box .ypxingq_t","lists":".paly_list_btn:eq(#id) a"},
+    搜索:'.mi_cont&&ul&&li;h3&&Text;.lazy&&data-original;.jidi&&Text;a&&href',
 }
