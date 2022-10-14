b8b0d5ad9aac3a711c2f3f22dc64265c5158f315
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-14 15:13:16 -0600

    Update MXONE.js

--- Js/MXONE.js
+++ Js/MXONE.js
@@ -2,6 +2,6 @@ var rule = Object.assign(muban.mxpro,{
 	title:'MXONE',
     host:'https://www.jpys.me',
     url:'/vodshow/fyclass--------fypage---.html',
-    searchUrl:'/vodsearch/**----------fypage---.html/',
+    searchUrl:'/vodsearch/**----------fypage---.html',
 class_parse:'.navbar-items;a&&Text;a&&href;.*/(.*?).html',
 });
