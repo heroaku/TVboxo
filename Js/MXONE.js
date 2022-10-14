959d091d9782e695d50e740e7530b4d2fdef067b
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-14 16:11:27 -0600

    Update MXONE.js

--- Js/MXONE.js
+++ Js/MXONE.js
@@ -1,26 +1,26 @@
+//已知问题:
+// 相较于其他壳,pluto player 无法正常嗅探的"分类"或"影片"较多
+// 推荐页面: pluto正常, 其他壳异常
+// 无法搜索
+
 var rule = {
-    title:'麦豆',
-    host:'https://www.mdoutv.com',
-    // homeUrl:'/',
-    url:'/movie_bt_series/fyclass/page/fypage',
-    searchUrl:'/search/**/page/fypage',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-        'User-Agent':'MOBILE_UA',
-        // "Cookie": "searchneed=ok"
+    title:'短视频',
+    host:'http://www.sharenice.net',
+    url:'/fyclass?page=fypage',
+    //searchUrl:'/video/search?search_key=**',
+    searchable:0,
+    quickSearch:0,
+    headers:{
+        'User-Agent':'PC_UA'
     },
-    //class_parse:'.follow-align&&ul&&li;a&&Text;a&&href;movie_bt_series/(.*?)',
-	class_name:'国产&港台&欧美&韩剧&日剧&泰剧&剧集&电影&动漫&综艺',
-	class_url:'guocanju&gangtai&en&hanju&riju&taiju&tv&movie&ac&zongyi',
-	cate_exclude:'留言|幸运码|更多播放线路|蚂蚁导航|迷历史',
+    timeout:5000,
+    class_name:'抖音&快手&微视&火山&场库&体育&美拍&秒拍&全民&梨&好兔&美女&正能量&搞笑&社会&游戏&娱乐&旅游&萌系&生活&音乐&美食&明星&动漫&影视&时尚',
+    class_url:'douyin&kuaishou&weishi&huoshan&changku&tags/t-5L2T6IKy.html&meipai&miaopai&quanmin&lishipin&haotu&tags/t-576O5aWz.html&tags/t-5q2j6IO96YeP.html&tags/t-5pCe56yR.html&tags/t-56S+5Lya.html&tags/t-5ri45oiP.html&tags/t-5aix5LmQ.html&tags/t-5peF5ri4.html&tags/t-6JCM57O7.html&tags/t-55Sf5rS7.html&tags/t-6Z+z5LmQ.html&tags/t-576O6aOf.html&tags/t-5piO5pif.html&tags/t-5Yqo5ryr.html&tags/t-5b2x6KeG.html&tags/t-5pe25bCa.html',
     play_parse:true,
     lazy:'',
     limit:6,
-    推荐:'body&&.mi_btcon;ul&&li;img&&alt;img&&data-original;.jidi&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'.mrb&&ul&&li;img&&alt;img&&data-original;.jidi&&Text;a&&href',
-    二级:{"title":"h1&&Text;.moviedteail_list&&li:eq(2)&&Text;","img":".dyxingq&&img&&src","desc":".moviedteail_list&&li:eq(3)&&Text;.moviedteail_list&&li:eq(4)&&Text","content":".yp_context&&Text","tabs":".fr&&a","lists":".paly_list_btn:eq(#id) a"},
-    搜索:'.search_list&&ul&&li;h3&&Text;img&&data-original;.hdinfo&&span&&Text;a&&href',
+    double:false,
+    推荐:'*',
+    一级:'.item-box&&ul&&li;a&&title;img&&data-original;;a&&href',
+    二级:'*',
 }
