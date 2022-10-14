3504d5af97c12169b3b6c6b00f56bc9077d78a01
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-14 13:03:00 -0600

    Update 皮皮泡.js

--- Js/皮皮泡.js
+++ Js/皮皮泡.js
@@ -1,11 +1,18 @@
-muban.首图2.二级.tabs = '.stui-pannel__head.bottom-line.active.clearfix h3';
-var rule = Object.assign(muban.首图2,{
-title:'皮皮泡',
-host:'https://www.pipipao.com',
-url:'/vodshow/id/fyclass/page/fypage.html',
-searchUrl:'/vodsearch**/page/fypage.html',
-class_name:'电视剧&电影&综艺&动漫&纪录片',//静态分类名称拼接
-class_url:'dianshiju&dianying&zongyi&dongman&jilupian',//静态分类标识拼接
-class_parse:' ',
-搜索:'ul.stui-vodlist__media&&li;a&&title;.lazyload&&data-original;.text-muted&&Text;a&&href;.text-muted:eq(-1)&&Text',
-});
+var rule={
+    title:'皮皮泡',
+    host:'https://www.pipipao.com',
+    url:'/vodshow/id/fyclass/page/fypage.html',
+    searchUrl:'/vodsearch.html?wd=**',
+    searchable:2,
+    quickSearch:0,
+    filterable:0,
+    headers:{'User-Agent':'MOBILE_UA',},
+    class_name:'电视剧&电影&综艺&动漫&纪录片',
+    class_url:'dianshiju&dianying&zongyi&dongman&jilupian',
+    class_parse:'stui-header__menu&&li;a&&Text;a&&href;.*/(.*?).html',
+    play_parse:true,
+    推荐:'.stui-vodlist__box;a&&title;a&&data-original;.pic-text&&Text;a&&href',
+    一级:'.stui-vodlist__box;a&&title;a&&data-original;.pic-text&&Text;a&&href',
+    二级:{"title":".stui-content__thumb&&a&&title;.stui-content__detail&&p:eq(0)&&Text","img":"img.lazyload&&data-original","desc":"","content":".desc.hidden-xs&&Text","tabs":"h3.title","lists":".playlist:eq(#id) li"},
+       搜索:'li.clearfix&&div;h3&&Text;.lazyload&&data-original;.pic-text&&Text;a.btn-default&&href',
+}
