bea4b6ebabd4abf4c9250e2cdc28145ab8d3e443
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-14 13:27:20 -0600

    Update jstest.js

--- Js/jstest.js
+++ Js/jstest.js
@@ -1,26 +1,21 @@
-//已知问题:
-// 相较于其他壳,pluto player 无法正常嗅探的"分类"或"影片"较多
-// 推荐页面: pluto正常, 其他壳异常
-// 无法搜索
 
-var rule = {
-    title:'短视频',
-    host:'http://www.sharenice.net',
-    url:'/fyclass?page=fypage',
-    //searchUrl:'/video/search?search_key=**',
-    searchable:0,
+var rule={
+    title:'RP影视',
+    host:'https://mac.rp123.top',
+    url:'/vodshow/fyclass--------fypage---/',
+    searchUrl:'/vodsearch/**----------fypage---/',
+    searchable:2,
     quickSearch:0,
-    headers:{
-        'User-Agent':'PC_UA'
-    },
-    timeout:5000,
-    class_name:'抖音&快手&微视&火山&场库&体育&美拍&秒拍&全民&梨&好兔&美女&正能量&搞笑&社会&游戏&娱乐&旅游&萌系&生活&音乐&美食&明星&动漫&影视&时尚',
-    class_url:'douyin&kuaishou&weishi&huoshan&changku&tags/t-5L2T6IKy.html&meipai&miaopai&quanmin&lishipin&haotu&tags/t-576O5aWz.html&tags/t-5q2j6IO96YeP.html&tags/t-5pCe56yR.html&tags/t-56S+5Lya.html&tags/t-5ri45oiP.html&tags/t-5aix5LmQ.html&tags/t-5peF5ri4.html&tags/t-6JCM57O7.html&tags/t-55Sf5rS7.html&tags/t-6Z+z5LmQ.html&tags/t-576O6aOf.html&tags/t-5piO5pif.html&tags/t-5Yqo5ryr.html&tags/t-5b2x6KeG.html&tags/t-5pe25bCa.html',
-    play_parse:true,
+    filterable:0,
+    headers:{'User-Agent':'MOBILE_UA',},
+    class_name:'电影&电视剧&综艺&动漫&纪录片&4K专区',
+    class_url:'1&2&3&4&35&40',
+ play_parse:true,
     lazy:'',
     limit:6,
-    double:false,
-    推荐:'*',
-    一级:'.item-box&&ul&&li;a&&title;img&&data-original;;a&&href',
-    二级:'*',
+    推荐:'.tab-list.active;a.module-poster-item.module-item;.module-poster-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    一级:'body a.module-poster-item.module-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+    二级:{"title":"h1&&Text;.module-info-tag&&Text","img":".lazyload&&data-original","desc":".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text","content":".module-info-introduction&&Text","tabs":".module-tab-item.tab-item","lists":".module-play-list:eq(#id) a"},
+    搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
 }
