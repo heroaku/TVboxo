539afd2123817bdb309928a7a913d0cd782e0718
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-15 13:04:00 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,7 +1,7 @@
 var rule={
     title:'欧乐影院',
     host:'https://www.olevod.com',
-    url:'/index.php/vod/type/id/fyclass/page/fypage.html',
+    url:'/index.php/vod/show/id/fyclass/page/fypage.html',
     searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
     searchable:2,//是否启用全局搜索,
     quickSearch:0,//是否启用快速搜索,
