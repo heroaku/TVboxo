0a304d70bc9645cf4ab1bdd30e99862fb39a0752
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-16 12:40:00 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -11,9 +11,9 @@ var rule = {
         'User-Agent':'MOBILE_UA',
         // "Cookie": "searchneed=ok"
     },
-    //class_name:'电影&电视剧&动漫&综艺&记录片',
-    //class_url:'dianying&dianshiju&dongman&zongyi',
-    class_parse:'.myui-panel-box&&ul&&li;a&&Text;a&&href;/v/(.*)/',
+    class_name:'电影&电视剧&综艺&动漫&记录片',
+    class_url:'20&21&22&23&32',
+    //class_parse:'.myui-panel-box&&ul&&li;a&&Text;a&&href;/v/(.*)/',
     play_parse:true,
     lazy:'',
     limit:6,
