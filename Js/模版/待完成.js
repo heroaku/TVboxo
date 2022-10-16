35b1874fe655b2f9b236ca3bfeab536e019fbf82
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-15 19:05:41 -0600

    Update 待完成.js

--- Js/模版/待完成.js
+++ Js/模版/待完成.js
@@ -23,6 +23,34 @@ var rule={
 }
 
 
+
+var rule={
+    title:'小宝影视',
+    host:'https://xiaobao.tv',
+    url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+    searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{
+        'User-Agent':'UC_UA',
+    },
+    // class_parse:'.fed-pops-navbar&&ul.fed-part-rows&&a.fed-part-eone:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
+    //class_parse:'.top_nav&&ul li:gt(0):lt(25);a&&Text;a&&href;/(\\d+).html',
+    class_name:'电影&电视剧&综艺&动漫&记录片',
+    class_url:'1&2&3&4&24',
+    play_parse:true,
+    lazy:'',
+    limit:6,
+    推荐:'ul.vodlist.vodlist_wi;li;a&&title;a&&data-original;.pic_text.text_right&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    一级:'li.vodlist_item;a&&title;a&&data-original;.pic_text.text_right&&Text;a&&href',
+    二级:{"title":"h2&&Text;.content_detail.content_min.fl .data_style&&Text","img":".content_thumb .vodlist_thumb&&data-original","desc":".content_detail.content_min.fl li:eq(0)&&Text;.content_detail.content_min.fl li:eq(2)&&Text;.content_detail.content_min.fl li:eq(3)&&Text","content":".content&&Text","tabs":".play_source_tab:eq(0) a","lists":".content_playlist:eq(#id) li"},
+    搜索:'body .searchlist_item;a&&title;.vodlist_thumb&&data-original;.text-muted&&Text;a&&href;.vodlist_sub&&Text',
+}
+
+
+
 //未完成
 
 var rule={
@@ -54,25 +82,3 @@ var rule={
 
 
 
-var rule={
-    title:'小宝影视',
-    host:'https://xiaobao.tv',
-    url:'/index.php/vod/show/id/fyclass/page/fypage.html',
-    searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    headers:{
-        'User-Agent':'UC_UA',
-    },
-    // class_parse:'.fed-pops-navbar&&ul.fed-part-rows&&a.fed-part-eone:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
-    class_parse:'.top_nav&&ul li:gt(0):lt(25);a&&Text;a&&href;.*/(.*?).html',
-    play_parse:true,
-    lazy:'',
-    limit:6,
-    推荐:'ul.myui-vodlist.clearfix;li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'.vodlist&&ul li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-    二级:{"title":".myui-content__detail .title&&Text;.myui-content__detail p:eq(-2)&&Text","img":".myui-content__thumb .lazyload&&data-original","desc":".myui-content__detail p:eq(0)&&Text;.myui-content__detail p:eq(1)&&Text;.myui-content__detail p:eq(2)&&Text","content":".content&&Text","tabs":".nav-tabs:eq(0) li","lists":".myui-content__list:eq(#id) li"},
-    搜索:'#searchList li;a&&title;.lazyload&&data-original;.text-muted&&Text;a&&href;.text-muted:eq(-1)&&Text',
-}
