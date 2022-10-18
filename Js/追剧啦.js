130315c8e7e9557d6281de9e8ba420b073d917ed
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-18 10:14:57 -0600

    Update 追剧啦.js

--- Js/追剧啦.js
+++ Js/追剧啦.js
@@ -16,7 +16,8 @@ var rule = {
     play_parse:true,
     lazy:'',
     limit:6,
-    推荐:'.module-items;.module-item-cover&&.module-item-pic;a&&title;.lazyloaded&&data-src;.item-text&&Text;a&&href',
+    //推荐:'.module-items;.module-item-cover&&.module-item-pic;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
+    推荐:'.module-items;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.module-list&&.module-item-pic;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
     二级:{"title":"h1.page-title&&Text;.video-info-aux&&Text","img":".lazyload&&data-src","content":".content.vod_content&&Text","tabs":".module-tab-item.tab-item","lists":".module-blocklist.scroll-box:eq(#id) a"},
