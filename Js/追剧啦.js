223e6d17a489d250a17f7d3318a4aa32fbe06059
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-18 11:43:53 -0600

    Update 追剧啦.js

--- Js/追剧啦.js
+++ Js/追剧啦.js
@@ -16,11 +16,11 @@ var rule = {
     play_parse:true,
     lazy:'',
     limit:6,
-    推荐:'.module-item;.module-item-cover&&.module-item-pic;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
-    //推荐:'.module-items;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
+    //推荐:'.module-item;.module-item-cover&&.module-item-pic;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
+    推荐:'.module-item;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     //一级:'.module-list&&.module-item-pic;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
     一级:'.module-item;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
     二级:{"title":"h1.page-title&&Text;.video-info-aux&&Text","img":".lazyload&&data-src","desc":".video-info-items:eq(0)&&Text;.video-info-items:eq(3)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(1)&&Text","content":".video-info-items:eq(5)&&Text","tabs":".module-tab-item.tab-item","lists":".module-blocklist.scroll-box:eq(#id) a"},
-    搜索:'.module-items .module-search-item;h3&&Text;img&&data-src;.video-serial&&Text;a&&href',
+    搜索:'.module-search-item;h3&&Text;img&&data-src;.module-item-text&&Text;a&&href',
  }
