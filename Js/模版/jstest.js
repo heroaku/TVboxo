460faecb7e3f8169c6d5881e73e71a9991d5f28c
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-17 18:04:13 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -18,7 +18,7 @@ var rule = {
     play_parse:true,
     lazy:'',
     limit:6,
-    推荐:'.module-item;.module-item-cover&&.module-item-pic&&a&&title;.module-item-cover&&.module-item-pic&&.lazyloaded&&data-src;.module-item-text&&Text;.module-item-cover&&.module-item-pic&&a&&href',
+    推荐:'.module-item;.module-item-cover&&.module-item-pic&&a&&title;.lazyloaded&&data-src;.module-item-text&&Text;.module-item-cover&&.module-item-pic&&a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.module-list&&.module-item;.module-item-pic&&a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
     二级:{"title":"h1.page-title&&Text;.video-info-aux&&Text","img":".lazyload&&data-src","content":".content.vod_content&&Text","tabs":".module-tab-item.tab-item","lists":".module-blocklist.scroll-box:eq(#id) a"},
