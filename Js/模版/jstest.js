169c49af1fed88af3d44ef6e917e05739768ed3e
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-19 12:46:32 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -16,7 +16,7 @@ var rule = {
     play_parse:true,
     lazy:'',
     limit:6,
-    推荐:'.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    推荐:'.module-items.module-poster-items-base&&.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
     二级:{"title":"h1&&Text;.module-info-tag-link:eq(2)&&Text","img":".ls-is-cached.lazy.lazyload&&data-original","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(4)&&Text;.module-info-content&&.module-info-item:eq(3)&&Text;.module-info-content&&.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-content&&.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction-content&&Text","tabs":".module-tab-items-box:eq(0)&&.module-tab-item","lists":".module-play-list-content:eq(0) a"},
