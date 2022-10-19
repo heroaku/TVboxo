cd3430e87de96d7620b6b4c76666a903f977b01c
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-19 16:00:58 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,9 +1,9 @@
 var rule = {
-    title:'胖子影视',
-    host:'https://www.pangzitv.com',
+    title:'蛋蛋魔法',
+    host:'https://ddmf.net',
     // homeUrl:'/',
-    url:'/vod-list-id-fyclass-pg-fypage-order--by--class-0-year-0-letter--area--lang-.html',
-    searchUrl:'/vod-search-pg-fypage-wd-**.html',
+    url:'/vodshow/fyclass--------fypage---.html',
+    searchUrl:'/vodsearch/**----------fypage---.html',
     searchable:2,//是否启用全局搜索,
     quickSearch:0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
@@ -11,14 +11,15 @@ var rule = {
         'User-Agent':'MOBILE_UA',
         // "Cookie": "searchneed=ok"
     },
-    class_name:'电影&电视剧&综艺&动漫',
-    class_url:'1&2&3&4',
+    class_name:'电影&电视剧&综艺&动漫&纪录片',
+    class_url:'1&2&3&4&22',
     play_parse:true,
     lazy:'',
     limit:6,
-    推荐:'.mainwidth&&.imglist02.cl&&.imgItemWrp&&.imgItem;img&&alt;img&&src;.updateEpisode.pa&&Text;a&&href',
+    推荐:'.module-item;.module-item-cover&&.module-item-pic;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:'.imgItem;img&&alt;img&&src;.updateEpisode.pa&&Text;a&&href',
-    二级:{"title":".v-title.clearfix&&Text;.item.item-line:eq(0)&&Text","img":"img&&src","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.item-lines.clearfix&&Text;.item.starring&&Text;.module-info-content&&.module-info-item:eq(-1)&&.module-info-item-content&&Text","content":".intro&&.text&&Text","tabs":".supply.clearfix&&li","lists":".vlink_1&&li"},
-    搜索:'.module-card-item.module-item;.module-card-item-title&&Text;img&&data-original;.module-item-note&&Text;a.play-btn-o&&href',
+    一级:'.module-list&&.module-item;.module-item-pic&&a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
+    二级:{"title":"h1&&Text;.video-info-aux.scroll-content&&Text","img":".lazyload&&data-src","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.video-info-items:eq(1)&&.video-info-item.video-info-actor&&Text","content":".video-info-item.video-info-content.vod_content&&Text","tabs":".module-tab-content&&.module-tab-item.tab-item","lists":".module-blocklist.scroll-box.scroll-box-y:eq(0)&&.scroll-content a"},
+    搜索:'.module-search-item;.lazy.lazyload&&alt;img&&data-src;.video-serial&&Text;a.video-serial&&href',
 }
+
