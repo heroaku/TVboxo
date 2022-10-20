58267fb941d27b114ad0a1b1cbcd0fe675fed4e6
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-20 07:57:41 -0600

    Update drpy.js

--- libs/drpy.js
+++ libs/drpy.js
@@ -76,6 +76,7 @@ var log;
 var rule_fetch_params;
 var fetch_params; // 每个位置单独的
 var oheaders;
+// var play_url; // 二级详情页注入变量,为了适配js模式0 (不在这里定义了,直接二级里定义了个空字符串)
 var _pdfh;
 var _pdfa;
 var _pd;
@@ -706,7 +707,10 @@ function readFile(filePath){
 function dealJson(html) {
     try {
         // html = html.match(/[\w|\W|\s|\S]*?(\{[\w|\W|\s|\S]*\})/).group[1];
-        html = '{'+html.match(/.*?\{(.*)\}/)[1]+'}';
+        html = html.trim();
+        if(!((html.startsWith('{') && html.endsWith('}'))||(html.startsWith('[') && html.endsWith(']')))){
+            html = '{'+html.match(/.*?\{(.*)\}/m)[1]+'}';
+        }
     } catch (e) {
     }
     try {
@@ -898,13 +902,16 @@ function request(url,obj,ocr_flag){
             obj.headers["Content-Type"] = 'text/html; charset='+rule.encoding;
         }
     }
-    if(typeof(obj.headers.body)!='undefined'&&obj.headers.body&&typeof (obj.headers.body)==='string'){
+    if(typeof(obj.body)!='undefined'&&obj.body&&typeof (obj.body)==='string'){
         let data = {};
-        obj.headers.body.split('&').forEach(it=>{
+        obj.body.split('&').forEach(it=>{
             data[it.split('=')[0]] = it.split('=')[1]
         });
         obj.data = data;
-        delete obj.headers.body
+        delete obj.body
+    }else if(typeof(obj.body)!='undefined'&&obj.body&&typeof (obj.body)==='object'){
+        obj.data = obj.body;
+        delete obj.body
     }
     if(!url){
         return obj.withHeaders?'{}':''
@@ -926,6 +933,17 @@ function request(url,obj,ocr_flag){
     }else{
         return html
     }
+}
+
+/**
+ *  快捷post请求
+ * @param url 地址
+ * @param obj 对象
+ * @returns {string|DocumentFragment|*}
+ */
+function post(url,obj){
+    obj.method = 'POST';
+    return request(url,obj);
 }
 
 fetch = request;
@@ -1410,7 +1428,7 @@ function searchParse(searchObj) {
         _pd = _ps.pd;
         let is_json = p0.startsWith('json:');
         p0 = p0.replace(/^(jsp:|json:|jq:)/,'');
-        // print('1381 p0:'+p0);
+        print('1381 p0:'+p0);
         try {
             let html = getHtml(MY_URL);
             if (html) {
@@ -1430,9 +1448,14 @@ function searchParse(searchObj) {
                     console.log(html);
                 }
                 if(is_json){
+                    // console.log(html);
                     html = dealJson(html);
                 }
+                console.log(JSON.stringify(html));
+                console.log(html);
                 let list = _pdfa(html, p0);
+                print(list.length);
+                print(list);
                 list.forEach(it => {
                     let p1 = getPP(p, 1, pp, 1);
                     let p2 = getPP(p, 2, pp, 2);
@@ -1511,6 +1534,7 @@ function detailParse(detailObj){
     }else if(typeof(p)==='string'&&p.trim().startsWith('js:')){
         const TYPE = 'detail';
         var input = MY_URL;
+        var play_url = '';
         eval(p.trim().replace('js:',''));
         vod = VOD;
         console.log(JSON.stringify(vod));
@@ -1596,12 +1620,13 @@ function detailParse(detailObj){
                 playFrom = TABS;
             }else{
                 let p_tab = p.tabs.split(';')[0];
-                console.log(p_tab);
+                // console.log(p_tab);
                 let vHeader = _pdfa(html, p_tab);
-
                 console.log(vHeader.length);
+                let tab_text = p.tab_text||'body&&Text';
+                // print('tab_text:'+tab_text);
                 for(let v of vHeader){
-                    let v_title = _pdfh(v,'body&&Text').trim();
+                    let v_title = _pdfh(v,tab_text).trim();
                     console.log(v_title);
                     if(tab_exclude&& (new RegExp(tab_exclude)).test(v_title)){
                         continue;
@@ -1628,9 +1653,15 @@ function detailParse(detailObj){
                 eval(p.lists.replace('js:',''));
                 vod_play_url = LISTS.map(it=>it.join('#')).join(vod_play_url);
             }else{
+                let list_text = p.list_text||'body&&Text';
+                let list_url = p.list_url||'a&&href';
+                // print('list_text:'+list_text);
+                // print('list_url:'+list_url);
+                // print('list_parse:'+p.lists);
+                let is_tab_js = p.tabs.trim().startsWith('js:');
                 for(let i=0;i<playFrom.length;i++){
                     let tab_name = playFrom[i];
-                    let tab_ext =  p.tabs.split(';').length > 1 ? p.tabs.split(';')[1] : '';
+                    let tab_ext =  p.tabs.split(';').length > 1 && !is_tab_js ? p.tabs.split(';')[1] : '';
                     let p1 = p.lists.replaceAll('#idv', tab_name).replaceAll('#id', i);
                     tab_ext = tab_ext.replaceAll('#idv', tab_name).replaceAll('#id', i);
                     console.log(p1);
@@ -1643,6 +1674,7 @@ function detailParse(detailObj){
                         // console.log(e.message);
                     }
                     let new_vod_list = [];
+                    // print('tab_ext:'+tab_ext);
                     let tabName = tab_ext?_pdfh(html, tab_ext):tab_name;
                     console.log(tabName);
                     // console.log('cheerio解析Text');
@@ -1650,7 +1682,8 @@ function detailParse(detailObj){
                         // 请注意,这里要固定pdfh解析body&&Text,不需要下划线,没写错
                         // new_vod_list.push(pdfh(it,'body&&Text')+'$'+_pd(it,'a&&href',MY_URL));
                         // new_vod_list.push(cheerio.load(it).text()+'$'+_pd(it,'a&&href',MY_URL));
-                        new_vod_list.push(_pdfh(it, 'body&&Text').trim() + '$' + _pd(it, 'a&&href', MY_URL));
+                        // new_vod_list.push(_pdfh(it, list_text).trim() + '$' + _pd(it, list_url, MY_URL));
+                        new_vod_list.push(_pdfh(it, list_text).trim() + '$' + _pd(it, list_url, MY_URL));
                     });
                     let vlist = new_vod_list.join('#');
                     vod_tab_list.push(vlist);
@@ -1659,6 +1692,9 @@ function detailParse(detailObj){
             }
         }
         vod.vod_play_url = vod_play_url;
+    }
+    if(!vod.vod_id){
+        vod.vod_id = detailObj.orId;
     }
     // print(vod);
     return JSON.stringify({
