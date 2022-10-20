e9fe91bea455b5b10d2aaa22e44996fa07b57b6c
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-20 11:39:09 -0600

    Update 待完成模版.js

--- Js/模版/待完成模版.js
+++ Js/模版/待完成模版.js
@@ -213,6 +213,30 @@ var rule={
 }
 //需三级
 
-
+var rule={
+    title:'188影视',
+    host:'https://188kan.com',
+    // homeUrl:'/',
+    url:'/vodshow/fyclass--------fypage---.html',
+    searchUrl:'/vod-search-page-fypage-wd-**.html',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent':'MOBILE_UA',
+        // "Cookie": "searchneed=ok"
+    },
+    //class_parse:'.nav&&ul&&li;a&&title;a&&href;.*/(.*?).html',
+    class_name:'电影&电视剧&综艺&动漫',
+    class_url:'1&2&3&4',
+    play_parse:true,
+    lazy:'',
+    limit:6,
+    推荐:'.module-main.tab-list.active&&.module-items.module-poster-items-base&&a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+double:true, // 推荐内容是否双层定位
+一级:'.module&&a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+二级:{"title":"h1&&Text;.tag-link&&Text","img":".module-item-pic&&img&&data-src","desc":".video-info-items:eq(0)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(3)&&Text","content":".vod_content&&Text","tabs":".module-tab-item","lists":".module-player-list:eq(#id)&&.scroll-content&&a"},
+搜索:'.module-items .module-search-item;a&&title;img&&data-src;.video-serial&&Text;a&&href',
+}
 
 
