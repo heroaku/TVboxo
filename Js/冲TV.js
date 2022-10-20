0c30dfaa5c6c5686e67756bd1f91f1013bef9654
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-20 16:38:16 -0600

    Update 冲TV.js

--- Js/冲TV.js
+++ Js/冲TV.js
@@ -11,7 +11,7 @@ var rule={
 		class_url:'1&2&3&4',
 		推荐:'.list-a.size;a&&title;.lazy&&data-original;.list-remarks&&Text;a&&href',
 		一级:'.list-a.size&&ul li;a&&title;.lazy&&data-original;.list-remarks&&Text;a&&href',
-		二级:{"title":"h2&&Text;.data:eq(0) a:eq(0)&&Text","img":".lazyload&&data-original","desc":".data:eq(-1) a:eq(2)&&Text;.data:eq(-1) a:eq(1)&&Text;.play-top p:eq(0)&&Text;.play-top p:eq(1)&&Text","content":".play-bottom&&Text","tabs":".swiper-slide.zbs a","lists":".content-playlist li:eq(#id) a"},
+		二级:{"title":"h2&&Text;.data:eq(0) a:eq(0)&&Text","img":".lazyload&&data-original","desc":";.data:eq(0) a:eq(2)&&Text;.data:eq(-1) a:eq(2)&&Text;.data:eq(-1) a:eq(1)&&Text;.play-top p:eq(0)&&Text;.play-top p:eq(1)&&Text","content":".play-bottom&&Text","tabs":".swiper-slide.zbs a","lists":".content-playlist li:eq(#id) a"},
 		//二级:{"title":"h2&&Text;.data:eq(0) a:eq(0)&&Text","img":".lazyload&&data-original","desc":";.data:eq(0) a:eq(2)&&Text;.data:eq(0) a:eq(1)&&Text;.data:eq(2)&&Text;.data:eq(3)&&Text","content":".text-collapse span&&Text","tabs":".swiper-slide a","lists":".content-playlist:eq(#id) a"},		
 	搜索:'.myui-vodlist__media.clearfix li;*;*;*;*',
 }
