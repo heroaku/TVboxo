f44e01aa44710f744edfcd6317bef4cce4d05de9
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-20 12:11:27 -0600

    Update 待完成模版.js

--- Js/模版/待完成模版.js
+++ Js/模版/待完成模版.js
@@ -240,3 +240,29 @@ double:true, // 推荐内容是否双层定位
 }
 
 
+var rule={
+    title:'追剧达人',
+    host:'http://zjdr.tv',
+    url:'/vodshow/id/fyclass/page/fypage.html',
+    searchUrl:'/vod/search/page/fypage/wd/**.html',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{
+        'User-Agent':'UC_UA',
+    },
+    // class_parse:'.fed-pops-navbar&&ul.fed-part-rows&&a.fed-part-eone:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
+    //class_parse:'.myui-header__menu&&li:gt(0):lt(6);a&&Text;a&&href;.*/(.*?).html',
+    class_name:'电影&电视剧&综艺&动漫',
+    class_url:'1&2&3&4',
+    play_parse:true,
+    lazy:'',
+    limit:6,
+    推荐:'.module-main.tab-list&&.module-items.module-poster-items-base&&a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+   double:true, // 推荐内容是否双层定位
+   一级:'.module&&a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+   二级:{"title":"h1&&Text;.tag-link&&Text","img":".module-item-pic&&img&&data-src","desc":".video-info-items:eq(0)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(3)&&Text","content":".vod_content&&Text","tabs":".module-tab-item","lists":".module-player-list:eq(#id)&&.scroll-content&&a"},
+   搜索:'.module-items .module-search-item;a&&title;img&&data-src;.video-serial&&Text;a&&href',
+}
+
+
