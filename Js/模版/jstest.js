874585bc474e90870a78d40b8fc37e2a0900f587
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-22 17:07:52 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,19 +1,21 @@
 var rule={
-    title:'188看',
-    host:'https://188kan.com',
-    url:'/vodshow/id/fyclass/page/fypage.html',
-    //searchUrl:'/vod/search/page/fypage/wd/**.html',
+    title:'小白菜电影',
+    host:'https://www.5imv.cc',
+    url:'/vodshow/fyclass--------fypage---.html',
+    https://www.5imv.cc/vodtype/tv-1/
+    //searchUrl:'/vodsearch/page/fypage/wd/**.html',
+    https://www.5imv.cc/vodsearch/page/2/wd/ai/
     //class_parse:'.navbar-items li:gt(1):lt(6);a&&Text;a&&href;.*/(.*?).html',
 
     searchable:2,
     quickSearch:1,
     filterable:0,
     headers:{'User-Agent':'MOBILE_UA', },
-    class_name:'电影&电视剧&综艺&动漫&纪录片',
-    class_url:'1&2&3&4&5',
-    推荐:'.tab-list.active;a.module-poster-item.module-item;.module-poster-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+    class_name:'电影&电视剧&综艺&动漫',
+    class_url:'dianying&dianshiju&zongyi&dongman',
+    推荐:'ul.stui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:'body a.module-poster-item.module-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-    二级:{"title":"h1&&Text;.module-info-tag&&Text","img":".lazyload&&data-original","desc":".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text","content":".module-info-introduction&&Text","tabs":".module-tab-item","lists":".module-play-list:eq(#id) a"},
-    搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
+    一级:'body .stui-vodlist__box;a&&title;.lazyload&&data-original;;a&&href',
+    二级:{"title":"h1.title&&Text;.stui-content__detail p:eq(1)&&Text","img":".lazyload&&data-original","desc":".stui-content__detail p:eq(-3)&&Text;.stui-content__detail p:eq(-2)&&Text","content":".stui-content__detail p.detail&&Text","tabs":".stui-pannel__head ul li","lists":".stui-content__playlist:eq(#id) li"},
+    // 搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
 }
