315d2fb8a888552c7f3cca25d5424fe9aad84800
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-22 18:30:48 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -11,9 +11,9 @@ var rule={
     headers:{'User-Agent':'MOBILE_UA', },
     class_name:'电影&电视剧&综艺&动漫',
     class_url:'movie&tv&variety&comic',
-    推荐:'ul.stui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;;a&&href',
+    推荐:'ul.stui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;.pic-text.text-right&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:'body .stui-vodlist__box;a&&title;.lazyload&&data-original;;a&&href',
+    一级:'body .stui-vodlist__box;a&&title;.lazyload&&data-original;.pic-text.text-right&&Text;a&&href',
     二级:{"title":"h1.title&&Text;.stui-content__detail p:eq(1)&&Text","img":".lazyload&&data-original","desc":".stui-content__detail p:eq(-3)&&Text;.stui-content__detail p:eq(-2)&&Text;.stui-content__detail p:eq(-2)&&Text;.stui-content__detail p:eq(6)&&Text;.stui-content__detail p:eq(5)&&Text","content":".stui-content__detail p.detail&&Text","tabs":".stui-vodlist__head h3","lists":".stui-content__playlist:eq(#id) li"},
     // 搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
 }
