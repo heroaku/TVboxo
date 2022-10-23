568852fb1831434eee8206a7ba6aab6a7e368652
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-23 16:57:06 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,18 +1,9 @@
-var rule={
-    title:'皮皮泡',
-    host:'https://www.pipipao.com',
-    url:'/vodshow/id/fyclass/page/fypage.html',
-    searchUrl:'/vodsearch.html?wd=**',
-    searchable:2,
-    quickSearch:0,
-    filterable:0,
-    headers:{'User-Agent':'MOBILE_UA',},
-    class_name:'电视剧&电影&综艺&动漫&纪录片',
-    class_url:'dianshiju&dianying&zongyi&dongman&jilupian',
-    class_parse:'stui-header__menu&&li;a&&Text;a&&href;.*/(.*?).html',
-    play_parse:true,
-    推荐:'.stui-vodlist__box;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-    一级:'.stui-vodlist__box;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-    二级:{"title":".stui-content__thumb&&a&&title;.stui-content__detail&&p:eq(0)&&Text","img":"img.lazyload&&data-original","desc":"","content":".desc.hidden-xs&&Text","tabs":"h3.title","lists":".playlist:eq(#id) li"},
-       搜索:'li.clearfix&&div;h3&&Text;.lazyload&&data-original;.pic-text&&Text;a.btn-default&&href',
-}
+var rule = Object.assign(muban.mxpro,{
+	title:'爱看影视',
+    host:'https://ikan6.vip',
+    url:'/vodtype/fyclass-fypage/',
+    searchUrl:'/vodsearch/**----------fypage---/',
+    class_name:'电影&电视剧&综艺&动漫&日韩剧&美剧&港台剧',
+    class_url:'1&2&3&4&15&16&14',
+	class_parse:'',
+});
