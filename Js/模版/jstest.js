b22f0cf080b7554a8fd3fb0040ad6c3a2b414f52
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-23 12:33:59 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,12 +1,13 @@
-var rule = Object.assign(muban.vfed,{
-title:'兰花影院',
-host:'https://www.lanhua.tv/',
-'User-Agent':'MOBILE_UA',
-class_parse:'ul.top_nav&&li;a&&Text;a&&href;./(\\d+).html',
-cate_exclude:'午夜影院|VIP蓝光影院|电视直播',
-推荐:'.cbox_list;ul&&li;a&&title;.lazyload&&data-original;.pic_text.text_right&&Text;a&&href',
-double:true, // 推荐内容是否双层定位
-一级:'.vodlist&&li;a&&title;.lazyload&&data-original;.pic_text.text_right&&Text;a&&href',
-二级:{"title":".hd_tit&&Text;.content_min&&ul&&li&&Text","img":".lazyload&&data-original","desc":";;;.content_min&&ul&&li:eq(2) a&&Text;.content_min&&ul&&li:eq(3) a&&Text","content":".context&&span&&Text","tabs":".play_source_tab&&.active","lists":".content_playlist:eq(#id) li"},
-搜索:'.vodlist&&li;*;*;*;*',
+muban.mxone5.二级.tabs = '.module-tab-item';
+muban.mxone5.二级.lists = '.module-player-list:eq(#id)&&.scroll-content&&a';
+var rule = Object.assign(muban.mxone5,{
+title:'Fun4K',
+host:'https://www.fun4k.com',
+url:'/vod/fyclass--------fypage---/',
+searchUrl:'/vodsearch/**----------fypage---/',
+class_parse:'.nav ul li;a&&Text;a&&href;.*/(.*?)/',
+cate_exclude: 'Bilibili|虎牙',
+headers:{
+		'User-Agent':'PC_UA',
+	},
 });
