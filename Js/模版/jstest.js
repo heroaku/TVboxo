46451d4ece4c5c338c8b0a688df9e5489566182a
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-24 08:06:50 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,9 +1,24 @@
-var rule = Object.assign(muban.mxpro,{
-	title:'爱看影视',
-    host:'https://ikan6.vip',
-    url:'/vodtype/fyclass-fypage/',
-    searchUrl:'/vodsearch/**----------fypage---/',
-    class_name:'电影&电视剧&综艺&动漫&日韩剧&美剧&港台剧',
-    class_url:'1&2&3&4&15&16&14',
-	class_parse:'',
-});
+var rule={
+    title:'if101',
+    host:'https://www.oulevod.tv',
+    // homeUrl:'/',
+    url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+    searchUrl:'/index.php/vod/search.html?wd=**',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent':'MOBILE_UA',
+        // "Cookie": "searchneed=ok"
+    },
+    class_parse:'.conch-nav&&ul&&li;a&&Text;a&&href;./(\\d+).html',
+    cate_exclude:'',
+    play_parse:true,
+    lazy:'',
+    limit:6,
+    推荐:'body&&.hl-list-wrap;ul&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    一级:'.hl-vod-list&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+    二级:{"title":".hl-dc-title&&Text;.hl-col-xs-12&&em&&.hl-text-muted:eq(0)&&Text","img":".hl-lazy&&data-original","desc":".hl-col-xs-12&&em&&.hl-text-muted:eq(-2)&&Text;.hl-col-xs-12&&em&&.hl-text-muted:eq(1)&&Text;.hl-col-xs-12&&em&&.hl-text-muted:eq(2)&&Text","content":".hl-content-text&&Text","tabs":".hl-plays-wrap","lists":".hl-plays-list:eq(#id) li"},
+    搜索:'.hl-list-wrap&&ul&&li;h3&&Text;.hl-lazy&&data-original;.hl-item-title:eq(0)&&Text;a&&href',
+}
