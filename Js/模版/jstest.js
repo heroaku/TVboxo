efdba5130b40d804b78f4b14eb009753aec1c917
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-24 16:48:37 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,6 +1,5 @@
-var rule = Object.assign(muban.mxpro,{
-title:'豆芽影院',
-host:'https://www.imdy.tv',
-class_parse:'.navbar-items&&li;a&&Text;a&&href;.*/(.*?).html',
-cate_exclude: '追剧周表|今日更新0|热搜',
+muban.海螺3.二级.content = '.hl-infos-content&&Text';
+var rule = Object.assign(muban.海螺3,{
+    title:'艾旦影视',
+    host:'https://www.lovedan.net',
 });
