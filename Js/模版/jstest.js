f7b0282b0de9670373ca19d86d1400d79f189516
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-24 15:15:02 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,5 +1,5 @@
 var rule = Object.assign(muban.mxpro,{
 title:'豆芽影院',
 host:'https://www.imdy.tv',
-class_parse:'.navbar-items li:gt(1):lt(8);a&&Text;a&&href;.*-(.*?).html',
+class_parse:'.navbar-items li;a&&Text;a&&href;.*-(.*?).html',
 });
