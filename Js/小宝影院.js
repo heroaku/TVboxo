3081d33761fb7cf3bc3d0f831680673f003f701b
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-25 16:48:25 -0600

    Update 小宝影院.js

--- Js/小宝影院.js
+++ Js/小宝影院.js
@@ -18,5 +18,5 @@ var rule={
     double:true, // 推荐内容是否双层定位
     一级:'.myui-vodlist li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
     二级:{"title":".myui-content__detail .title&&Text;.text-muted:eq(-1)&&Text","img":".myui-content__thumb .lazyload&&data-original","desc":".text-muted:eq(-1)&&Text;.text-muted:eq(-1)&&Text;.myui-content__detail p:eq(1)&&Text;.myui-content__detail p:eq(3)&&Text;.myui-content__detail p:eq(4)&&Text","content":".content&&Text","tabs":".nav-tabs:eq(0) li","lists":".myui-content__list:eq(#id) li"},
-    搜索:'#searchList li;a&&title;.lazyload&&data-original;.myui-vodlist__thumb&&span:eq(2)&&Text;a&&href;.text-muted:eq(-1)&&Text',
+    搜索:'#searchList li;a&&title;.lazyload&&data-original;.myui-vodlist__thumb&&span:eq(3)&&Text;a&&href;.text-muted:eq(-1)&&Text',
 }
