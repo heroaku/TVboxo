ee6f861931cb570262b817eda88a1814b0088d0f
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-24 18:10:47 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,5 +1,10 @@
-muban.海螺3.二级.content = '.hl-infos-content&&Text';
 var rule = Object.assign(muban.海螺3,{
-    title:'艾旦影视',
-    host:'https://www.lovedan.net',
+title:'艾旦影视',
+host:'https://www.lovedan.net',
+url:'/vodshow/fyclass--------fypage---.html',
+searchUrl:'/vodsearch/**----------fypage---.html',
 });
+
+
+
+//muban.海螺3.二级.content = '.hl-infos-content&&Text';
