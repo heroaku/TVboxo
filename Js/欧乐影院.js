a2c280b1f220da2b4bdf5bd39980c45717af5fef
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-26 11:20:49 -0600

    Update 欧乐影院.js

--- Js/欧乐影院.js
+++ Js/欧乐影院.js
@@ -8,6 +8,6 @@ cate_exclude:'午夜影院|VIP蓝光影院|电视直播',
 推荐:'.cbox_list;ul&&li;a&&title;.lazyload&&data-original;.text_dy&&Text;a&&href',
 double:true, // 推荐内容是否双层定位
 一级:'.vodlist&&li;a&&title;.lazyload&&data-original;.text_dy&&Text;a&&href',
-二级:{"title":".hd_tit&&Text;.content_min&&ul&&li&&Text","img":".lazyload&&data-original","desc":";;;.content_min&&ul&&li:eq(2) a&&Text;.content_min&&ul&&li:eq(3) a&&Text","content":".context&&span&&Text","tabs":".play_source_tab&&.active","lists":".content_playlist:eq(#id) li","playerType":2},
+二级:{"title":".hd_tit&&Text;.content_min&&ul&&li&&Text","img":".lazyload&&data-original","desc":";;;.content_min&&ul&&li:eq(2) a&&Text;.content_min&&ul&&li:eq(3) a&&Text","content":".context&&span&&Text","tabs":".play_source_tab&&.active","lists":".content_playlist:eq(#id) li"},
 搜索:'.vodlist&&li;*;*;*;*',
 });
