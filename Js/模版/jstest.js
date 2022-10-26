31142c80eb5d4c9c6f03fc4dd391f682c64b0233
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-26 12:27:07 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,4 +1,4 @@
-muban.mxone5.二级.desc = '.video-info-items:eq(-1)&&Text;.video-info-items:eq(-1)&&Text;.video-info-items:eq(-2)&&Text;.video-info-items:eq(1)&&.video-info-item.video-info-actor&&Text;.video-info-items:eq(0)&&.video-info-item.video-info-actor&&Text';
+muban.mxone5.二级.desc = '.mute-info-items:eq(-1)&&Text;.mute-info-items:eq(-1)&&Text;.video-info-items:eq(-2)&&Text;.video-info-items:eq(1)&&.video-info-item.video-info-actor&&Text;.video-info-items:eq(0)&&.video-info-item.video-info-actor&&Text';
 var rule = Object.assign(muban.mxone5,{
 title:'我爱跟剧',
 host:'https://www.genmov.com',
