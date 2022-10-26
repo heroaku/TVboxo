857f6d1d4694987c65e6ce10ae83f4dae46a8ee3
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-26 12:13:14 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -3,8 +3,6 @@ title:'我爱跟剧',
 host:'https://www.genmov.com',
 url:'/vodshow/fyclass--------fypage---/',
 searchUrl:'/vodsearch/**----------fypage---/',
-//class_parse:'.nav ul li;a&&Text;a&&href;.*/(.*?)/',
-//cate_exclude: 'Bilibili|虎牙',
 headers:{
 		'User-Agent':'PC_UA',
 	},
