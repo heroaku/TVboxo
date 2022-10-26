77623dac9b006d2f0c11a3f40478d2d1c86b396a
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-26 10:37:39 -0600

    Update 欧乐影院.js

--- Js/欧乐影院.js
+++ Js/欧乐影院.js
@@ -2,6 +2,9 @@ var rule = Object.assign(muban.vfed,{
 title:'欧乐影院',
 host:'https://www.olevod.com',
 'User-Agent':'MOBILE_UA',
+//"playerType":2,  0 系统  1 ljk  2 exo
+playerType:2,
+  
 class_parse:'ul.top_nav&&li;a&&Text;a&&href;./(\\d+).html',
 cate_exclude:'午夜影院|VIP蓝光影院|电视直播',
 推荐:'.cbox_list;ul&&li;a&&title;.lazyload&&data-original;.text_dy&&Text;a&&href',
