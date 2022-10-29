c9e6a0b1b58ea43d945f75e3fbb8480101792145
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-29 10:54:27 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,24 +1,16 @@
-var rule = {
-    title:'天天视频',
-    host:'http://www.ttsp.tv',
-    // homeUrl:'/',
-    url:'/vodshow/fyclass--------fypage---.html',
-    searchUrl:'/vodsearch/**----------fypage---.html',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-        'User-Agent':'MOBILE_UA',
-        // "Cookie": "searchneed=ok"
-    },
-    class_name:'电影&电视剧&综艺&动漫',
-    class_url:'1&2&3&4',
-    play_parse:true,
-    lazy:'',
-    limit:6,
-    推荐:'ul.vodlist.vodlist_wi;li;a&&title;a&&data-original;.pic_text.text_right&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'li.vodlist_item;a&&title;a&&data-original;.pic_text.text_right&&Text;a&&href',
-    二级:{"title":"h1&&Text;.text-muted:eq(-1)&&Text","img":".content_thumb .vodlist_thumb&&data-original","desc":".text-muted:eq(-1)&&Text;.text-muted:eq(-1)&&Text;.text-muted:eq(-1)&&Text;.content_detail.content_min.fl&&ul&&li:eq(3)&&a&&Text;.content_detail.content_min.fl&&ul&&li:eq(4)&&a&&Text","content":".content&&Text","tabs":".play_source_tab:eq(0) a","lists":".play_list_box:eq(#id) .playlist_notfull&&ul li"},
-    搜索:'body .searchlist_item;a&&title;.vodlist_thumb.lazyload&&data-original;.pic_text.text_right&&Text;a&&href',
-}
+
+muban.mxone5.二级.tabs = '.module-tab-item';
+muban.mxone5.二级.lists = '.module-player-list:eq(#id)&&.scroll-content&&a';
+var rule = Object.assign(muban.mxone5,{
+title:'Fun4K',
+host:'https://ilqtugu.cn',
+url:'/vod/fyclass--------fypage---/',
+    https://ilqtugu.cn/show/tv--------fypage---.html
+    https://ilqtugu.cn/search/page/fypage/wd/**.html
+searchUrl:'/vodsearch/**----------fypage---/',
+class_parse:'.nav ul li;a&&Text;a&&href;.*/(.*?)/',
+cate_exclude: 'Bilibili|虎牙',
+headers:{
+		'User-Agent':'PC_UA',
+	},
+});
