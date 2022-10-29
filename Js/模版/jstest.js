dad8d54bf784912816e31f3acd3d5d9dca38423d
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-29 11:44:04 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -4,10 +4,8 @@ muban.mxone5.二级.lists = '.module-player-list:eq(#id)&&.scroll-content&&a';
 var rule = Object.assign(muban.mxone5,{
 title:'Fun4K',
 host:'https://ilqtugu.cn',
-url:'/vod/fyclass--------fypage---/',
-    https://ilqtugu.cn/show/tv--------fypage---.html
-    https://ilqtugu.cn/search/page/fypage/wd/**.html
-searchUrl:'/vodsearch/**----------fypage---/',
+url:'/show/fyclass--------fypage---.html',
+searchUrl:'/search/page/fypage/wd/**.html,
 class_parse:'.nav ul li;a&&Text;a&&href;.*/(.*?)/',
 cate_exclude: 'Bilibili|虎牙',
 headers:{
