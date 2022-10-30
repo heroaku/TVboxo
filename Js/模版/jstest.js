7b1d72553b574dfa313a22c761e654b448a959f8
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-30 09:22:51 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -16,5 +16,5 @@ limit:6,
 double:true, // 推荐内容是否双层定位
 一级:'.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
 二级:{"title":"h1&&Text;.video-info-aux&&div&&a:eq(0)&&Text","img":".module-item-pic&&img&&data-src","desc":";.video-info-aux&&a:eq(1)&&Text;.video-info-aux&&a:eq(2)&&Text;.video-info-items:eq(1)&&.video-info-actor&&Text;.video-info-items:eq(0)&&.video-info-actor&&Text","content":".video-info-items:eq(7)&&.video-info-content&&Text","tabs":".module-tab-item.tab-item","lists":".module-player-list:eq(#id)&&.scroll-content&&a"},
-搜索:'.module-items .module-item;*;*;*;*',
+搜索:'.module-items&&.module-search-item;h3&&Text;*;.video-serial&&Text;*',
 }
