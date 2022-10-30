cfab0a0d5e0026b8d1ebb95b45f299c3cee9c6e0
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-30 09:56:59 -0600

    Update 待完成模版.js

--- Js/模版/待完成模版.js
+++ Js/模版/待完成模版.js
@@ -1,3 +1,24 @@
+var rule={
+title:'兰花影院',
+host:'https://www.lanhua.tv/',
+url:'/vodshow/fyclass--------fypage---.html',
+searchUrl:'/vodsearch/**----------fypage---.html',
+searchable:2,
+quickSearch:0,
+filterable:0,
+headers:{'User-Agent':'MOBILE_UA', },
+class_name:'电影&电视剧&综艺&动漫&记录片',
+class_url:'1&2&3&4&5',
+play_parse:true,
+lazy:'',
+limit:6,
+推荐:'.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+double:true, // 推荐内容是否双层定位
+一级:'.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+二级:{"title":"h1&&Text;.video-info-aux&&div&&a:eq(0)&&Text","img":".module-item-pic&&img&&data-src","desc":";.video-info-aux&&a:eq(1)&&Text;.video-info-aux&&a:eq(2)&&Text;.video-info-items:eq(1)&&.video-info-actor&&Text;.video-info-items:eq(0)&&.video-info-actor&&Text","content":".video-info-items:eq(7)&&.video-info-content&&Text","tabs":".module-tab-item.tab-item","lists":".module-player-list:eq(#id)&&.scroll-content&&a"},
+搜索:'.module-items&&.module-search-item;h3&&Text;*;.video-serial&&Text;*',
+}
+
 var rule = {
     title:'天天视频',
     host:'http://www.ttsp.tv',
