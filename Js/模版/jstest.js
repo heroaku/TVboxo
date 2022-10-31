b44468f549cb5a671307d750d887e7fceffbde68
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-31 14:59:20 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,27 +1,28 @@
-var rule = {
-    title:'首播影院',
-    host:'https://www.23zhui.com',
+
+var rule={
+    title:'7喜影院',
+    host:'https://7xi.tv',
+    // host:'https://zaoii.com',
     // homeUrl:'/',
-    url:'/vodtype/fyclass-fypage.html',
-    headers:{
+    url:'/vodshow/fyclass--------fypage---/',
+    searchUrl:'/vodsearch/**----------fypage---/',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
         'User-Agent':'MOBILE_UA',
-        "Cookie": "searchneed=ok"
+        // "Cookie": "searchneed=ok"
     },
-    searchUrl:'/vodsearch/**----------fypage---.html',
-    searchable:2,
-    // class_name:'电影&网剧&剧集&动漫&综艺&记录',
-    // class_url:'20&1&2&3&4&23',
-    class_parse:'.stui-header__menu li:gt(0):lt(5);a&&Text;a&&href;/(\\d+).html',
+    class_parse:'..hl-nav-wrap&&ul&&li;a&&Text;a&&href;.*/(.*?)/',
+    cate_exclude:'',
+    //class_name:'电视剧&电影&综艺&动漫',
+    //class_url:'lianxuju&dianying&zongyi&dongman',
     play_parse:true,
-    // lazy:'干饭',
-    // lazy:'@js:cacheUrl = d.getParse(input);print(cacheUrl);input=cacheUrl',
-    // lazy:'js:cacheUrl = d.getParse(input);print(cacheUrl);input=cacheUrl',
-    // lazy:'js:cacheUrl=d.getParse(input);if(cacheUrl){input=cacheUrl}else{try{let html=fetch(input,{headers:d.headers,timeout:d.timeout,encoding:d.encoding});let ret=html.match(/var player_(.*?)=(.*?)</)[2];let url=JSON.parse(ret).url;if(url.length>10){real_url="https://player.buyaotou.xyz/?url="+url;d.saveParse(input,real_url);input=real_url}}catch(e){print("网络请求发生错误:"+e.message)}}',
-    lazy:'js:cacheUrl=getParse(input);if(cacheUrl){input=cacheUrl}else{try{let html=fetch(input,fetch_params);let ret=html.match(/var player_(.*?)=(.*?)</)[2];let url=JSON.parse(ret).url;if(url.length>10){real_url="https://player.buyaotou.xyz/?url="+url;saveParse(input,real_url);input=real_url}}catch(e){print("网络请求发生错误:"+e.message)}}',
-    limit:5,
-    推荐:'ul.stui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;.pic-text.text-right&&Text;a&&href',
+    lazy:'',
+    limit:6,
+    推荐:'body&&.hl-list-wrap;ul&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:'body .stui-vodlist__box;a&&title;.lazyload&&data-original;.pic-text.text-right&&Text;a&&href',
-    二级:{"title":"h1.title&&Text;.stui-content__detail p:eq(1)&&Text","img":".lazyload&&data-original","desc":".stui-content__detail p:eq(-3)&&Text;.stui-content__detail p:eq(-2)&&Text","content":".stui-content__detail p.detail&&Text","tabs":".nav.nav-tabs.dpplay li","lists":".tab-pane:eq(#id)&&.stui-content__playlist li"},
-    // 搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
+    一级:'.hl-vod-list&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+    二级:{"title":".hl-dc-title&&Text;.hl-col-xs-12&&em&&.hl-text-muted:eq(0)&&Text","img":".hl-lazy&&data-original","desc":".hl-col-xs-12&&em&&.hl-text-muted:eq(-2)&&Text;.hl-col-xs-12&&em&&.hl-text-muted:eq(1)&&Text;.hl-col-xs-12&&em&&.hl-text-muted:eq(2)&&Text","content":".hl-content-text&&Text","tabs":".hl-plays-from a","lists":".hl-plays-list:eq(#id) li"},
+    搜索:'.hl-list-wrap&&ul&&li;.hl-item-thumb&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
 }
