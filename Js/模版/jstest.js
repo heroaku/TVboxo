f8d63378220d65ba234f1f3a858202e8e5726e8b
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-31 09:09:52 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -17,9 +17,9 @@ var rule={
     play_parse:true,
     lazy:'',
     limit:6,
-		推荐:'body .main;.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-		double:true, // 推荐内容是否双层定位
-		一级:'.module-items .module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-		二级:{"title":"h1&&Text;.tag-link&&Text","img":".module-item-pic&&img&&data-src","desc":".video-info-items:eq(-2)&&Text;.video-info-items:eq(-1)&&Text;.video-info-items:eq(-2)&&Text;.video-info-items:eq(1)&&.video-info-item&&Text;.video-info-items:eq(-2)&&.video-info-item&&Text","content":".zkjj_a&&Text","tabs":".module-tab-items-box&&.module-tab-item.tab-item","lists":".module-list:eq(#id)&&.module-play-list-content a"},
-		搜索:'.module-card-item-poster;.lazy&&alt;*;.module-item-note&&Text;*',
+    推荐:'body .main;.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    一级:'body a.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    二级:{"title":"h1&&Text;.module-info-tag&&Text","img":"img&&data-original","desc":".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text","content":".module-info-introduction&&Text","tabs":".module-tab-item","lists":".module-play-list:eq(#id) a"},
+    搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
 }
