9123ee3c9def8e845de8890194c0aa0b78a2f58b
heroaku <108630267+heroaku@users.noreply.github.com>
2022-10-31 14:34:01 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,27 +1,27 @@
 var rule = {
-    title:'米宝影院',
-    //host:'https://www.zhuiju.la',
-    host:'https://www.mibaotv.com/',
+    title:'首播影院',
+    host:'https://www.23zhui.com',
     // homeUrl:'/',
-    url:'/vodshow/fyclass--------fypage---.html',
-    searchUrl:'/vodsearch/**----------fypage---.html',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+    url:'/vodtype/fyclass-fypage.html',
+    headers:{
         'User-Agent':'MOBILE_UA',
-        // "Cookie": "searchneed=ok"
+        "Cookie": "searchneed=ok"
     },
-    class_name:'电影&电视剧&综艺&动漫',
-    class_url:'1&2&3&4',
+    searchUrl:'/vodsearch/**----------fypage---.html',
+    searchable:2,
+    // class_name:'电影&网剧&剧集&动漫&综艺&记录',
+    // class_url:'20&1&2&3&4&23',
+    class_parse:'.stui-header__menu li:gt(0):lt(5);a&&Text;a&&href;/(\\d+).html',
     play_parse:true,
-    lazy:'',
-    limit:6,
-    推荐:'.module-items;.module-item;.module-item-cover&&.module-item-pic&&a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-    //推荐:'.module-item;.module-item-cover&&.module-item-pic;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
+    // lazy:'干饭',
+    // lazy:'@js:cacheUrl = d.getParse(input);print(cacheUrl);input=cacheUrl',
+    // lazy:'js:cacheUrl = d.getParse(input);print(cacheUrl);input=cacheUrl',
+    // lazy:'js:cacheUrl=d.getParse(input);if(cacheUrl){input=cacheUrl}else{try{let html=fetch(input,{headers:d.headers,timeout:d.timeout,encoding:d.encoding});let ret=html.match(/var player_(.*?)=(.*?)</)[2];let url=JSON.parse(ret).url;if(url.length>10){real_url="https://player.buyaotou.xyz/?url="+url;d.saveParse(input,real_url);input=real_url}}catch(e){print("网络请求发生错误:"+e.message)}}',
+    // lazy:'js:cacheUrl=getParse(input);if(cacheUrl){input=cacheUrl}else{try{let html=fetch(input,fetch_params);let ret=html.match(/var player_(.*?)=(.*?)</)[2];let url=JSON.parse(ret).url;if(url.length>10){real_url="https://player.buyaotou.xyz/?url="+url;saveParse(input,real_url);input=real_url}}catch(e){print("网络请求发生错误:"+e.message)}}',
+    limit:5,
+    推荐:'ul.stui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;.pic-text.text-right&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    //一级:'.module-list&&.module-item-pic;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
-    一级:'.module-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-    二级:{"title":"h1&&Text;.video-info-aux&&Text","img":".lazyload&&data-original","desc":".video-info-items:eq(-1)&&Text;.video-info-items:eq(-3)&&Text;.video-info-items:eq(-2)&&Text;.video-info-items:eq(1)&&.video-info-item.video-info-actor&&Text;.video-info-items:eq(0)&&.video-info-item.video-info-actor&&Text","content":".module-info-introduction-content&&Text","tabs":".module-tab-item.tab-item","lists":".module-list:eq(#id)&&.module-play-list-content a"},
-    搜索:'.module-search-item;h3&&Text;img&&data-original;.module-item-note&&Text;a&&href',
- }
+    一级:'body .stui-vodlist__box;a&&title;.lazyload&&data-original;.pic-text.text-right&&Text;a&&href',
+    二级:{"title":"h1.title&&Text;.stui-content__detail p:eq(1)&&Text","img":".lazyload&&data-original","desc":".stui-content__detail p:eq(-3)&&Text;.stui-content__detail p:eq(-2)&&Text","content":".stui-content__detail p.detail&&Text","tabs":".stui-vodlist__head h3","lists":".stui-vodlist__head:eq(#id)&&.stui-content__playlist li"},
+    // 搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
+}
