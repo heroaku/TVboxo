e2ec4683168e8a48f5af8b2137f948b77752f24a
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-02 13:54:56 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,6 +1,6 @@
 var rule = Object.assign(muban.vfed,{
 title:'zhui影院',
-host:'https://www.zhuijumi.cc/',
+host:'https://www.zhuijumi.cc',
 url:'/videotype//fyclass--------fypage---.html',
-searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
+searchUrl:'/vodsearch/**-fypage.html',
 });
