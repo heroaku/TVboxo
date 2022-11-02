57c36df58a6c025f73cf01fe7f0212606a813482
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-02 12:59:43 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -10,7 +10,7 @@ var rule={
         'User-Agent':'UC_UA',
     },
     // class_parse:'.fed-pops-navbar&&ul.fed-part-rows&&a.fed-part-eone:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
-    class_parse:'.myui-header__menu&&li;a&&Text;a&&href;.*/(.*?).html',
+    class_parse:'.myui-header__menu&&li.hidden-sm;a&&Text;a&&href;.*/(.*?).html',
     play_parse:true,
     lazy:'',
     limit:6,
