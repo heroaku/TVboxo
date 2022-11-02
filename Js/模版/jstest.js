cc8832eb59abf21565fbb06594b1266d776a68eb
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-01 19:00:42 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -3,7 +3,7 @@ var rule = {
     host:'https://www.xigutv.com',
     // homeUrl:'/',
     url:'/index.php/vod/show/id/fyclass/page/fypage.html',
-    searchUrl:'/vodsearch/**----------fypage---.html',
+    searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
     searchable:2,//是否启用全局搜索,
     quickSearch:0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
