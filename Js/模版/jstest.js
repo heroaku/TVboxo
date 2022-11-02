3587771c686c200eb69b8a4907acc469f04fc3be
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-02 15:28:35 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -9,6 +9,6 @@ searchUrl:'/vodsearch/**-fypage.html',
     推荐:'ul.fed-list-info.fed-part-rows;li;a.fed-list-title&&Text;a&&data-original;.fed-list-remarks&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.fed-list-info&&li;a.fed-list-title&&Text;a&&data-original;.fed-list-remarks&&Text;a&&href',
-    二级:{"title":"h1&&Text;.fed-mute&&Text","img":".fed-list-info&&a&&data-original","desc":".fed-deta-content&&.fed-part-rows&&li:eq(-1)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(-2)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(-3)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(0)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(1)&&Text","content":".fed-part-esan&&Text","tabs":".fed-tabs-foot&&.fed-part-rows li","lists":".fed-deta-playlist&&.fed-tabs-btm:eq(#id) li"},
+    二级:{"title":"h1&&Text;.fed-mute&&Text","img":".fed-list-info&&a&&data-original","desc":".fed-deta-content&&.fed-part-rows&&li:eq(-1)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(-2)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(-3)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(0)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(1)&&Text","content":".fed-conv-text&&Text","tabs":".fed-tabs-foot&&.fed-part-rows li","lists":".fed-deta-playlist&&.fed-tabs-btm:eq(#id) li"},
     搜索:'.fed-list-deta;h1&&Text;.fed-lazy&&data-original;.fed-list-remarks&&Text;a&&href;.fed-deta-content&&Text',
 });
