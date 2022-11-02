736b8a67fc9dd6cf9d9f5aa90e4d221ec68e7da1
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-02 10:05:08 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -11,13 +11,13 @@ var rule={
         'User-Agent':'PC_UA',
         // "Cookie": "searchneed=ok"
     },
-    class_name:'电影&剧集&综艺&动漫&记录',
+    class_name:'电影&剧集&综艺&动漫&纪录',
     class_url:'dianying&juji&zhongyi&dongman&jilu',
     lazy:'',
     limit:6,
     推荐:'.pack-ykpack;a&&title;.bj.eclazy&&data-original;.pack-prb&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.pack-ykpack;a&&title;.bj.eclazy&&data-original;.pack-prb&&Text;a&&href',
-    二级:{"title":"h1&&title;.s-top-info-title&&Text","img":".a&&img","desc":".data:eq(0)&&Text;.data:eq(1)&&Text;.data:eq(2)&&Text;.data:eq(3)&&Text","content":".item-desc&&Text","tabs":".channelname.swiper-slide","lists":".content_playlist:eq(#id) a"},
+    二级:{"title":"h1&&title;.s-top-info-title&&Text","img":".eclazy&&data-original","desc":".data:eq(0)&&Text;.data:eq(1)&&Text;.data:eq(2)&&Text;.data:eq(3)&&Text","content":".item-desc&&Text","tabs":".channelname.swiper-slide","lists":".content_playlist:eq(#id) a"},
     搜索:'.pack-packcover.returl.list-top-b;a&&title;.bj.eclazy&&data-original;.pack-prb&&Text;a&&href',
 }
