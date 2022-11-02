9cc637f01dc2e9d6b6a5fb38a50863e44fc216ed
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-01 18:40:58 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,5 +1,5 @@
 var rule={
-title:'兰花影院',
+title:'艾菲影视',
 host:'https://www.ifimv.com',
 
 url:'/vodshow/fyclass--------fypage---.html',
@@ -10,13 +10,13 @@ quickSearch:0,
 filterable:0,
 headers:{'User-Agent':'MOBILE_UA', },
 class_name:'电影&电视剧&综艺&动漫&纪录片',
-class_url:'1&2&3&4&5',
+class_url:'1&2&3&4&29',
 play_parse:true,
 lazy:'',
 limit:6,
 推荐:'.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
 double:true, // 推荐内容是否双层定位
 一级:'.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
-二级:{"title":"h1&&Text;.video-info-aux&&div&&a:eq(0)&&Text","img":".module-item-pic&&img&&data-src","desc":";.video-info-aux&&a:eq(1)&&Text;.video-info-aux&&a:eq(2)&&Text;.video-info-items:eq(1)&&.video-info-actor&&Text;.video-info-items:eq(0)&&.video-info-actor&&Text","content":".video-info-items:eq(7)&&.video-info-content&&Text","tabs":".module-tab-item.tab-item","lists":".module-player-list:eq(#id)&&.scroll-content&&a"},
+二级:{"title":"h1&&Text;.video-info-aux&&div&&a:eq(0)&&Text","img":".module-item-pic&&img&&data-src","desc":";.video-info-aux&&a:eq(1)&&Text;.video-info-aux&&a:eq(2)&&Text;.video-info-items:eq(1)&&.video-info-actor&&Text;.video-info-items:eq(0)&&.video-info-actor&&Text","content":".video-info-items:eq(5)&&.video-info-content&&Text","tabs":".module-tab-item.tab-item","lists":".module-player-list:eq(#id)&&.scroll-content&&a"},
 搜索:'.module-items&&.module-search-item;h3&&Text;*;.video-serial&&Text;*',
 }
