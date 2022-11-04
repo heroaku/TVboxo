98c960848fa7ee908b3759cfb1539dc6b86b2740
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-04 17:56:29 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,21 +1,26 @@
-var rule={
-title:'稀饭影视',
-host:'https://www.xifanys.com',
-
-url:'/yingpianshow/fyclass--------fypage---.html',
-searchUrl:'/yingpiansearch/**----------fypage---.html',
-searchable:2,
-quickSearch:0,
-filterable:0,
-headers:{'User-Agent':'MOBILE_UA', },
-class_name:'电影&电视剧&综艺&动漫&纪录片',
-class_url:'dianying&lianxuju&zongyi&dongman&jilupian',
-play_parse:true,
-lazy:'',
-limit:6,
-推荐:'.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
-double:true, // 推荐内容是否双层定位
-一级:'.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
-二级:{"title":"h1&&Text;.video-info-aux&&div&&a:eq(0)&&Text","img":".module-item-pic&&img&&data-src","desc":";.video-info-aux&&a:eq(1)&&Text;.video-info-aux&&a:eq(2)&&Text;.video-info-items:eq(1)&&.video-info-actor&&Text;.video-info-items:eq(0)&&.video-info-actor&&Text","content":".video-info-items:eq(6)&&.video-info-content&&Text","tabs":".module-tab-item.tab-item","lists":".module-player-list:eq(#id)&&.scroll-content&&a"},
-搜索:'.module-items&&.module-search-item;h3&&Text;*;.video-serial&&Text;*',
+var rule = {
+    title:'歪片星球',
+    host:'https://www.mfxs0.cn',
+    // homeUrl:'/',
+    url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+    searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent':'MOBILE_UA',
+        // "Cookie": "searchneed=ok"
+    },
+    //class_parse:'.nav-menu-items&&li;a&&title;a&&href;/(\\d+).html',
+    class_name:'电影&电视剧&综艺&动漫&纪录片',
+    class_url:'1&2&3&4&jilupian',
+    play_parse:true,
+    lazy:'',
+    limit:6,
+    //推荐:'a.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    推荐:'.tab-list.active;.module-poster-item.module-item;.module-poster-item-title&&Text;.lazyload&&data-src;.module-item-note&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    一级:'.module-poster-item.module-item;a&&title;img&&data-src;.module-item-note&&Text;a&&href',
+    二级:{"title":"h1&&Text;.module-info-tag-link:eq(2)&&Text","img":".module-item-pic&&img&&data-src","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-4)&&Text;.module-info-content&&.module-info-item:eq(5)&&Text;.module-info-content&&.module-info-item:eq(3)&&.module-info-item-content&&Text;.module-info-content&&.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction-content&&Text","tabs":".module-tab-items-box:eq(0)&&.module-tab-item","lists":".module-list:eq(#id)&&.module-play-list-content a"},
+    搜索:'.module-card-item.module-item;.module-card-item-title&&Text;img&&data-src;.module-item-note&&Text;a.play-btn-o&&href',
 }
