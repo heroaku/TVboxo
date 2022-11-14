7fb23ea5e5a14e94b723b6c1249158d56456e2d9
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-14 12:25:30 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -20,6 +20,6 @@ var rule = {
     推荐:'ul.stui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;.pic-text.text-right&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'body .stui-vodlist__box;a&&title;.lazyload&&data-original;.pic-text.text-right&&Text;a&&href',
-    二级:{"title":"h1.title&&Text;.stui-content__detail p:eq(1)&&Text","img":".lazyload&&data-original","desc":".stui-content__detail p:eq(-3)&&Text;.stui-content__detail p:eq(-2)&&Text;.stui-content__detail p:eq(-2)&&Text;.stui-content__detail p:eq(5)&&Text;.stui-content__detail p:eq(4)&&Text","content":".stui-content__detail p.detail&&Text","tabs":".stui-pannel-box.b.playlist.mb&&.stui-pannel__head.bottom-line.active.clearfix h3","lists":".stui-pannel-box.b.playlist.mb&&.stui-content__playlist:eq(#id) li"},
+    二级:{"title":"h1.title&&Text;.stui-content__detail p:eq(1)&&Text","img":".lazyload&&data-original","desc":".stui-content__detail p:eq(-3)&&Text;.stui-content__detail p:eq(-2)&&Text;.stui-content__detail p:eq(-2)&&Text;.stui-content__detail p:eq(5)&&Text;.stui-content__detail p:eq(4)&&Text","content":".stui-content__detail p.detail&&Text","tabs":".stui-pannel-bg:eq(#id)&&.stui-pannel-box.b.playlist.mb&&.stui-pannel__head.bottom-line.active.clearfix h3","lists":".stui-pannel-bg:eq(#id)&&.stui-pannel-box.b.playlist.mb&&.stui-content__playlist li"},
     搜索:'.stui-vodlist.clearfix&&ul&&li;h4&&Text;.stui-vodlist__thumb&&data-original;.stui-vodlist__thumb.lazyload&&.pic-text.text-right&&Text;a&&href',
 }
