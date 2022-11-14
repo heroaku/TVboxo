1c6b206a087a4d5e6ea99ea9c138624b1c9c3b1d
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-14 12:51:04 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,25 +1,25 @@
+
 var rule = {
-     title:'蜂鸟影院',
-    host:'https://www.6ygo.com',
+    title:'闪影影院',
+    host:'https://sytv.cc',
     // homeUrl:'/',
-    url:'/6ytype-fyclass--------fypage---.html',
-    searchUrl:'/vodsearch/**----------fypage---.html',
+    url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+    searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
     searchable:2,//是否启用全局搜索,
-    quickSearch:1,//是否启用快速搜索,
+    quickSearch:0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
     headers:{//网站的请求头,完整支持所有的,常带ua和cookies
         'User-Agent':'MOBILE_UA',
         // "Cookie": "searchneed=ok"
     },
-    //class_parse:'.stui-header__menu li.hidden-xs;a&&Text;a&&href;/(\\d+)/',
-    class_name:'电影&电视剧&综艺&动漫',
-    class_url:'1&2&3y&4',
+    class_name:'电影&电视剧&综艺&动漫&纪录片&国产&港台&日韩&欧美',
+    class_url:'1&2&3&4&27&13&14&15&16',
     play_parse:true,
     lazy:'',
-    limit:8,
-    推荐:'ul.stui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;.pic-text.text-right&&Text;a&&href',
+    limit:6,
+    推荐:'.module-items;.module-poster-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:'body .stui-vodlist__box;a&&title;.lazyload&&data-original;.pic-text.text-right&&Text;a&&href',
-    二级:{"title":"h1.title&&Text;.stui-content__detail p:eq(1)&&Text","img":".lazyload&&data-original","desc":".stui-content__detail p:eq(-3)&&Text;.stui-content__detail p:eq(-2)&&Text;.stui-content__detail p:eq(-2)&&Text;.stui-content__detail p:eq(5)&&Text;.stui-content__detail p:eq(4)&&Text","content":".stui-content__detail p.detail&&Text","tabs":".stui-pannel-box.b.playlist.mb&&.stui-pannel__head.bottom-line.active.clearfix h3","lists":".stui-pannel-bg:eq(#id)&&.stui-pannel-box.b.playlist.mb&&.stui-content__playlist li"},
-    搜索:'.stui-vodlist.clearfix&&ul&&li;h4&&Text;.stui-vodlist__thumb&&data-original;.stui-vodlist__thumb.lazyload&&.pic-text.text-right&&Text;a&&href',
+    一级:'.module-poster-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    二级:{"title":"h1&&Text;.module-info-tag-link:eq(2)&&Text","img":".ls-is-cached.lazy.lazyload&&data-original","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-content&&.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction-content&&Text","tabs":".module-tab-items-box:eq(0)&&.module-tab-item","lists":".module-play-list-content:eq(0) a"},
+    搜索:'.module-card-item.module-item;.module-card-item-title&&Text;img&&data-original;.module-item-note&&Text;a.play-btn-o&&href',
 }
