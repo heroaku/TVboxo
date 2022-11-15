8e37d9412e46a4ed740fa97a34d0fa0c4f4cdb1e
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-15 12:33:56 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -18,6 +18,6 @@ var rule={
     推荐:'.mo-cols-lays;ul.mo-cols-rows li;.mo-situ-name&&Text;a&&data-original;.mo-situ-rema&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.mo-cols-lays:eq(1)&&ul.mo-cols-rows li;.mo-situ-name&&Text;a&&data-original;.mo-situ-rema&&Text;a&&href',
-    二级:{"title":"h1&&title;.s-top-info-title&&Text","img":".g-playicon.s-cover-img&&img&&src","desc":".data:eq(0)&&Text;.data:eq(1)&&Text;.data:eq(2)&&Text;.data:eq(3)&&Text","content":".item-desc&&Text","tabs":".channelname.swiper-slide","lists":".content_playlist:eq(#id) a"},
+    二级:{"title":"h1&&Text;.s-top-info-title&&Text","img":"img&&src","desc":".data:eq(0)&&Text;.data:eq(1)&&Text;.data:eq(2)&&Text;.data:eq(3)&&Text","content":".mo-word-info&&Text","tabs":"h2.mo-fsxs-14px&&.mo-movs-btns","lists":"ul.mo-movs-item:eq(#id)&&li"},
     搜索:'.pack-packcover.returl.list-top-b;a&&title;.bj.eclazy&&data-original;.pack-prb&&Text;a&&href',
 }
