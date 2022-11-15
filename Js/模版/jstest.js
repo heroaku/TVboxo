5b5d83c92644e1c403b6d71601d87f7fb84e3064
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-15 16:11:48 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,23 +1,24 @@
-var rule={
-    title:'天天影视',
-    host:'http://www.b7yy.cc',
+var rule = {
+    title:'蚂蚁影视',
+    host:'https://www.4kvcd.com',
     // homeUrl:'/',
-    url:'/yy6090-show/fyclass/page/fypage.html',
-    searchUrl:'/yy6090-search/page/fypage/wd/**.html',
+    url:'/fyclass/index_fypage_______.html',
+    searchUrl:'/index.php?s=vod-search-wd-**-p-fypage.html',
     searchable:2,//是否启用全局搜索,
     quickSearch:0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
     headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-        'User-Agent':'PC_UA',
+        'User-Agent':'MOBILE_UA',
         // "Cookie": "searchneed=ok"
     },
-    class_name:'电影&剧集&综艺&动漫&纪录',
-    class_url:'dianying&lianxuju&zongyi&dongman&jilupian',
+    class_name:'电影&电视剧&综艺&动漫&纪录片&国产&香港&台湾&韩国&日本&欧美&海外',
+    class_url:'mov&tv&fun&cartoon&jilupian&guocanju&xianggangju&taiwanju&hanguoju&ribenju&oumeiju&haiwaiju',
+    play_parse:true,
     lazy:'',
     limit:6,
-    推荐:'.mo-cols-lays;ul.mo-cols-rows li;.mo-situ-name&&Text;a&&data-original;.mo-situ-rema&&Text;a&&href',
+    推荐:'.module-items;.module-poster-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:'.mo-cols-lays:eq(1)&&ul.mo-cols-rows li;.mo-situ-name&&Text;a&&data-original;.mo-situ-rema&&Text;a&&href',
-    二级:{"title":"h1&&Text;.s-top-info-title&&Text","img":"img.mo-part-full&&src","desc":".data:eq(0)&&Text;.data:eq(1)&&Text;.data:eq(2)&&Text;.data:eq(3)&&Text","content":".mo-word-info&&Text","tabs":"h2.mo-fsxs-14px&&.mo-movs-btns","lists":"ul.mo-movs-item:eq(#id)&&li"},
-    搜索:'.mo-deta-info.mo-cols-rows;h1&&Text;a&&data-original;.mo-situ-rema&&Text;a&&href',
+    一级:'.module-poster-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    二级:{"title":"h1&&Text;.module-info-tag-link:eq(2)&&Text","img":".ls-is-cached.lazy.lazyload&&data-original","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-content&&.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction-content&&Text","tabs":".module-tab-items-box:eq(0)&&.module-tab-item","lists":".module-play-list-content:eq(0) a"},
+    搜索:'.module-card-item.module-item;.module-card-item-title&&Text;img&&data-original;.module-item-note&&Text;a.play-btn-o&&href',
 }
