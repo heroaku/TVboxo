bdaa4d96e0d77dd231bb1dda9389b3ee56f0f99c
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-16 15:59:08 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,9 +1,9 @@
 var rule = {
     title:'KOK影视',
-    host:'https://www.kokyy.com',
+    host:'https://www.leyupro.com',
     // homeUrl:'/',
-    url:'/koks/fyclass/page/fypage.html',
-    searchUrl:'/kokso/page/fypage/wd/**.html',
+    url:'/lys/fyclass/page/fypage.html',
+    searchUrl:'/lyso/page/fypage/wd/**.html',
     searchable:2,//是否启用全局搜索,
     quickSearch:0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
@@ -12,13 +12,13 @@ var rule = {
         // "Cookie": "searchneed=ok"
     },
     class_name:'电影&电视剧&综艺&动漫&纪录片',
-    class_url:'kokdy&kokds&kokzy&kokdm&kokjl',
+    class_url:'lyMovie&lyTv&lyVariety&lyCartoon&lydocumentary',
     play_parse:true,
     lazy:'',
     limit:6,
-    推荐:'.movie-list-body;.movie-list-item;.movie-title&&Text;.movie-post-lazyload&&data-original;.module-item-note&&Text;a&&href',
+    推荐:'.movie-list-body;.movie-list-item;a&&title;.inlist&&data-original;.module-item-note&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:'.movie-list-body&&.movie-list-item;.movie-title&&Text;.movie-post-lazyload&&data-original;.module-item-note&&Text;a&&href',
-    二级:{"title":"h1&&Text;.cr3&&Text","img":".poster&&img&&src","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.scroll-content&&Text;.title-block&&.starLink&&Text;.module-info-content&&.module-info-item:eq(2)&&.module-info-item-content&&Text","content":".detailsTxt&&Text","tabs":".swiper-wrapper&&a","lists":".play_list_box:eq(#id)&&.content_playlist li"},
-    搜索:'.movie-list-body&&.movie-search-list;.movie-title&&Text;.movie-post-lazyload&&data-original;.module-item-note&&Text;a&&href',
+    一级:'.gcol;a&&title;.inlist&&data-original;.module-item-note&&Text;a&&href',
+    二级:{"title":"h1&&Text;.cr3&&Text","img":".poster&&img&&src","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.detailinfo&&p:eq(8)&&Text;.scroll-content&&Text;.album-title&&Text;.detailinfo&&p:eq(2)&&Text","content":".tjuqing&&Text","tabs":".yunplay&&.downtitle&&ul li","lists":".videolist:eq(#id) a"},
+    搜索:'.movie-list-body&&.movie-search-list;a&&title;.inlist&&data-original;.module-item-note&&Text;a&&href',
 }
