279cf40d3ec32cdd86aa34235f769674a9bbd825
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-16 16:07:16 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -16,9 +16,9 @@ var rule = {
     play_parse:true,
     lazy:'',
     limit:6,
-    推荐:'.movie-list-body;.movie-list-item;a&&title;.inlist&&data-original;.module-item-note&&Text;a&&href',
+    推荐:'.listwap;.gcol;a&&title;.inlist&&data-original;.module-item-note&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.gcol;a&&title;.inlist&&data-original;.module-item-note&&Text;a&&href',
-    二级:{"title":"h1&&Text;.cr3&&Text","img":".poster&&img&&src","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.detailinfo&&p:eq(8)&&Text;.scroll-content&&Text;.album-title&&Text;.detailinfo&&p:eq(2)&&Text","content":".tjuqing&&Text","tabs":".yunplay&&.downtitle&&ul li","lists":".videolist:eq(#id) a"},
+    二级:{"title":"h1&&Text;.cr3&&Text","img":".detailpic&&img&&src","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.detailinfo&&p:eq(-8)&&Text;.detailinfo&&p:eq(7)&&Text;.album-title&&Text;.detailinfo&&p:eq(1)&&Text","content":".tjuqing&&Text","tabs":".yunplay&&.downtitle&&ul li","lists":".videolist:eq(#id) a"},
     搜索:'.movie-list-body&&.movie-search-list;a&&title;.inlist&&data-original;.module-item-note&&Text;a&&href',
 }
