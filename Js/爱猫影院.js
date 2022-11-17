1b93d41ffd72e467c676fef77edfc40911e0c72f
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-17 13:04:34 -0700

    Update 爱猫影院.js

--- Js/爱猫影院.js
+++ Js/爱猫影院.js
@@ -18,6 +18,6 @@ var rule={
     推荐:'ul.myui-vodlist.clearfix;li;a&&title;a&&data-original;.pic-tag.text-right&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.myui-vodlist li;a&&title;a&&data-original;.text-right&&Text;a&&href',
-    二级:{"title":".myui-content__detail .title&&Text;.text-muted:eq(-1)&&Text","img":"img&&src","desc":".text-muted:eq(-1)&&Text;.text-muted:eq(-1)&&Text;.pic-text.text-right&&Text;.myui-content__detail p:eq(2)&&Text;.myui-content__detail p:eq(-4)&&Text","content":".yh-text&&Text","tabs":".nav-tabs:eq(0) li","lists":".myui-content__list:eq(#id) li"},
+    二级:{"title":".myui-content__detail .title&&Text;.text-muted:eq(-1)&&Text","img":"img&&src","desc":".text-muted:eq(-1)&&Text;.text-muted:eq(-1)&&Text;.pic-text.text-right&&Text;.myui-content__detail p:eq(2)&&Text;.text-muted:eq(-1)&&Text","content":".yh-text&&Text","tabs":".nav-tabs:eq(0) li","lists":".myui-content__list:eq(#id) li"},
     搜索:'#searchList li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href;.text-muted:eq(-1)&&Text',
 }
