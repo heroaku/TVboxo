7c7517df637eb50768b70b95b9c42d557b7570ad
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-20 14:33:40 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,8 +1,9 @@
 var rule={
     title:'厂长资源',
-    host:'https://www.c-zzy.com',
+    host:'https://www.guazitv.tv',
+    //https://www.guazitv.tv/video/list?channel_id=fyclass&tag=0
      //host:'WWW.CZZY.TV',
-    url:'/fyclass/page/fypage',
+    url:'/video/list?channel_id=fyclass&tag=0',
     searchUrl:'/xssearch?q=**&f=_all&p=fypage',
     searchable:2,//是否启用全局搜索,
     quickSearch:1,//是否启用快速搜索,
@@ -11,11 +12,11 @@ var rule={
     play_parse:true,
     lazy:'',
     limit:6,
-    class_name:'豆瓣电影Top250&最新电影&电视剧&国产剧&美剧&韩剧&番剧&动漫',
-    class_url:'dbtop250&zuixindianying&dsj&gcj&meijutt&hanjutv&fanju&dm',
+    class_name:'电影&电视剧&综艺&动漫&纪录片&世界杯',
+    class_url:'1&2&3&4&32&33',
     推荐:'.bt_img;ul&&li;*;*;*;*',
     double:true,
-    一级:'.bt_img&&ul&&li;h3.dytit&&Text;img.lazy&&data-original;.jidi&&Text;a&&href',
+    一级:'dd;h5&&Text;img&&originalsrc;.mark&&Text;a&&href',
     二级:{"title":"h1&&Text;.moviedteail_list li&&a&&Text","img":"div.dyimg img&&src","desc":".moviedteail_list li:eq(3) a&&Text;.moviedteail_list li:eq(2) a&&Text;.moviedteail_list li:eq(1) a&&Text;.moviedteail_list li:eq(7) a&&Text;.moviedteail_list li:eq(5) a&&Text","content":".yp_context&&Text","tabs":".mi_paly_box&&span","lists":".paly_list_btn:eq(#id) a"},
     搜索:'.search_list&&ul&&li;*;*;*;*',
 }
