905774e50aa77079be742b15355fa624b5d12c69
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-20 15:53:14 -0700

    Update 待完成模版.js

--- Js/模版/待完成模版.js
+++ Js/模版/待完成模版.js
@@ -1,3 +1,28 @@
+
+var rule={
+    title:'瓜子TV',
+    host:'https://m.guazitv9.com',
+    //https://m.guazitv9.com/video/list?channel_id=2&tag=0
+    url:'/video/list?channel_id=fyclass&data-page=fypage',
+    searchUrl:'/xssearch?q=**&f=_all&p=fypage',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:1,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{'User-Agent':'UC_UA', },
+    play_parse:true,
+    lazy:'',
+    limit:6,
+    class_name:'电影&电视剧&综艺&动漫&纪录片&世界杯',
+    class_url:'1&2&3&4&32&33',
+    推荐:'dd;h5&&Text;img&&originalsrc;p.mark&&Text;a&&href',
+    double:false,
+    一级:'dd;h5&&Text;img&&originalsrc;.mark&&Text;a&&href',
+    二级:{"title":"h1&&Text;.moviedteail_list li&&a&&Text","img":"div.dyimg img&&src","desc":".moviedteail_list li:eq(3) a&&Text;.moviedteail_list li:eq(2) a&&Text;.moviedteail_list li:eq(1) a&&Text;.moviedteail_list li:eq(7) a&&Text;.moviedteail_list li:eq(5) a&&Text","content":".pop-intro-detail&&Text","tabs":".pop-intro.video-pop-source&&ul li","lists":".video-detail-series-bottom.pop-content-list&&ul:eq(#id)&&li a"},
+    搜索:'.search_list&&ul&&li;*;*;*;*',
+}
+//播放地址动态嗅探不出
+
+
 var rule={
     title:'回响影视',
     host:'https://hxys.tv',
