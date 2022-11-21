53adaa8deb04e56535ba2a414fd9defff9daec38
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-21 12:19:25 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -2,7 +2,7 @@ var rule={
     title:'布雷蓝光',     
     host:'https://www.bulei.cc',     
     url:'/index.php/vod/show/by/time/id/fyclass/page/fypage.html',
-    searchUrl:'/index.php/vod/search.html?wd=**', 
+    searchUrl:'/index.php/vod/search/page/fypage/wd/**.html', 
     searchable:2,
     quickSearch:1,
     filterable:0,
@@ -12,5 +12,5 @@ var rule={
     推荐:'.module-poster-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
     一级:'.module-poster-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
     二级:{"title":"h1&&Text;","img":".lazyload&&data-original","desc":";;.module-info-item:eq(4) p&&Text;.module-info-item:eq(3) div&&Text;.module-info-item:eq(1) a&&Text","content":".show-desc&&Text","tabs":'#y-playList&&span',"lists":".module-play-list:eq(#id) a"},     
-    搜索:'.module-card-item;imga&&alt;.lazyload&&data-original;.module-item-note&&Text;a&&href', 
+    搜索:'.module-card-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href', 
 }
