0f7d13cabdb8e607080d63516bbd800dcb7aedf1
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-22 14:52:25 -0700

    Update 看视界.js

--- Js/看视界.js
+++ Js/看视界.js
@@ -8,8 +8,8 @@ var rule={
     filterable:0,//是否启用分类筛选,
     //class_parse:'.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
     //cate_exclude:'演员',
-    class_name:'电影&电视剧&综艺&动漫&纪录片&竞技体育&国产剧&港台剧&日韩剧&欧美剧&新马泰&其他',
-    class_url:'1&2&3&4&29&5&30&16&18&47&14&13',  
+    class_name:'电影&电视剧&综艺&动漫&纪录片',
+    class_url:'1&2&3&4&20',  
     推荐:'.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
