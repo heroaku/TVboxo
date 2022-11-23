e146ad52c4c2ce834b55b721d8e2ef22673788aa
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-23 15:09:46 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,16 +1,16 @@
 
 var rule={     
     title:'看视界',
-    host:'https://www.1080kan.cc',
-    url:'/show/fyclass--------fypage---.html',
-    searchUrl:'/search/**----------fypage---.html',
+    host:'https://yrzj.vip',
+    url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+    searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
     searchable:2,//是否启用全局搜索,
     quickSearch:0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
     //class_parse:'.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
     //cate_exclude:'演员',
-    class_name:'电影&电视剧&综艺&动漫&纪录片&竞技体育&国产剧&港台剧&日韩剧&欧美剧&新马泰&其他',
-    class_url:'1&2&3&4&29&5&30&16&18&47&14&13',  
+    class_name:'电影&电视剧&综艺&动漫',
+    class_url:'1&2&3&4',  
 推荐:'.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
 double:true, // 推荐内容是否双层定位
 一级:'.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
