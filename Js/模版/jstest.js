437c65200a66cb4956cd934e54528af09ec768b3
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-25 15:58:10 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,9 +1,18 @@
-var rule={
-    title:'898TV',
-    host:'https://898tv.net',
-    // homeUrl:'/',
-    url:'/show/fyclass--------fypage---.html',
-    searchUrl:'/search/**----------fypage---.html',
+
+//muban.首图2.二级.content = '.stui-content__desc&&Text';
+//muban.首图2.二级.title = '.stui-content__detail&&h3&&Text;.stui-content__detail&&p:eq(1)&&Text;';
+muban.首图2.二级.重定向='js:let url = jsp.pd(html,".playbtn&&a&&href");log("重定向到:"+url);html = request(url)';
+muban.首图2.二级.tabs='.t-ul&&li';
+muban.首图2.二级.lists='.stui-content__playlist:eq(#id)&&li';
+
+var rule = Object.assign(muban.首图2,{
+title:'4K电影',
+host:'https://www.4kvm.com',
+url:'/fyclass/page/fypage',
+searchUrl:'/xssearch?s=**',
+
+
+
     searchable:2,//是否启用全局搜索,
     quickSearch:0,//是否启用快速搜索,
     filterable:0,//是否启用分类筛选,
@@ -14,13 +23,13 @@ var rule={
     //class_parse:'.conch-nav&&ul&&li;a&&Text;a&&href;./(\\d+).html',
     //cate_exclude:'',
     class_name:'电影&电视剧&综艺&动漫&国剧&港剧&台剧&韩剧&日剧&泰剧&欧美剧',
-    class_url:'movie&drama&variety&anime&china&hkg&taiwan&kr&jp&tailan&usa',
+    class_url:'movie&tvshows&variety&anime&china&hkg&taiwan&kr&jp&tailan&usa',
     play_parse:true,
     lazy:'',
     limit:6,
     推荐:'body&&.hl-list-wrap;ul&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:'.hl-vod-list&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+    一级:'.animation-2.items&&.item;h3&&Text;img&&src;.hl-pic-text&&Text;a&&href',
     二级:{"title":".hl-dc-title&&Text;.hl-col-xs-12&&em&&.hl-text-muted:eq(-2)&&Text","img":".hl-lazy&&data-original","desc":";;.hl-col-xs-12:eq(5)&&Text;.hl-col-xs-12:eq(3)&&Text;.hl-col-xs-12:eq(4)&&Text","content":".hl-content-text&&Text","tabs":".hl-plays-from a","lists":".hl-plays-list:eq(#id) li"},
     搜索:'.hl-list-wrap&&ul&&li;.hl-item-thumb&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
-}
+});
