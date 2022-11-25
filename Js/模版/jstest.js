d699b706e0c148e06ee9c571b132355d4ac2a9b1
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-25 12:27:24 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,5 +1,5 @@
 var rule = {
-     title:'爱看',
+     title:'酷客影院',
     host:'http://www.2kuke.com',
     // homeUrl:'/',
     url:'/list/fyclass_fypage.html',
@@ -12,14 +12,14 @@ var rule = {
         // "Cookie": "searchneed=ok"
     },
     //class_parse:'.stui-header__menu li.hidden-xs;a&&Text;a&&href;/(\\d+)/',
-    class_name:'电影&剧集&综艺&动漫&记录',
-    class_url:'1&2&3&4&4&23',
+    class_name:'电影&剧集&综艺&动漫',
+    class_url:'1&2&4&3',
     play_parse:true,
     lazy:'',
     limit:8,
     推荐:'.stui-vodlist.clearfix;.stui-vodlist__box;a&&title;a&&data-original;.pic-text&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.stui-vodlist.clearfix li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-    二级:{"title":"h1.title&&Text;.stui-content__detail p:eq(0)&&Text","img":".lazyload&&data-original","content":".stui-content__detail p:eq(4)&&Text","tabs":".nav.nav-tabs.pull-right&&li","lists":".stui-content__playlist:eq(#id) li"},
+    二级:{"title":"h1.title&&Text;.stui-content__detail p:eq(0)&&Text","img":".lazyload&&data-original","content":";;;.stui-content__detail p:eq(2)&&Text;.stui-content__detail p:eq(3)&&Text","tabs":".nav.nav-tabs.pull-right&&li","lists":".stui-content__playlist:eq(#id) li"},
     
 }
