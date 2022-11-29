58f1b2e1baa2a6b26fd1f0255b87f67228ea8900
heroaku <108630267+heroaku@users.noreply.github.com>
2022-11-29 14:18:14 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -9,8 +9,8 @@ var rule={
     headers:{
         'User-Agent':'UC_UA',
     },
-    class_name:'电影&电视剧&综艺&动漫&纪录片&国产剧&港台剧&日韩剧&欧美剧&其他剧',
-    class_url:'1&2&3&4&16&20&21&22&23&24',
+    class_name:'电影&电视剧&番剧&动漫&国产剧&港台剧&日韩剧&欧美剧',
+    class_url:'1&2&20&4&13&14&15&16',
 
     play_parse:true,
     lazy:'',
@@ -18,6 +18,6 @@ var rule={
     推荐:'ul.fed-list-info.fed-part-rows;li;a.fed-list-title&&Text;a&&data-original;.fed-list-remarks&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.fed-list-info&&li;a.fed-list-title&&Text;a&&data-original;.fed-list-remarks&&Text;a&&href',
-    二级:{"title":"h1&&Text;.fed-mute&&Text","img":".fed-list-info&&a&&data-original","desc":".fed-deta-content&&.fed-part-rows&&li:eq(-1)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(-2)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(-3)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(0)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(1)&&Text","content":".fed-conv-text&&Text","tabs":".fed-drop-boxs&&.fed-part-rows li","lists":".fed-play-item:eq(#id)&&.fed-part-rows:eq(1) li"},
+    二级:{"title":"h1&&Text;.fed-mute&&Text","img":".fed-list-info&&a&&data-original","desc":".fed-deta-content&&.fed-part-rows&&li:eq(-1)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(-2)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(-3)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(0)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(1)&&Text","content":".fed-deta-content&&.fed-part-rows&&li:eq(6)&&Text","tabs":".fed-drop-boxs&&.fed-part-rows li","lists":".fed-play-item:eq(#id)&&.fed-part-rows:eq(1) li"},
     搜索:'.fed-list-deta;h1&&Text;.fed-lazy&&data-original;.fed-list-remarks&&Text;a&&href;.fed-deta-content&&Text',
 }
