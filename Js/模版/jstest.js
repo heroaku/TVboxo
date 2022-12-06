78f367b849cdcbc346599864de72c2203fb68688
heroaku <108630267+heroaku@users.noreply.github.com>
2022-12-06 15:16:33 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,24 +1,23 @@
-var rule = {
-    title:'看一看影视',
-    host:'https://www.kanyk.cc',
-    // homeUrl:'/',
-    url:'/index.php?m=vod-list-id-fyclass-pg-fypage-order--by--class--year--letter--area--lang-.html',
-    searchUrl:'/index.php?m=vod-search-pg-fypage-wd-**.html',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-        'User-Agent':'MOBILE_UA',
-        // "Cookie": "searchneed=ok"
-    },
-    class_name:'电影&电视剧&综艺&动漫&纪录片&国产剧&港台剧&日韩剧&欧美剧&泰剧',
-    class_url:'1&2&3&4&16&12&13&14&15&19',
-    play_parse:true,
-    lazy:'',
-    limit:6,
-    推荐:'.index-area.clearfix;ul&&li;a&&title;img&&data-original;.other&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'.index-area.clearfix&&ul&&li;a&&title;img&&data-original;.other&&Text;a&&href',
-    二级:{"title":".ek.title&&a:eq(2)&&Text;.module-info-tag-link:eq(2)&&Text","img":".ek.ct-l&&img&&data-original","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.ek.ct-c&&dl&&dt:eq(1)&&Text;.ek.ct-c&&dl&&dt:eq(3)&&Text","content":".ek.ct-c&&.ek.ee&&Text","tabs":".ek.playfrom&&li","lists":".ek.playlist:eq(#id)&&.ek.videourl li"},
-    搜索:'.index-area.clearfix&&ul&&li;*;*;*;*',
-}
+var rule = { // 模板字典
+            title: 'fositv',
+            host: 'https://fositv.com/',
+            // homeUrl:'/',
+            url: '/vod_____type/fyclass-fypage.html',
+            searchUrl: '/vod_____search/**----------fypage---.html',
+            searchable: 2,//是否启用全局搜索,
+            quickSearch: 0,//是否启用快速搜索,
+            filterable: 0,//是否启用分类筛选,
+            headers: {//网站的请求头,完整支持所有的,常带ua和cookies
+                'User-Agent': 'MOBILE_UA',
+                // "Cookie": "searchneed=ok"
+            },
+            class_parse: '.dropdown-box&&ul&&li;a&&Text;a&&href;/(\\d+).html',
+//cate_exclude:'电影|电视剧',//分类过滤
+            play_parse: true,//播放是否解析
+            lazy: '',//是否免嗅探
+            limit: 6,
+            推荐:'ul.myui-vodlist;a&&title;.lazyload&&data-original;;a&&href',
+            double: true, // 推荐内容是否双层定位
+            一级: 'ul.myui-vodlist;a&&title;.lazyload&&data-original;;a&&href',
+            二级: {"title":"h3&&a&&Text","img":".lazyload&&data-original","desc":".col-pd&&p:eq(1)&&Text","content":".data&&Text","tabs":"ul.nav-tabs&&li","lists":".myui-content__list:eq(#id) a"},
+        }
