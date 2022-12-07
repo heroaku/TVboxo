aa08a3dc70dd63975a46f2a7a2ee41dbdd2fb494
heroaku <108630267+heroaku@users.noreply.github.com>
2022-12-07 14:48:52 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,9 +1,9 @@
 var rule = { 
     title: 'fositv',
-    host: 'https://fositv.com/',
+    host: 'https://www.citydy.com',
     // homeUrl:'/',
-    url: '/vod_____type/fyclass-fypage.html',
-    searchUrl: '/vod_____search/**----------fypage---.html',
+    url: '/show/id-fyclass/page/fypage.html',
+    searchUrl: '/search/page/fypage/wd/**.html',
     class_name:'电影&电视剧&综艺&动漫&纪录片',
     class_url:'1&2&3&4&5',
     searchable: 2,//是否启用全局搜索,
@@ -16,9 +16,9 @@ var rule = {
     play_parse: true,//播放是否解析
     lazy: '',//是否免嗅探
     limit: 6,
-    推荐:'ul.myui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
+    推荐:'ul.img-list;li;a&&title;.lazyload&&data-original;.text-right&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:'.myui-vodlist li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
+    一级:'.img-list li;a&&title;.lazyload&&data-original;.text-right&&Text;a&&href',
     二级:{"title":".myui-content__detail .title&&Text;.t-muted:eq(-1)&&Text","img":".lazyload&&data-original","desc":";;.myui-player__data&&p.text-muted&&Text;.col-pd.text-collapse p:eq(1)&&Text;.col-pd.text-collapse p:eq(0)&&Text","content":".sketch.content&&Text","tabs":".nav-tabs:eq(0) li","lists":".myui-content__list:eq(#id) li"},
     搜索:'.myui-vodlist__media li;a&&title;*;*;a&&href;.text-muted:eq(-1)&&Text',
         }
