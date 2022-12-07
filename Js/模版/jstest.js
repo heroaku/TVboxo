e795a6cea8a847dbcb45da36d0df4e977ca0f370
heroaku <108630267+heroaku@users.noreply.github.com>
2022-12-07 16:17:21 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,9 +1,9 @@
 var rule = { 
-    title: '城市影视',
-    host: 'https://www.citydy.com',
+    title: '23影视',
+    host: 'https://222.186.150.32',
     // homeUrl:'/',
-    url: '/show/id-fyclass/page/fypage.html',
-    searchUrl: '/search/page/fypage/wd/**.html',
+    url: '/vodshow/id/fyclass/page/fypage.html',
+    searchUrl: '/vodsearch/page/fypage/wd/**.html',
     class_name:'电影&电视剧&综艺&动漫&纪录片',
     class_url:'1&2&3&4&5',
     searchable: 2,//是否启用全局搜索,
@@ -16,9 +16,9 @@ var rule = {
     play_parse: true,//播放是否解析
     lazy: '',//是否免嗅探
     limit: 6,
-    推荐:'ul.img-list;li;a&&title;.lazyload&&data-original;.text-right&&Text;a&&href',
+    推荐:'ul;li.leo-video-item;a&&title;img&&data-original;.leo-video-remark&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:'.img-list li;a&&title;.lazyload&&data-original;.text-right&&Text;a&&href',
+    一级:'li.leo-video-item;a&&title;img&&data-original;.leo-video-remark&&Text;a&&href',
     二级:{"title":"h1&&Text;.t-muted:eq(-1)&&Text","img":".lazyload&&data-original","desc":";;.vod-detail-info&&ul&&li:eq(2)&&Text;.vod-detail-info&&ul&&li:eq(3)&&Text;.vod-detail-info&&ul&&li:eq(4)&&Text","content":".vod-detail-info&&ul&&li:eq(10)&&Text","tabs":".details-play-nav li","lists":".play-list.fade-in:eq(#id) li"},
-    搜索:'.img-list li;a&&title;*;.title.text-center&&Text;a&&href;.text-muted:eq(-1)&&Text',
+    搜索:'li.leo-video-item;a&&title;*;*;a&&href;.text-muted:eq(-1)&&Text',
         }
