3a8776fda2128f9bd36615d65100a88cbfeec117
heroaku <108630267+heroaku@users.noreply.github.com>
2022-12-08 14:25:08 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,24 +1,18 @@
-var rule = { 
-    title: '23影视',
-    host: 'https://222.186.150.32',
-    // homeUrl:'/',
-    url: '/vodshow/id/fyclass/page/fypage.html',
-    searchUrl: '/vodsearch/page/fypage/wd/**.html',
-    class_name:'电影&电视剧&综艺&动漫&纪录片',
-    class_url:'1&2&3&4&5',
-    searchable: 2,//是否启用全局搜索,
-    quickSearch: 0,//是否启用快速搜索,
-    filterable: 0,//是否启用分类筛选,
-    headers: {//网站的请求头,完整支持所有的,常带ua和cookies
-                'User-Agent': 'MOBILE_UA',
-                // "Cookie": "searchneed=ok"
-            },
-    play_parse: true,//播放是否解析
-    lazy: '',//是否免嗅探
-    limit: 6,
-    推荐:'ul;li.leo-video-item;a&&title;img&&data-original;.leo-video-remark&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'li.leo-video-item;a&&title;img&&data-original;.leo-video-remark&&Text;a&&href',
-    二级:{"title":"h1&&Text;.t-muted:eq(-1)&&Text","img":".lazyload&&data-original","desc":";;.vod-detail-info&&ul&&li:eq(2)&&Text;.vod-detail-info&&ul&&li:eq(3)&&Text;.vod-detail-info&&ul&&li:eq(4)&&Text","content":".vod-detail-info&&ul&&li:eq(10)&&Text","tabs":".play_source_tab&&.swiper-wrapper a","lists":".play_list_box:eq(#id)&&.playlist_notfull li"},
-    搜索:'li.leo-video-item;a&&title;*;*;a&&href;.text-muted:eq(-1)&&Text',
-        }
+var rule={     
+    title:'YOKE影视',
+    host:'https://qnyoke.top',
+    url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+    searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    //class_parse:'.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
+    //cate_exclude:'演员',
+    class_name:'电影&电视剧&综艺&动漫&哔站&国产剧&港台剧&日韩剧&欧美剧&直播',
+    class_url:'1&2&3&4&5&13&14&15&16&27',  
+    推荐:'.module-poster-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+    一级:'.module-poster-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+    //二级:{"title":"h1&&Text;","img":".lazyload&&data-original","desc":";;.module-info-item:eq(4) p&&Text;.module-info-item:eq(3) div&&Text;.module-info-item:eq(1) a&&Text","content":".show-desc&&Text","tabs":'#y-playList&&span',"lists":".module-play-list:eq(#id) a"},     
+    二级:{"title":"h1&&Text;.module-info-tag-link:eq(2)&&Text","img":".lazyload&&data-original","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-content&&.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction-content&&Text","tabs":".module-tab-items-box:eq(0)&&.module-tab-item","lists":".module-list:eq(#id)&&.module-play-list-content a"}, 
+    搜索:'.module-card-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href', 
+}
