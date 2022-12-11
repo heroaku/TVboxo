c4bca4af43a2a18e9e4e8c3e302b315d2e07ecf9
heroaku <108630267+heroaku@users.noreply.github.com>
2022-12-11 16:44:08 -0700

    Update 待完成模版.js

--- Js/模版/待完成模版.js
+++ Js/模版/待完成模版.js
@@ -9,6 +9,28 @@
 // Pluto Player官方TG https://t.me/PlutoPlayerChannel
 
 
+
+var rule={
+title:'1985影视',
+host:'https://www.1985.one',
+url:'/vodshow/id/fyclass/page/fyclass.html',
+searchUrl:'/vodsearch/page/fyclass/wd/**.html',
+searchable:2,
+quickSearch:0,
+filterable:0,
+headers:{'User-Agent':'MOBILE_UA', },
+class_name:'电影&电视剧&综艺&动漫&B站',
+class_url:'1&2&3&4&51',
+play_parse:true,
+lazy:'',
+limit:6,
+推荐:'.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+double:true, // 推荐内容是否双层定位
+一级:'.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+二级:{"title":"h1&&Text;.video-info-aux&&div&&a:eq(0)&&Text","img":".module-item-pic&&img&&data-src","desc":";.video-info-aux&&a:eq(1)&&Text;.video-info-aux&&a:eq(2)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(0)&&Text","content":".video-info-content&&Text","tabs":".module-tab-item.tab-item","lists":".module-player-list:eq(#id)&&.scroll-content&&a"},
+搜索:'.module-items .module-search-item;a&&title;img&&data-src;.video-serial&&Text;a&&href',
+}
+
 //资源一般
 var rule={     
     title:'320影视',
