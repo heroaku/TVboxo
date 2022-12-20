c87a18f94058149160bdf821dd3da3d7e613f8cf
heroaku <108630267+heroaku@users.noreply.github.com>
2022-12-20 14:22:24 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,25 +1,27 @@
-var rule={
-    title:'BD影视',
-    host:'https://bd360.xyz',
-    // homeUrl:'/',
-    url:'/vodshow/fyclass--------fypage---.html',
-    searchUrl:'/vodsearch/**----------fypage---.html',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-        'User-Agent':'MOBILE_UA',
-        // "Cookie": "searchneed=ok"
-    },
-    //class_parse:'.navbar-items li:gt(2):lt(8);a&&Text;a&&href;.*/(.*?).html',
-    class_name:'电影&电视剧&综艺&动漫',
-    class_url:'1&2&3&4',
-    play_parse:true,
-    lazy:'',
-    limit:6,
-    推荐:'body .main;.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'a.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-    二级:{"title":"h1&&Text;.module-info-tag&&Text","img":".lazy.lazyload&&data-original","desc":".module-info-item:eq(-1)&&Text;.module-info-item:eq(-2)&&Text;.module-info-item:eq(-3)&&Text;.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction&&Text","tabs":".module-tab-item.tab-item","lists":".module-list:eq(#id)&&.module-play-list a"},
-    搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
+var rule = {
+    title: '时光影视',
+    host: 'http://www.shigys.com',
+    class_name:'电影&电视剧&综艺&动漫&哔哩哔哩&独家',
+    class_url:'1&2&3&4&40&41',
+    homeUrl: '',
+    searchUrl: '/index.php/ajax/suggest?mid=1&wd=**',
+    searchable: 2,
+    quickSearch: 0,
+    headers:{'User-Agent':'MOBILE_UA'},
+    // 分类链接fypage参数支持1个()表达式
+    url: '/index.php/api/vod#type=fyclass&page=fypage',
+    detailUrl:'/index.php/vod/detail/id/fyid.html',
+    推荐:'.list-vod.flex .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
+    一级:'',
+    一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list;',
+    二级:{
+		"title":".slide-info-title&&Text;.slide-info:eq(3)--strong&&Text",
+		"img":".detail-pic&&data-original",
+		"desc":".fraction&&Text;.slide-info-remarks:eq(1)&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text",
+		"content":"#height_limit&&Text",
+		"tabs":".anthology.wow.fadeInUp.animated&&.swiper-wrapper&&a",
+		"tab_text":".swiper-slide&&Text",
+		"lists":".anthology-list-box:eq(#id)&&.anthology-list-play&&li"
+	},
+    搜索:'json:list;name;pic;;id',
 }
