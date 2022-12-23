0fe395b9faa7529942f34117ae1bc44653d4e229
heroaku <108630267+heroaku@users.noreply.github.com>
2022-12-22 17:00:27 -0700

    Update 子子影视.js

--- Js/子子影视.js
+++ Js/子子影视.js
@@ -2,17 +2,25 @@ var rule={
     title:'子子影视',
     host:'https://www.ziziys.com',
     // homeUrl:'/',
-    url:'/list/fyclass/page/fypage.html',
+    //url:'/list/fyclass/page/fypage.html',
     searchUrl:'/vsearch/--.html?wd=**',
     searchable:2,//是否启用全局搜索,
     quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
+    url:'/list/fyfilter/page/fypage.html',
+    filterable:1,//是否启用分类筛选,
+	filter_url:'{{fl.cateId}}',
+	filter: {"1":[{"key":"cateId","name":"类型","value":[{"n":"全部","v":"1"},{"n":"动漫片","v":"23"},{"n":"恐怖片","v":"24"},{"n":"历史传记片","v":"25"},{"n":"战争片","v":"26"},{"n":"情色片","v":"27"},{"n":"武侠古装","v":"28"},{"n":"记录片","v":"29"},{"n":"灾难片","v":"30"},{"n":"音乐歌舞","v":"31"},{"n":"运动片","v":"32"},{"n":"科幻片","v":"33"},{"n":"动作片","v":"6"},{"n":"剧情片","v":"7"},{"n":"喜剧","v":"8"},{"n":"爱情片","v":"9"},{"n":"惊悚片","v":"10"},{"n":"奇幻片","v":"11"},{"n":"悬疑片","v":"12"}]}],"2":[{"key":"cateId","name":"类型","value":[{"n":"全部","v":"2"},{"n":"国产剧","v":"13"},{"n":"欧美剧","v":"14"},{"n":"日韩剧","v":"15"},{"n":"泰剧","v":"16"}]}],"3":[{"key":"cateId","name":"类型","value":[{"n":"全部","v":"3"},{"n":"国漫","v":"20"},{"n":"日漫","v":"21"},{"n":"欧美动漫","v":"22"}]}]},
+	filter_def:{
+		1:{cateId:'1'},
+		2:{cateId:'2'},
+		3:{cateId:'3'}
+	},
     headers:{//网站的请求头,完整支持所有的,常带ua和cookies
         'User-Agent':'MOBILE_UA',
         // "Cookie": "searchneed=ok"
     },
-    class_name:'电影&动漫&国产剧&美剧&日韩剧',
-    class_url:'1&3&13&14&15',
+    class_name:'电影&电视剧&动漫',
+    class_url:'1&2&3',
     lazy:'',
     limit:6,
     double:true, // 推荐内容是否双层定位
