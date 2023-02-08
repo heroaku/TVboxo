bab95e6f681e942640363ba8bf489ba6e3a3c2c1
heroaku <108630267+heroaku@users.noreply.github.com>
2023-02-07 18:00:05 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,25 +1,24 @@
-var rule={
-    title:'秋香影视',
-    host:'https://www.shuanlang.com',
-    // homeUrl:'/',
-    url:'/list/fyclass--------fypage---.html',
-    searchUrl:'/search/**----------fypage---.html',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-        'User-Agent':'MOBILE_UA',
-        // "Cookie": "searchneed=ok"
+muban.首图2.二级.desc = '.data:eq(0)&&Text;;;.data--span:eq(2)&&Text;.data--span:eq(1)&&Text';
+muban.首图2.二级.tabs = '.stui-pannel__head h3';
+var rule = {
+    title:'星辰',
+    模板:'首图2',
+    host:'http://www.40yb.com',
+    // url:'/fyclass/indexfypage.html[/fyclass/index.html]',
+    url:'/fyfilter/indexfypage.html[/fyfilter/index.html]',
+    filterable:1,//是否启用分类筛选,
+    filter_url:'{{fl.cateId}}',
+    filter:{
+        "dianying":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"dianying"},{"n":"动作片","v":"dongzuopian"},{"n":"爱情片","v":"aiqingpian"},{"n":"科幻片","v":"kehuanpian"},{"n":"恐怖片","v":"kongbupian"},{"n":"喜剧片","v":"xijupian"},{"n":"剧情片","v":"juqingpian"}]}],
+        "dianshiju":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"dianshiju"},{"n":"国产剧","v":"guochanju"},{"n":"港台剧","v":"tangtaiju"},{"n":"欧美剧","v":"oumeiju"},{"n":"日韩剧","v":"rihanju"}]}]
     },
-    //class_parse:'.navbar-items li:gt(2):lt(8);a&&Text;a&&href;.*/(.*?).html',
-    class_name:'电影&电视剧&综艺&动漫',
-    class_url:'1&2&3&4',
-    play_parse:true,
-    lazy:'',
-    limit:6,
-    推荐:'body .main;.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'a.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-    二级:{"title":"h1&&Text;.module-info-tag&&Text","img":".lazy.lazyload&&data-original","desc":".module-info-item:eq(-1)&&Text;.module-info-item:eq(-2)&&Text;.module-info-item:eq(-3)&&Text;.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction&&Text","tabs":".module-tab-item.tab-item","lists":".module-list:eq(#id)&&.module-play-list a"},
-    搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
+    filter_def:{
+        dianying:{cateId:'dianying'},
+        dianshiju:{cateId:'dianshiju'},
+        zongyi:{cateId:'zongyi'},
+        dongman:{cateId:'dongman'}
+    },
+    // searchUrl:'/search.php?page=fypage&searchword=**&searchtype=',
+    searchUrl:'/search.php#searchword=**;post',
+    class_parse:'.stui-header__menu li:gt(0):lt(7);a&&Text;a&&href;.*/(.*?)/.*html',
 }
