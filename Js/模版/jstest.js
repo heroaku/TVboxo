add2d6f960198bff96b72e7f25b0991b5da58907
heroaku <108630267+heroaku@users.noreply.github.com>
2023-03-24 14:58:06 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -27,16 +27,16 @@ var rule={
     limit:6,
     double:true, // 推荐内容是否双层定位
     // 推荐:'.module-item;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
-    推荐:'.module-items:eq(0);.module-item;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
-    一级:'.module-item;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
-    二级:{
-    	"title":"h1&&Text;.video-info-header&&Text",
-    	"img":".lazyload&&data-src",
-    	// "desc":".video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(3)&&Text",
-    	"desc":";;;.video-info-main .video-info-actor:eq(1)&&Text;.video-info-main .video-info-actor:eq(0)&&Text",
-    	"content":".vod_content&&Text",
-    	"tabs":".module-tab-item",
-    	"lists":".sort-item:eq(#id) a"
-	},
-    搜索:'.module-items;.lazyload&&alt;.lazyload&&data-src;.video-serial&&Text;*',
+            推荐:'.list-a;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
+            一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list.map(function(it){it.vod_pic=urljoin2(input.split("/i")[0],it.vod_pic);return it});',
+            二级:{
+                "title":".slide-info-title&&Text;.slide-info:eq(3)--strong&&Text",
+                "img":".detail-pic&&data-original",
+                "desc":".fraction&&Text;.slide-info-remarks:eq(1)&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text",
+                "content":"#height_limit&&Text",
+                "tabs":".anthology.wow.fadeInUp.animated&&.swiper-wrapper&&a",
+                "tab_text":".swiper-slide&&Text",
+                "lists":".anthology-list-box:eq(#id) li"
+            },
+            搜索:'json:list;name;pic;;id',
 }
