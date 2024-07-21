fed7f69030bd7f3fec07079b36ea112cf0ca7051
heroaku <108630267+heroaku@users.noreply.github.com>
2024-07-21 14:04:01 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,34 +1,73 @@
 var rule = {
-    title: '大豆',
+    title: '花子动漫[漫]',
     host: 'https://www.heimaotv.vip',
     class_name:'电影&电视剧&综艺&动漫',
     class_url:'31&49&66&4',
-    searchUrl: '/index.php/ajax/suggest?mid=1&wd=**',
+    searchUrl: '/index.php/ajax/suggest?mid=1&wd=**&limit=50',
+    searchUrl: '/vodsearch/**----------fypage---.html',
     searchable: 2,
     quickSearch: 0,
-    headers:{'User-Agent':'MOBILE_UA'},
-    url: '/vod/show/id/fyclass.html',
-	//url: '/index.php/api/vod#type=fyfilter&page=fypage',
-	filterable:1,//是否启用分类筛选,
-	filter_url:'{{fl.cateId}}',
-	filter: {"1":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"动作","v":"6"},{"n":"喜剧","v":"7"},{"n":"爱情","v":"8"},{"n":"动画","v":"9"},{"n":"纪录","v":"10"},{"n":"科幻","v":"11"},{"n":"剧情","v":"30"},{"n":"悬疑","v":"31"},{"n":"惊悚","v":"32"},{"n":"恐怖","v":"33"},{"n":"犯罪","v":"34"},{"n":"谍战","v":"35"},{"n":"冒险","v":"36"},{"n":"奇幻","v":"37"},{"n":"灾难","v":"38"},{"n":"战争","v":"39"},{"n":"歌舞","v":"40"},{"n":"历史","v":"41"},{"n":"其他","v":"42"}]}],"2":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"日韩","v":"16"},{"n":"大陆","v":"13"},{"n":"欧美","v":"14"},{"n":"港台","v":"15"},{"n":"动画","v":"21"},{"n":"其他","v":"20"},{"n":"记录","v":"22"}]}],"3":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"3"},{"n":"华语","v":"23"},{"n":"日韩","v":"24"},{"n":"欧美","v":"25"},{"n":"其他","v":"26"}]}],"4":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"4"},{"n":"国漫","v":"27"},{"n":"日漫","v":"28"},{"n":"其他","v":"29"}]}]},
-	filter_def:{
-		1:{cateId:'1'},
-		2:{cateId:'2'},
-		3:{cateId:'3'},
-		4:{cateId:'4'}
-	},
-    detailUrl:'/index.php/vod/detail/id/fyid.html',
-    推荐:'.list-vod.flex .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
-    一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list.map(function(it){it.vod_pic=urljoin2(input.split("/i")[0],it.vod_pic);return it});',
-    二级:{
-		"title":".slide-info-title&&Text;.slide-info:eq(3)--strong&&Text",
-		"img":".detail-pic&&data-original",
-		"desc":".fraction&&Text;.slide-info-remarks:eq(1)&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text",
-		"content":"#height_limit&&Text",
-		"tabs":".anthology.wow.fadeInUp.animated&&.swiper-wrapper&&a",
-		"tab_text":".swiper-slide&&Text",
-		"lists":".anthology-list-box:eq(#id) li"
-	},
-    搜索:'json:list;name;pic;;id',
+    headers: {
+        'User-Agent': 'MOBILE_UA',
+    },
+    url: '/index.php/api/vod#type=fyclassfyfilter&page=fypage',
+    filterable: 0,
+    filter_url: '&class={{fl.class}}&year={{fl.year}}&letter={{fl.letter}}&by={{fl.by}}',
+    filter: 'H4sIAAAAAAAAA+2W204TURSG32WuMdnT0nbDnZzP57PhopImEhETWk0IIUFOloNQDLSigGJCLCIWhJCWgr4MM9O+hdPO2mstDzE10cSYudv/989u55t0uteUpmuVd6a0+6FJrVIbGQuGw1qZNh58ELKjdZo19lbs/Dg49ihUvG7cxsZCMj+XLGA7aNNlQK9mb9Jxa2sVCsrqCmv7sxGLQg0Bd8fWjI9HaqsTcN/ccW5/Ru1zAnaXKSt6qjonYHf4JXexrDon4PcdPDUyWfV9TlCdGX1hxhPQQSjlPs3YnvVhQ+1zAnaJAyOdVp0T8D7fbdC9QMB9n7LmvHpmELB7s2+8OladE7B7skLPBQI6LD7Pbx8qByfgvrWkFVtU+5yA3c6l/TRU5wTV5V+f3WRi0EFQXS62m1tbgg4C3WfMnInjfRYDdnPL5uxL1TkBu7P9/O5ba+u9qjHjFem0GV03NzPqCsz4HE7Wc6lzuoIyfsbVkbWZZZ+BGd0WlHRhNT1c4M67NBkKTtCrZGTOb7LXJb5KHuEpB1ZcMu4l7uXcQ9zDuU5c51wQF4zrFcjtJeOSuOQ8QDzAuZ+4n3MfcR/n5KtzX518de6rk6/OfXXy1bmvTr469xXkK7ivIF/BfQX5Cu4ryFdwX0G+gvsK8hXcV5Cv4L6CfAX3FeQruK8gX0G+ekWF8i0uGZfEJecB4gHO/cT9nPuI+zgvJ17OuZe4l3MPcQ/nOnGdc0Gc+0ryldxXkq/kvpJ8JfeV5Cu5ryRfyX0l+UruK8lXcl9JvpL7SvKV3FeSr73kfztjoUgkxP94jhNm6lmJfzy3AdxGUgWkCkk1kGokNUBqkNQCqUVSB6QOST2QeiQNQBqQNAJpRNIEpAlJM5BmJC1AWpC0AmlF0gakDUk7kHYkHUA6kHQC6UTSBaQLSTeQbiQ9QHqQ9ALpRdIHpA9JP5B+JANABpAMAhlEMgRkCIm4pd6Bwor/VO5O0s/ETFzkE+c//EzM1SVzZ8aMn8BHREbtq/F0LJb2QAblvdFImJe51LwRVbNEeOThRKjw9cNlmuefGDl/OVb+hdHxvxgPf3cEdAcidyByByK1dAcidyByByJ3IPp+IPL+qYFoOXlzvaOObieUMrb8dMRwj2736HaPbrV0j2736HaPbvfo/vbonv4K+TrlXkAbAAA=',
+    filter_def: {},
+    detailUrl: '/voddetail/fyid.html',
+    play_parse: true,
+    sniffer: 1,
+    is_video: 'obj/tos|bd.xhscdn|/ugc/',
+    lazy: $js.toString(() => {
+        input = {
+            parse: 1,
+            url: input,
+            //js:'try{let urls=Array.from(document.querySelectorAll("iframe")).filter(x=>x.src.includes("?url="));if(urls){location.href=urls[0].src}}catch{}document.querySelector("button").click()',
+            js: 'try{location.href=document.querySelector("#playleft iframe").src}catch{}document.querySelector("button.swal-button--confirm").click()',
+            parse_extra: '&is_pc=1&custom_regex=' + rule.is_video,
+        }
+    }),
+    limit: 6,
+    推荐: '.list-vod.flex .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
+    一级: $js.toString(() => {
+        let body = input.split("#")[1];
+        let t = Math.round(new Date / 1e3).toString();
+        let key = md5("DS" + t + "DCC147D11943AF75");
+        let url = input.split("#")[0];
+        body = body + "&time=" + t + "&key=" + key;
+        print(body);
+        fetch_params.body = body;
+        let html = post(url, fetch_params);
+        let data = JSON.parse(html);
+        VODS = data.list.map(function (it) {
+            it.vod_pic = urljoin2(input.split("/i")[0], it.vod_pic);
+            return it
+        });
+    }),
+    二级: {
+        title: '.slide-info-title&&Text;.slide-info:eq(3)--strong&&Text',
+        img: '.detail-pic&&data-original',
+        desc: '.fraction&&Text;.slide-info-remarks:eq(1)&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text',
+        content: '#height_limit&&Text',
+        tabs: '.anthology.wow.fadeInUp.animated&&.swiper-wrapper&&a',
+        tab_text: '.swiper-slide&&Text',
+        lists: '.anthology-list-box:eq(#id) li',
+    },
+    搜索: 'json:list;name;pic;;id',
+    搜索: $js.toString(() => {
+        let html = fetch(input);
+        let list = pdfa(html, ".public-list-box");
+        VODS = list.map(x => {
+            return {
+                vod_name: pdfh(x, ".thumb-txt&&Text"),
+                vod_pic: pdfh(x, ".lazy&&data-src"),
+                vod_remarks: pdfh(x, ".public-list-prb&&Text"),
+                vod_content: pdfh(x, ".thumb-blurb&&Text"),
+                vod_id: pdfh(x, "a&&href")
+            }
+        });
+    }),
+    图片替换: '&amp;=>&'
 }
+
