a11566ac6593ff3eee165cb7097e0c738b156af5
heroaku <108630267+heroaku@users.noreply.github.com>
2024-07-20 19:50:19 -0600

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,19 +1,28 @@
-var rule={     
-    title:'菠萝小站',
-    host:'https://www.boluoxiaozhan.com/',
+var rule= {
+    title:'369影视',
+    host:'https://kuyatv.app',
+    // url:'/index.php/vod/show/id/fyclass/page/fypage.html',
     url:'/vodshow/fyclass/page/fypage.html',
-    searchUrl:'vodsearch.html?wd=**&submit=',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    //class_parse:'.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
-    //cate_exclude:'演员',
-    class_name:'电视剧&电影&动漫&综艺',
-    class_url:'fuju&bldianying&fuman&blzongyi',  
-    推荐:'body&&.hl-list-wrap;ul&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'.hl-vod-list&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
-    二级:{"title":".hl-dc-title&&Text;.hl-col-xs-12&&em&&.hl-text-muted:eq(-2)&&Text","img":".hl-lazy&&data-original","desc":";;.hl-col-xs-12:eq(5)&&Text;.hl-col-xs-12:eq(3)&&Text;.hl-col-xs-12:eq(4)&&Text","content":".hl-content-text&&Text","tabs":".hl-plays-from a","lists":".hl-plays-list:eq(#id) li"},
-    搜索:'.hl-list-wrap&&ul&&li;.hl-item-thumb&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+    searchable: 2,//是否启用全局搜索,
+    quickSearch: 0,//是否启用快速搜索,
+    filterable:1,//是否启用分类筛选,
+    class_parse: '.stui-header__menu&&li:gt(0):lt(4);a&&Text;a&&href;.*/(.*?).html',
+    play_parse: true,
+    lazy:'js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=="1"){url=unescape(url)}else if(html.encrypt=="2"){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}',
+    limit: 6,
+    searchUrl:'/search/page/fypage/wd/**.html',
+    detailUrl:'/detail/fyid.html', //非必填,二级详情拼接链接
+    推荐: '.stui-pannel:eq(2);.stui-vodlist&&li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
+    double: true, // 推荐内容是否双层定位
+    一级: '.stui-vodlist li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
+    二级: {
+        "title": "h1&&Text;.data&&Text",
+        "img": "img&&data-original",
+        "desc": ';;;.video-info-actor:eq(1)&&Text;.video-info-actor:eq(0)&&Text',
+        "content": ".stui-pannel:eq(2)&&p&&Text",
+        "tabs": "h3:eq(0)",
+        "lists": ".stui-content__playlist&&li"
+    },
+    搜索:'.stui-vodlist__media li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
 }
 
