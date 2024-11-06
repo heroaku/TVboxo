4ad78c92574ff340e5b44b03945653a0c8aa9725
heroaku <108630267+heroaku@users.noreply.github.com>
2024-11-06 12:25:28 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,18 +1,24 @@
-var rule={     
-    title:'游子视频',
-    host:'https://www.youzisp.tv',
-    url:'/vodshow/fyclass--------fypage---.html',
-    searchUrl:'/vodsearch/**----------fypage---.html',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    //class_parse:'.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
-    //cate_exclude:'演员',
-    class_name:'电影&电视剧&综艺&动漫&纪录片&国产剧&日韩剧&港台剧&欧美剧&东南亚&热门短剧&同影',
-    class_url:'dianying&dianshiju&zongyi&dongman&jilupian&guochan&rihan&gangtai&oumei&dongnanya&remenduanju&tongxing',  
-    推荐:'body&&.hl-list-wrap;ul&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'.hl-vod-list&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
-    二级:{"title":".hl-dc-title&&Text;.hl-col-xs-12&&em&&.hl-text-muted:eq(-2)&&Text","img":".hl-lazy&&data-original","desc":";;.hl-col-xs-12:eq(5)&&Text;.hl-col-xs-12:eq(3)&&Text;.hl-col-xs-12:eq(4)&&Text","content":".hl-content-text&&Text","tabs":".hl-plays-from a","lists":".hl-plays-list:eq(#id) li"},
-    搜索:'.hl-list-wrap&&ul&&li;.hl-item-thumb&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+var rule={
+title:'TVA云播',
+host:'https://www.fujutv.net',
+    //https://www.fujuyy.com/sptype/20-2.html
+    //https://www.fujutv.net/spshow/fyclass--------fypage---.html
+    //https://www.fujutv.net/spsearch/**----------fypage---.html
+url:'/spshow/fyclass--------fypage---.html',
+searchUrl:'/spsearch/**----------fypage---.html',
+searchable:2,//是否启用全局搜索,
+quickSearch:0,//是否启用快速搜索,
+filterable:0,//是否启用分类筛选,
+class_name:'电影&电视剧&综艺&动漫',
+class_url:'21&20&23&22',
+play_parse:true,
+lazy:'',
+limit:6,
+// 推荐:'ul.myui-vodlist;ul li;*;*;*;*',
+推荐:'ul.myui-vodlist;li;*;*;*;*',
+// 推荐:'ul.myui-vodlist;ul&&li;*;*;*;*', // 此写法本地js无效
+double:true, // 推荐内容是否双层定位
+一级:'.myui-vodlist__box;a&&title;.lazyload&&data-original;.tag&&Text;a&&href',
+二级:{"title":"h1&&Text;.data:eq(0) a:eq(0)&&Text","img":".lazyload&&data-original","desc":";.data:eq(0) a:eq(2)&&Text;.data:eq(0) a:eq(1)&&Text;.data:eq(2)&&Text;.data:eq(3)&&Text","content":".text-collapse span&&Text","tabs":".myui-panel__head h3","lists":".myui-content__list:eq(#id) li"},
+搜索:'ul.myui-vodlist__media li;*;*;*;*',
 }
