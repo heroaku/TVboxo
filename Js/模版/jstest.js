e1cd5885fcb89ac141f8022541a835c534febb93
heroaku <108630267+heroaku@users.noreply.github.com>
2024-11-06 12:32:48 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -18,7 +18,7 @@ limit:6,
 推荐:'ul.myui-vodlist;li;*;*;*;*',
 // 推荐:'ul.myui-vodlist;ul&&li;*;*;*;*', // 此写法本地js无效
 double:true, // 推荐内容是否双层定位
-一级:'.myui-vodlist__box;a&&title;.lazyload&&data-original;.tag&&Text;a&&href',
+一级:'.myui-vodlist__box;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
 二级:{"title":"h1&&Text;.data:eq(0) a:eq(0)&&Text","img":".lazyload&&data-original","desc":";.data:eq(0) a:eq(2)&&Text;.data:eq(0) a:eq(1)&&Text;.data:eq(2)&&Text;.data:eq(3)&&Text","content":".text-collapse span&&Text","tabs":".myui-panel__head h3","lists":".myui-content__list:eq(#id) li"},
 搜索:'ul.myui-vodlist__media li;*;*;*;*',
 }
