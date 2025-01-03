5b5eb6c9ef2db89ff016027025f366371f6a4b13
heroaku <108630267+heroaku@users.noreply.github.com>
2025-01-03 15:25:44 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,64 +1,27 @@
-muban.短视2.二级.title = '.slide-info-title&&Text;.hl-ma0&&Text';
-muban.短视2.二级.img = '.detail-pic&&img&&data-src';
-muban.短视2.二级.desc = '.fraction&&Text;;;.slide-info:eq(3)--strong&&Text;.slide-info:eq(2)--strong&&Text';
 var rule={
-    title:'NY影院',
-    模板:'短视2',
-    host:'https://www.nycvod.com',
-    homeUrl:'/index.php/label/rb.html',
-    url: '/index.php/api/vod#type=fyfilter&page=fypage',
-    detailUrl:'/voddetail/fyid.html',
-    filterable:1,//是否启用分类筛选,
-    filter_url:'{{fl.cateId}}',
-    filter:{
-        "1":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"动作片","v":"9"},{"n":"喜剧片","v":"10"},{"n":"奇幻片","v":"11"},{"n":"科幻片","v":"12"},{"n":"恐怖片","v":"22"},{"n":"惊悚片","v":"27"},{"n":"悬疑片","v":"23"},{"n":"爱情片","v":"21"},{"n":"战争片","v":"20"},{"n":"剧情片","v":"6"},{"n":"网络电影","v":"24"},{"n":"犯罪片","v":"26"},{"n":"伦理片","v":"8"},{"n":"灾难片","v":"7"},{"n":"动画电影","v":"25"},{"n":"冒险片","v":"28"},{"n":"同性片","v":"44"},{"n":"纪录片","v":"29"},{"n":"歌舞片","v":"45"},{"n":"经典片","v":"46"}]}],
-        "2":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"国产剧","v":"30"},{"n":"欧美剧","v":"47"},{"n":"日韩剧","v":"18"},{"n":"港台剧","v":"16"},{"n":"新马泰剧","v":"14"},{"n":"其他剧","v":"13"}]}],
-        "3":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"3"},{"n":"国产综艺","v":"31"},{"n":"日韩综艺","v":"32"},{"n":"欧美综艺","v":"34"},{"n":"港台综艺","v":"35"},{"n":"其他综艺","v":"37"}]}],
-        "4":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"4"},{"n":"日韩动漫","v":"38"},{"n":"国产动漫","v":"39"},{"n":"欧美动漫","v":"40"},{"n":"港台动漫","v":"42"},{"n":"其他动漫","v":"43"}]}]
-    },
-    filter_def:{
-        1:{cateId:'1'},
-        2:{cateId:'2'},
-        3:{cateId:'3'},
-        4:{cateId:'4'},
-        5:{cateId:'5'}
-    },
-    // class_name:'电影&电视剧&综艺&动漫&竞技体育',
-    // class_url:'1&2&3&4&5',
-    class_parse:'.swiper-wrapper&&li.swiper-slide;a--em&&Text;a&&href;/(\\d+).html',
-	class_name:'',
-    class_url:'',
-    play_parse:true,
-    lazy:`js:
-        var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
-        var url = html.url;
-        if (html.encrypt == '1') {
-            url = unescape(url)
-        } else if (html.encrypt == '2') {
-            url = unescape(base64Decode(url))
-        }
-        if (/m3u8|mp4/.test(url)) {
-            input = url
-        } else {
-            input
-        }
-    `,
-    推荐:'.list-vod .public-list-box;a&&title;.lazy&&data-src;.public-list-prb&&Text;a&&href',
-    double: false, // 推荐内容是否双层定位
-    一级:`js:
-        let body = input.split("#")[1];
-        let t = Math.round(new Date / 1e3).toString();
-        let key = md5("DS" + t + "DCC147D11943AF75");
-        let url = input.split("#")[0];
-        body = body + "&time=" + t + "&key=" + key;
-        print(body);
-        fetch_params.body = body;
-        let html = post(url, fetch_params);
-        let data = JSON.parse(html);
-        VODS = data.list.map(function(it) {
-            it.vod_pic = urljoin2(input.split("/i")[0], it.vod_pic);
-            return it
-        });
-    `,
-
+		title:'NY影院',
+		host:'https://www.nycvod.com',
+		url:'/vodshow/fyclass-fyfilter-----fypage---{{fl.year}}.html',
+ 		searchUrl:'/vodsearch/**----------fypage---.html',
+		searchable:2,//是否启用全局搜索,
+		quickSearch:0,//是否启用快速搜索,
+		filterable:1,//是否启用分类筛选,
+		cate_exclude:'电影|电视剧|综艺|动漫|竞技体育',
+		filter:{
+	    '1':[{'key':'cyass','name':'剧情','value':[{'n':'全部','v':''},{'n':'伦理','v':'伦理'}]},{'key':'area','name':'地区','value':[{'n':'全部','v':''},{'n':'大陆','v':'大陆'},{'n':'香港','v':'香港'},{'n':'台湾','v':'台湾'},{'n':'美国','v':'美国'},{'n':'法国','v':'法国'},{'n':'英国','v':'英国'},{'n':'日本','v':'日本'},{'n':'韩国','v':'韩国'},{'n':'德国','v':'德国'},{'n':'泰国','v':'泰国'},{'n':'印度','v':'印度'},{'n':'意大利','v':'意大利'},{'n':'西班牙','v':'西班牙'},{'n':'加拿大','v':'加拿大'},{'n':'其他','v':'其他'}]},{'key':'year','name':'年代','value':[{'n':'全部','v':''},{'n':'2024','v':'2024'},{'n':'2023','v':'2023'},{'n':'2022','v':'2022'},{'n':'2021','v':'2021'},{'n':'2020','v':'2020'},{'n':'2019','v':'2019'},{'n':'2018','v':'2018'},{'n':'2017','v':'2017'},{'n':'2016','v':'2016'},{'n':'2015','v':'2015'},{'n':'2014','v':'2014'},{'n':'2013','v':'2013'},{'n':'2012','v':'2012'},{'n':'2011','v':'2011'},{'n':'2000之前','v':'lt|2000'}]},{'key':'by','name':'排序','value':[{'n':'全部','v':''},{'n':'时间','v':'time'},{'n':'人气','v':'hits'},{'n':'评分','v':'score'}]}]
+	
+        },	
+        filter_url:'{{fl.area}}-{{fl.by}}-{{fl.cyass}}',       
+        class_name:'電影',
+        class_url:'1',
+        class_parse:'.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
+	//        cate_exclude:'演员',
+		play_parse:true,
+		lazy:'',
+		limit:6,
+		推荐:'.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+		double:true, // 推荐内容是否双层定位
+		一级:'.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+		二级:{'title':'h1&&Text;.tag-link&&Text','img':'.module-item-pic&&img&&data-src','desc':'.video-info-items:eq(-2)&&Text;.video-info-items:eq(-1)&&Text;.video-info-items:eq(-2)&&Text;.video-info-items:eq(1)&&.video-info-item&&Text;.video-info-items:eq(0)&&.video-info-item&&Text','content':'.vod_content&&Text','tabs':'.module-tab-item','lists':'.module-player-list:eq(#id)&&.scroll-content&&a'},
+		搜索:'.module-items .module-search-item;h3&&Text;*;.video-serial&&Text;*',
 }
