4f2d015f8354dc03b59c74f560d0bdb2a30d0f5d
heroaku <108630267+heroaku@users.noreply.github.com>
2025-01-04 09:56:31 -0700

    Update jstest.js

--- Js/模版/jstest.js
+++ Js/模版/jstest.js
@@ -1,15 +1,25 @@
+
 var rule = {
-  模板:'首图2',
-  类型:'影视',//影视|听书|漫画|小说
-  title:'哈哈影视',
-  host:'https://www.hahads.com',
-  url:'/show/fyclass--------fypage---.html',
-  searchUrl:'/search/**----------fypage---.html',
-  searchable:2,
-  quickSearch:0,
-  filterable:1,
-  filter:'',
-  filter_url:'',
-  filter_def:{},
-  搜索验证标识:'系统安全验证',
-  }
+    title:'火车太堵',
+    host:'https://tdgo.shop',
+    // homeUrl:'/',
+    url:'/vodshow/fyclass--time------fypage---.html',
+    searchUrl:'/vodsearch/**----------fypage---.html',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent':'MOBILE_UA',
+        // "Cookie": "searchneed=ok"
+    },
+    class_name:'电影&电视剧&综艺&动漫&短剧',
+    class_url:'20&21&23&22&24',
+    play_parse:true,
+    lazy:'',
+    limit:6,
+    推荐:'.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    一级:'.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+    二级:{"title":"h1&&Text;.tag-link&&Text","img":".module-item-pic&&img&&data-src","desc":".video-info-items:eq(0)&&Text;.video-info-items:eq(3)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(1)&&Text","content":".vod_content&&Text","tabs":".module-tab-item","lists":".module-player-list:eq(#id)&&.scroll-content&&a"},
+    搜索:'.module-items .module-search-item;h3&&Text;img&&data-src;.video-serial&&Text;a&&href',
+}
