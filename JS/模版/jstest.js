c1a8965d199f8fb99b976f56cdc8138aaab0fbf1
heroaku <108630267+heroaku@users.noreply.github.com>
2025-01-06 13:48:10 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,34 +1,43 @@
+
 var rule = {
-    title: '爱弹幕',
-    host: 'https://cnys.tv',
-  //https://cnys.tv/vodshow-fyclass/page/fypage.html
-    class_name:'电影&电视剧&综艺&动漫',
-    class_url:'1&2&3&4',
-    homeUrl: '',
-    searchUrl: '/index.php/ajax/suggest?mid=1&wd=**',
-    searchable: 2,
-    quickSearch: 0,
-    headers:{'User-Agent':'MOBILE_UA'},
-    // 分类链接fypage参数支持1个()表达式
-    // url: '/index.php/api/vod#type=fyclass&page=fypage',
-	url: '/vodshow-fyclass/page/fypage.html',
-	filterable:0,//是否启用分类筛选,
-	filter_url:'',
-	filter: {},
-	filter_def:{},
-    detailUrl:'/voddetail-fyid/',
-    图片来源:'@Referer=https://cnys.tv/',
-    推荐:'.list-vod.flex.wrap.border-box.public-r.hide-a-21 .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
-    一级:'',
-    一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list.map(function(it){it.vod_pic=urljoin2(input.split("/i")[0],it.vod_pic);return it});',
-    二级:{
-		"title":".slide-info-title&&Text;.slide-info:eq(3)--strong&&Text",
-		"img":"body&&.wow.fadeInLeft;a&&data-original",
-		"desc":".fraction&&Text;.slide-info-remarks:eq(1)&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text",
-		"content":"#height_limit&&Text",
-		"tabs":".anthology.wow.fadeInUp.animated&&.swiper-wrapper&&a",
-		"tab_text":".swiper-slide&&Text",
-		"lists":".anthology-list-box:eq(#id)&&.anthology-list-play&&li"
-	},
-    搜索:'json:list;name;pic;;id',
-}
+     title: 'LIBHD',
+     host: 'https://cnys.tv',
+     模板:'短视2',
+     searchUrl: '/vodsearch/**-------------.html',
+     url: '/vodshow-fyclass/page/fypage.html',
+     searchable: 2,//是否启用全局搜索,
+     quickSearch: 1,//是否启用快速搜索,
+     filterable: 0,//是否启用分类筛选,
+     headers: {
+       'User-Agent': 'MOBILE_UA',
+     },
+     class_name:'电影&剧集&动漫&综艺',
+     class_url:'1&2&4&3',
+     play_parse: true,
+     tab_exclude:'网盘|云盘|SN|FX|LZ',
+     lazy:`js:
+        var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
+        var url = html.url;
+        var from = html.from;
+        var MacPlayerConfig={};
+        if (/.m3u8|.mp4/.test(url)) {
+            input = url
+        } else {
+        eval(fetch(HOST + "/static/js/playerconfig.js").replace('var Mac','Mac'));
+        var list = MacPlayerConfig.player_list[from].parse;
+            input={
+                jx:0,
+                url:list+url,
+                parse:1,
+                header: JSON.stringify({
+                    'referer': HOST
+                })
+            }
+        }
+     `,
+     limit: 6,
+     推荐: '*',
+     double: true, // 推荐内容是否双层定位
+     一级: '.public-list-exp;a&&title;img&&data-src;.ft2&&Text;a&&href',
+     搜索: '.public-list-box;.thumb-txt&&Text;.public-list-exp&&img&&data-src;.public-list-prb&&Text;a&&href',
+    }
