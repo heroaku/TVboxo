ed9a5b333b053b2d6f713c0373d4b8b2400ca24d
heroaku <108630267+heroaku@users.noreply.github.com>
2025-01-06 13:42:18 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,12 +1,34 @@
-var rule = Object.assign(muban.vfed,{
-title:'欧乐影院',
-host:'https://www.olehdtv.com',
-'User-Agent':'MOBILE_UA',
-class_parse:'ul.top_nav&&li;a&&Text;a&&href;./(\\d+).html',
-cate_exclude:'午夜影院|VIP蓝光影院|电视直播',
-推荐:'.cbox_list;ul&&li;a&&title;.lazyload&&data-original;.text_dy&&Text;a&&href',
-double:true, // 推荐内容是否双层定位
-一级:'.vodlist&&li;a&&title;.lazyload&&data-original;.text_dy&&Text;a&&href',
-二级:{"title":".hd_tit&&Text;.content_min&&ul&&li&&Text","img":".lazyload&&data-original","desc":";;;.content_min&&ul&&li:eq(2) a&&Text;.content_min&&ul&&li:eq(3) a&&Text","content":".context&&span&&Text","tabs":".play_source_tab&&.active","lists":".content_playlist:eq(#id) li"},
-搜索:'.vodlist&&li;*;*;*;*',
-});
+var rule = {
+    title: '爱弹幕',
+    host: 'https://cnys.tv',
+  //https://cnys.tv/vodshow-fyclass/page/fypage.html
+    class_name:'电影&电视剧&综艺&动漫',
+    class_url:'1&2&3&4',
+    homeUrl: '',
+    searchUrl: '/index.php/ajax/suggest?mid=1&wd=**',
+    searchable: 2,
+    quickSearch: 0,
+    headers:{'User-Agent':'MOBILE_UA'},
+    // 分类链接fypage参数支持1个()表达式
+    // url: '/index.php/api/vod#type=fyclass&page=fypage',
+	url: '/vodshow-fyclass/page/fypage.html',
+	filterable:0,//是否启用分类筛选,
+	filter_url:'',
+	filter: {},
+	filter_def:{},
+    detailUrl:'/voddetail-fyid/',
+    图片来源:'@Referer=https://cnys.tv/',
+    推荐:'.list-vod.flex.wrap.border-box.public-r.hide-a-21 .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
+    一级:'',
+    一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list.map(function(it){it.vod_pic=urljoin2(input.split("/i")[0],it.vod_pic);return it});',
+    二级:{
+		"title":".slide-info-title&&Text;.slide-info:eq(3)--strong&&Text",
+		"img":"body&&.wow.fadeInLeft;a&&data-original",
+		"desc":".fraction&&Text;.slide-info-remarks:eq(1)&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)--strong&&Text;.slide-info:eq(1)--strong&&Text",
+		"content":"#height_limit&&Text",
+		"tabs":".anthology.wow.fadeInUp.animated&&.swiper-wrapper&&a",
+		"tab_text":".swiper-slide&&Text",
+		"lists":".anthology-list-box:eq(#id)&&.anthology-list-play&&li"
+	},
+    搜索:'json:list;name;pic;;id',
+}
