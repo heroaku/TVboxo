c3da997702c031977aa207c4c887f07d5592a7b9
heroaku <108630267+heroaku@users.noreply.github.com>
2025-01-10 12:19:47 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,106 +1,300 @@
+globalThis.getRandomItem = function (items) {return items[Math.random() * items.length | 0];
+}
 var rule = {
-    title: '可可影视[优]',
-    host: 'https://www.keke8.app',
-    //"备注": "与毒舌，网飞猫同一站，就主页网址不一样，可可：https://www.keke7.app，毒舌: https://www.dushe7.app，网飞猫: https://www.ncat2.com:51111",
-    //host: 'https://www.kkys01.com',
-    // url: '/show/fyclass-----2-fypage.html',
-    url: '/show/fyclass-fyfilter-fypage.html',
-    filter_url: '{{fl.类型}}-{{fl.地区}}-{{fl.语言}}-{{fl.年份}}-{{fl.排序}}',
-    searchUrl: '/search?k=**&page=fypage',
-    searchable: 2,
-    quickSearch: 0,
-    filterable: 1,
-    headers: {
-        'User-Agent': 'MOBILE_UA',
-    },
-    class_parse: '#nav-swiper&&.nav-swiper-slide;a&&Text;a&&href;/(\\w+).html',
-    cate_exclude: 'Netflix|今日更新|专题列表|排行榜',
-    tab_exclude:'可可影视提供',
-    tab_order: ['超清', '蓝光', '极速蓝光'],
-    tab_remove:['4K(高峰不卡)'],
-    play_parse: true,
-    lazy: '',
-    limit: 20,
-    推荐: '.section-box:eq(2)&&.module-box-inner&&.module-item;*;*;*;*',
-    double: false,
-    一级: '.module-box-inner&&.module-item;.v-item-title:eq(1)&&Text;img:last-of-type&&data-original;.v-item-bottom&&span:eq(1)&&Text;a&&href',
-    二级: {
-        title: '.detail-pic&&img&&alt;.detail-tags&&a&&Text',
-        img: '.detail-pic&&img&&data-original',
-        desc: '.detail-info-row-main:eq(-2)&&Text;.detail-tags&&a&&Text;.detail-tags&&a:eq(1)&&Text;.detail-info-row-main:eq(1)&&Text;.detail-info-row-main&&Text',
-        content: '.detail-desc&&Text',
-        //tabs: '.source-item-label:nth-of-type(2)',
-        tabs: 'body&&.source-item-label[id]',
-        lists: '.episode-list:eq(#id) a',
-    },
-    搜索: '.search-result-list&&a;.title:eq(1)&&Text;*;.search-result-item-header&&Text;a&&href;.desc&&Text',
-    // 图片替换:$js.toString(()=>{
-    //     log(input);
-    //    input = input.replace(rule.host,'https://vres.a357899.cn');
-    // }),
-    //图片替换: 'https://keke5.app=>https://vres.a357899.cn',
-    预处理: $js.toString(() => {
-        let html = request(rule.host);
-        let scripts = pdfa(html, 'script');
-        let img_script = scripts.find(it => pdfh(it, 'script&&src').includes('rdul.js'));
-        if (img_script) {
-            let img_url = img_script.match(/src="(.*?)"/)[1];
-            //console.log(img_url);
-            let img_html = request(img_url);
-            let img_host = img_html.match(/'(.*?)'/)[1];
-            log(img_host);
-            rule.图片替换 = rule.host + '=>' + img_host;
+   title: '采集之王[合]',
+   author: '道长',
+   version: '20240624 beta7',
+   update_info: `20240604:1.首页推荐取消硬控等待。增加随机推荐功能。2.首页推荐新增更新日志查看功能`,
+   tab_remove: ['feifan', 'liangzi', 'fsyun', 'hhyun', 'jsyun'],
+   tab_rename: {'ffm3u8':'🐷非凡','feidaozy':'🐷飞刀','lzm3u8':'🐷量子','hhm3u8':'🐷豪华','jsm3u8':'🐷极速','360zy':'🐷360','heimuer':'🐷黑木耳','hw8':'🐷华为吧','hym3u8':'虎牙','bfzym3u8':'暴风','1080zyk':'优质','kuaikan':'快看','snm3u8':'索尼','gsm3u8':'光速','okm3u8':'OK','leshi':'乐视','tkm3u8':'天空','wjm3u8':'无尽','sdm3u8':'闪电','jinyingm3u8':'金鹰','fsm3u8':'飞速','hnm3u8':'红牛','xlm3u8':'新浪','kcm3u8':'快车','qhm3u8':'奇虎'},
+   tab_order: ['lzm3u8', 'ffm3u8', 'hhm3u8', 'jsm3u8', 'heimuer', '360zy', 'feidaozy', 'hw8'],
+   homeTid: '',
+   homeUrl: '/api.php/provide/vod/?ac=detail&t={{rule.homeTid}}',
+   detailUrl: '/api.php/provide/vod/?ac=detail&ids=fyid',
+   searchUrl: '/api.php/provide/vod/?wd=**&pg=#TruePage##page=fypage',
+   classUrl: '/api.php/provide/vod/',
+   url: '/api.php/provide/vod/?ac=detail&pg=fypage&t=fyfilter',
+   filter_url: '{{fl.类型}}',
+   headers: { 'User-Agent': 'MOBILE_UA' },
+   timeout: 5000, 
+   search_limit: 6,
+   play_parse: true,
+   params: 'http://127.0.0.1:9978/file/jars/采集.json',
+   预处理: $js.toString(() => {
+      function getClasses(item) {
+         let classes = [];
+         if (item.class_name && item.class_url) {
+            if (!/&|电影|电视剧|综艺|动漫[\u4E00-\u9FA5]+/.test(item.class_name)) {
+               try {
+                  item.class_name = ungzip(item.class_name)
+               } catch (e) {
+                  log(`不识别的class_name导致gzip解码失败:${e}`)
+                  return classes
+               }
+            }
+            let names = item.class_name.split('&');
+            let urls = item.class_url.split('&');
+            let cnt = Math.min(names.length, urls.length);
+            for (let i = 0; i < cnt; i++) {
+               classes.push({
+                  'type_id': urls[i], 'type_name': names[i]
+               });
+            }
+         }
+         return classes
+      }
+
+      let _url = rule.params;
+      if (_url && typeof(_url) === 'string' && /^(http|file)/.test(_url)) {
+         let html = request(_url);
+         let json = JSON.parse(html);
+         let _classes = [];
+         rule.filter = {};
+         rule.filter_def = {};
+         json.forEach(it => {
+            let _obj = {
+               type_name: it.name,
+               type_id: it.url,
+               parse_url: it.parse_url || '',
+               api: it.api || '',
+               cate_exclude: it.cate_exclude || '',
+            };
+            _classes.push(_obj);
+            try {
+               let json1 = [];
+               if (it.class_name && it.class_url) {
+                  json1 = getClasses(it);
+               } else {
+                  json1 = JSON.parse(request(urljoin(_obj.type_id, _obj.api || rule.classUrl))).class;
+               }
+               if (_obj.cate_exclude) {
+                  json1 = json1.filter(cl => !new RegExp(_obj.cate_exclude, 'i').test(cl.type_name));
+               }
+               rule.filter[_obj.type_id] = [{
+                  "key": "类型", "name": "类型", "value": json1.map(i => {
+                     return {
+                        "n": i.type_name, 'v': i.type_id
+                     }
+                  })
+               }];
+               if (json1.length > 0) {
+                  rule.filter_def[it.url] = {
+                     "类型": json1[0].type_id
+                  };
+               }
+            } catch (e) {
+               rule.filter[it.url] = [{ "key": "类型", "name": "类型", "value": [{
+                     "n": "全部",  "v": ""
+                  }]
+               }];
+            }
+         });
+         rule.classes = _classes;
+      }
+   }),
+   class_parse: $js.toString(() => {
+      input = rule.classes;
+   }),
+   推荐: $js.toString(() => {
+        let update_info = [{
+            vod_name: '更新日志',
+            vod_id: 'update_info',
+            vod_remarks: `版本:${rule.version}`,
+            vod_pic: 'https://ghproxy.net/https://raw.githubusercontent.com/hjdhnx/hipy-server/master/app/static/img/logo.png'
+        }];
+        VODS = [];
+        if (rule.classes) {
+            let randomClass = getRandomItem(rule.classes);
+            let _url = urljoin(randomClass.type_id, input);
+            if (randomClass.api) {
+                _url = _url.replace('/api.php/provide/vod/', randomClass.api)
+            }
+            try {
+                let html = request(_url, {timeout: rule.timeout});
+                let json = JSON.parse(html);
+                VODS = json.list;
+                VODS.forEach(it => {
+                    it.vod_id = randomClass.type_id + '$' + it.vod_id;
+                    it.vod_remarks = it.vod_remarks + '|' + randomClass.type_name;
+                });
+            } catch (e) {
+            }
+        }
+        VODS = update_info.concat(VODS);
+    }),
+   一级: $js.toString(() => {
+      VODS = [];
+      if (rule.classes) {
+         // log(input);
+         let _url = urljoin(MY_CATE, input);
+         let current_vod = rule.classes.find(item => item.type_id === MY_CATE);
+         if (current_vod && current_vod.api) {
+            _url = _url.replace('/api.php/provide/vod/', current_vod.api)
+         }
+         let html = request(_url);
+         let json = JSON.parse(html);
+         VODS = json.list;
+         VODS.forEach(it => {
+            it.vod_id = MY_CATE + '$' + it.vod_id
+         });
+      }
+   }),
+   二级: $js.toString(() => {
+      VOD = {};
+        if (orId === 'update_info') {
+            VOD = {
+                vod_content: rule.update_info.trim(),
+                vod_name: '更新日志',
+                type_name: '更新日志',
+                vod_pic: 'https://img1.doubanio.com/view/photo/m/public/p2455175550.webp',
+                vod_remarks: `版本:${rule.version}`,
+                vod_play_from: '道长在线',
+                vod_play_url: '六点半$https://mvvideo10.meitudata.com/645b73b6bdacajyaroszrz3280_H264_1_623e5a7cbad9cc.mp4'
+            };
+        } else {
+            if (rule.classes) {
+            let _url = urljoin(fyclass, input);
+            let current_vod = rule.classes.find(item => item.type_id === fyclass);
+            if (current_vod && current_vod.api) {
+               _url = _url.replace('/api.php/provide/vod/', current_vod.api)
+            }
+            let html = request(_url);
+            let json = JSON.parse(html);
+            let data = json.list;
+            VOD = data[0];
+            if (current_vod && current_vod.type_name) {
+               VOD.vod_play_from = VOD.vod_play_from.split('$$$').map(it => it).join('$$$')
+         }}}
+   }),
+   搜索: $js.toString(() => {
+        VODS = [];
+        if (rule.classes) {
+            let page = Number(MY_PAGE);
+            page = (MY_PAGE - 1) % Math.ceil(rule.classes.length / rule.search_limit) + 1;
+            let truePage = Math.ceil(MY_PAGE / Math.ceil(rule.classes.length / rule.search_limit));
+            if (rule.search_limit) {
+                let start = (page - 1) * rule.search_limit;
+                let end = page * rule.search_limit;
+                let t1 = new Date().getTime();
+                log('start:' + start);
+                log('end:' + end);
+                if (start < rule.classes.length) {
+                    let search_classes = rule.classes.slice(start, end);
+                    let urls = [];
+                    search_classes.forEach(it => {
+                        let _url = urljoin(it.type_id, input);
+                        if (it.api) {
+                            _url = _url.replace('/api.php/provide/vod/', it.api)
+                        }
+                        _url = _url.replace("#TruePage#", "" + truePage);
+                        urls.push(_url);
+                    });
+                    let results = [];
+
+                    if (typeof (batchFetch) === 'function') {
+                        let reqUrls = urls.map(it => {
+                            return {
+                                url: it,
+                                options: {timeout: rule.timeout}
+                            }
+                        });
+                        let rets = batchFetch(reqUrls);
+                        rets.forEach((ret, idx) => {
+                            let it = search_classes[idx];
+                            if (ret) {
+                                try {
+                                    let json = JSON.parse(ret);
+                                    let data = json.list.filter(it => it.type_name !== '伦理片');
+                                    data.forEach(i => {
+                                    i.vod_id = it.type_id + '$' + i.vod_id;
+                                    i.vod_remarks = i.vod_remarks + '✰' + it.type_name;
+                                    });
+                                    results = results.concat(data);
+                                } catch (e) {
+                                    log(`请求:${it.type_id}发生错误:${e.message}`)
+                                }
+                            }
+                        });
+					} else if (typeof (multiReq) === 'function') {
+                        let reqUrls = urls.map(it => {
+							return {
+								url:it,
+								data:{
+									method:'GET', 
+									headers: rule.headers,
+									timeout: rule.timeout,
+									buffer: 0
+								}
+							}
+                        });
+
+                        let rets = JSON.parse(multiReq(reqUrls));
+                        rets.forEach((ret, idx) => {
+                            let it = search_classes[idx];
+                            if (ret) {
+                                try {
+                                    let json = JSON.parse(ret.content);
+                                    let data = json.list.filter(it => it.type_name !== '伦理片');
+                                    data.forEach(i => {
+                                    i.vod_id = it.type_id + '$' + i.vod_id;
+                                    i.vod_remarks = i.vod_remarks + '✰' + it.type_name;
+                                    });
+                                    results = results.concat(data);
+                                } catch (e) {
+                                    log(`请求:${it.type_id}发生错误:${e.message}`)
+                                }
+                            }
+                        });
+                    } else {
+                        urls.forEach((_url, idx) => {
+                            let it = search_classes[idx];
+                            try {
+                                let html = request(_url);
+                                let json = JSON.parse(html);
+                                let data = json.list.filter(it => it.type_name !== '伦理片');
+                                data.forEach(i => {
+                                    i.vod_id = it.type_id + '$' + i.vod_id;
+                                    i.vod_remarks = i.vod_remarks + '✰' + it.type_name;
+                                });
+                                results = results.concat(data);
+                            } catch (e) {
+                                log(`请求:${it.type_id}发生错误:${e.message}`)
+                            }
+                        });
+                    }
+
+                    VODS = results;
+                    let t2 = new Date().getTime();
+                    log(`✰搜索:${urls.length}个站耗时:${(Number(t2) - Number(t1))}ms`)
+                }
+            }
         }
     }),
-    filter: 'H4sIAAAAAAAAA+2Zz08bRxTH/xefOdgGtTi3HlqpUpVLe6hURREHV4qa0kN/qFWEZLANxhBsEDFx7AIpGEyCf0CQY9bY/md2Ztf/RWf95r0ZR+3LtqGRqviC+LzvzOzs7Nt531k/isQid755FPku+VvkTsS76In9jchMZHHh+6TNvyw8/Dk5briowiJbH6XrQVhBZGkGoneTP3378MGvOnz3068+++Lzr0kV66cyndWiBtJKVRVBDYC0fN3tV1EDQM3LXZgxNaAml4syVdKaBtLSebnyDDUAGjPf8vovcEwA0k63xXUPNQAac+XcK23jmAB0D7U1008Daas7o/IZagA0Zu6p66zjmADUb2tVFC6xHwBphWP/iNYagLRmRzgN1ABQc28O/WZbaxpoLo0Td3CIcwEw2qaf2ydtDLRmBw1vfQ3XDMB6tt5uzzzbAEjLDL2XNdQAUPOXN0XV0ZqGpXuBCmksqm2x6Zg0Jg6TxuL4dFRexUXoNkSlr0PYYnRSlt3WRAsdMg+gLa8Hk2NAiJZlsKWCuCwAtJx7NVk9x+UEoGsfnJl+GmhZNi6MpoHGfPXEaBpotoPXRtNA2uO2cE5QAzBjtu0x23Y/t3s9on4a6N6390W2i/cOQNd7feXXhng9AJMuh3JjqB4GZQwyXXWY8fp7skQPh5jmnCmoDiKHb5xhatHaVSj3OtiC2Np2RHtXZPFNMUxPojb0CirVy/gwiOkqg1dwXdehjcgO0T1nO24PtzENdqr7rYZfT5lUJw6V6pW+ao+DA9BNXh4bTYOVaEbTYCWv0TRYyWs0DVaCWv0Awi6CuL5ye33rfUcOswjxaHxOx8b/WvFZE5+143ETj9vxmInH7HjUxKMUj0XH0zwiLRa9r/4kqEH0zQbRoEHUNEhMNoglEtH76o9pMP9mg/mgwbxpICtXcg9fhUD7ODGxsHJrRzgFs7DEkwvr9W5EMYdXMaNXU7KEL0ncDntpfNJmXf1WRuRw350LpnFvRvX6b8yJpjDmhDMZnHHhjARrFrjizZghrrBzhoczJ0FhovvTEMYMceaEMzVBQaPraQhjQNSWZ56RhjBGkDNfo3RfdFdwLgC3bUAoHLllA8LZhH9rPTgLwVkP1l681ShxJoMzJ9PCOS2cH3DhnH1PhTNfl6llefNy4hxnQqFO/9wpkPsywGzebKHkCh5X1JgTqSzue+dU0AFCGQimaHMnZ5UH/vMUagCkVfKyjFVFQxjj4fUK1rEMgPodPhcV2mQAUPtk8YFKK5Dgf8rMYVGtPW53AHRUcy5Fo4g7PwBtd7/vyKe40hpMOd9Qa0jlfAxhLMJfWqDb+mbAlc23l/N3+1rwbmZg+s1g+s1gan1snlqf/7n1mbst6+M5L0T/CaYzAGnVA9dxvFOqwcQ0t2bb9NZAWuZCFI5Mb8OhDBP36Z8xTNznfa/SFUe0OQHQbPMrXqWJUwUwJffvP/1zBo2zBl8mF378YVFrGmie3M8zjGHiDJosP3NvaEcFmP6cMC0j0zLyIZeRj/55GTFh+ymPUqv+H5R6AJTqu1XRxBOLBppVuajON5jqANTvbOh38tgPgK639tjbPcDrAYQpK+yv28zJij3fMudw9tdt7myf7Yhm2rySAbz3zFEpsvQnK+G0IDwhAAA=',
-    一级f: `js:
-    let urls = [
-    'https://keke5.app/show/1-----1-1.html',
-    'https://keke5.app/show/2-----1-1.html',
-    'https://keke5.app/show/3-----1-1.html',
-    'https://keke5.app/show/4-----1-1.html',
-    'https://keke5.app/show/6-----1-1.html',
-    ];
-    let filters = {};
-    pdfa = jsp.pdfa;
-    pdfh = jsp.pdfh;
-    for(let url of urls){
-    let fclass = url.match(/show\\/(\\d+)-/)[1];
-    console.log(fclass);
-    let html = request(url);
-    let tabs = pdfa(html, '.filter-row');
-    let data = [];
-    for (let tab of tabs) {
-        let title = pdfh(tab, 'strong&&Text').replace(':','');
-        let lis = pdfa(tab, 'a');
-        let _map = {key: title, name: title};
-        let value = [];
-        for (let li of lis) {
-            let n = pdfh(li, 'a&&Text').trim();
-            let v=n;
-            if(/全部|地区|类型/.test(n)){
-                v = '';
-            }else if(/综合/.test(n)){
-                v = '1';
-            }else{
-                v = pdfh(li,'a&&href');
-                try {
-                    v = v.match(/-(.*?)1-1\.html/)[1].replace(/-/g,'');
-                }catch (e) {
-                    v = v.match(/-(.*?)-1\.html/)[1].replace(/-/g,'');
-                }
-                v = decodeURIComponent(v);
+   lazy: $js.toString(() => {
+      let parse_url = '';
+      if (flag && flag.includes('|')) {
+         let type_name = flag.split('|')[0];
+         let current_vod = rule.classes.find(item => item.type_name === type_name);
+         if (current_vod && current_vod.parse_url) {
+            parse_url = current_vod.parse_url
+         }
+      }
+      if (/\.(m3u8|mp4)/.test(input)) {
+         input = {
+            parse: 0,
+            url: input
+         }
+      } else {
+         if (parse_url.startsWith('json:')) {
+            let purl = parse_url.replace('json:', '') + input;
+            let html = request(purl);
+            input = {
+               parse: 0,
+               url: JSON.parse(html).url
             }
-            value.push({
-                'n': n, 'v': v
-            });
-        }
-        _map['value'] = value;
-        data.push(_map);
-    }
-    filters[fclass] = data;
-    }
-    VODS = [filters];
-    console.log(gzip(JSON.stringify(filters)));
-    `,
+         } else {
+            input = parse_url + input;
+         }
+      }
+   }),
 }
