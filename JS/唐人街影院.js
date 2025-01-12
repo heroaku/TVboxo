06cca3a3413415e9ee420c287eaa01b276c75a4c
heroaku <108630267+heroaku@users.noreply.github.com>
2025-01-12 16:58:43 -0700

    Create 唐人街影院.js

---
+++ JS/唐人街影院.js
@@ -0,0 +1,28 @@
+var rule = {
+  title: '唐人街影院',
+  host: 'https://www.trjvod.com',
+  url: '/index.php/vod/show/id/fyclass/page/fypage.html',
+  searchUrl: '/index.php/vod/search/page/fypage/wd/*.html',
+  class_parse: '.top_nav li;a&&Text;a&&href;.*/(.*?).html',
+  searchable: 2,
+  quickSearch: 0,
+  filterable: 0,
+  headers: {
+    'User-Agent': 'MOBILE_UA',
+  },
+  play_parse: true,
+  lazy: '',
+  limit: 6,
+  推荐: '.cbox_list;*;*;*;*;*',
+  double: true,
+  一级: 'ul.vodlist li;a&&title;a&&data-original;.pic_text&&Text;a&&href',
+  二级: {
+    title: 'h2&&Text;.detail_list&&ul:eq(1)&&li&&a:eq(2)&&Text',
+    img: '.vodlist_thumb&&data-original',
+    desc: '.content_detail&&li:eq(1)&&Text;.detail_list&&ul:eq(1)&&li&&a&&Text;.detail_list&&ul:eq(1)&&li&&a:eq(1)&&Text;.detail_list&&ul:eq(1)&&li:eq(2)&&Text;.detail_list&&ul:eq(1)&&li:eq(3)&&Text',
+    content: '.content_desc&&span&&Text',
+    tabs: '.play_source_tab&&a',
+    lists: '.content_playlist:eq(#id) li',
+  },
+  搜索: '*',
+}
