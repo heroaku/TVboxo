b2d512326f7e8e5892c4f567a3d110860b732380
heroaku <108630267+heroaku@users.noreply.github.com>
2025-01-12 18:43:33 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -4,7 +4,7 @@ var rule = {
     模板:'短视2',
     host: 'https://www.tyys2.com',
     //homeUrl:'/map.html',
-    // url: '/index.php/api/vod#type=fyclass&page=fypage',https://www.tyys2.com/index.php/vod/type/id/fyclass/page/fypage.html
+    // url: 'https://www.tyys2.com/index.php/vod/type/id/1/page/2.html',https://www.tyys2.com/index.php/vod/type/id/fyclass/page/fypage.html
     url: '/index.php/vod/type/id/fyclass/page/fypage.html',
     filterable:1,//是否启用分类筛选,
     filter_url:'{{fl.cateId}}',
@@ -25,5 +25,5 @@ var rule = {
     detailUrl:'/index.php/vod/detail/id/fyid.html',
     推荐:'.border-box .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
     double: false, // 推荐内容是否双层定位
-    一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list.map(function(it){it.vod_pic=it.vod_pic.replace("mac:","https:");return it});',
+    //一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list.map(function(it){it.vod_pic=it.vod_pic.replace("mac:","https:");return it});',
 }
