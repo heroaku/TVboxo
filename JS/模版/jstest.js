b92f2e65e8f441b4c9602d4e184f3a3e0a7e2054
heroaku <108630267+heroaku@users.noreply.github.com>
2025-01-12 18:38:54 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,21 +1,29 @@
-var rule={     
-    title:'100影院',
-    host:'https://www.jqqzx.me',
-    //https://www.dy111.me
-    //https://www.dy112.me
-    //https://www.jqqzx.me/vodshow/id/fyclass/page/fypage.html
-    url:'/vodshow/id/fyclass/page/fypage.html',
-    searchUrl:'/vodsearch/**----------fypage---.html',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-    //class_parse:'.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
-    //cate_exclude:'演员',
-    class_name:'电影&电视剧&综艺&动漫&纪录片&国产剧&港台剧&日韩剧&欧美剧&其他剧',
-    class_url:'dianying&juji&3&4&38&13&14&15&16&25',  
-    推荐:'.module-poster-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-    一级:'.module-poster-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-    //二级:{"title":"h1&&Text;","img":".lazyload&&data-original","desc":";;.module-info-item:eq(4) p&&Text;.module-info-item:eq(3) div&&Text;.module-info-item:eq(1) a&&Text","content":".show-desc&&Text","tabs":'#y-playList&&span',"lists":".module-play-list:eq(#id) a"},     
-    二级:{"title":"h1&&Text;.module-info-tag-link:eq(2)&&Text","img":".lazyload&&data-original","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-content&&.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction-content&&Text","tabs":".module-tab-items-box:eq(0)&&.module-tab-item","lists":".module-list:eq(#id)&&.module-play-list-content a"}, 
-    搜索:'.module-card-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href', 
+muban.短视2.二级.img = '.detail-pic&&img&&data-original';
+var rule = {
+    title: 'moflix',
+    模板:'短视2',
+    host: 'https://www.tyys2.com',
+    //homeUrl:'/map.html',
+    // url: '/index.php/api/vod#type=fyclass&page=fypage',https://www.tyys2.com/index.php/vod/type/id/fyclass/page/fypage.html
+    url: '/index.php/vod/type/id/fyclass/page/fypage.html',
+    filterable:1,//是否启用分类筛选,
+    filter_url:'{{fl.cateId}}',
+    filter: {
+        "20":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"20"},{"n":"动作片","v":"21"},{"n":"喜剧片","v":"22"},{"n":"爱情片","v":"23"},{"n":"科幻片","v":"24"},{"n":"恐怖片","v":"25"},{"n":"剧情片","v":"26"},{"n":"战争片","v":"27"},{"n":"惊悚片","v":"28"},{"n":"犯罪片","v":"29"},{"n":"冒险篇","v":"30"},{"n":"动画片","v":"31"},{"n":"悬疑片","v":"32"},{"n":"武侠片","v":"33"},{"n":"奇幻片","v":"34"},{"n":"纪录片","v":"35"},{"n":"其他片","v":"36"}]}],
+        "37":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"37"},{"n":"国产剧","v":"38"},{"n":"港台剧","v":"39"},{"n":"欧美剧","v":"40"},{"n":"日韩剧","v":"41"},{"n":"其他剧","v":"42"}]}],
+        "43":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"43"},{"n":"国产动漫","v":"52"},{"n":"日本动漫","v":"53"},{"n":"欧美动漫","v":"54"}]}]
+    },
+    filter_def:{
+        20:{cateId:'20'},
+        37:{cateId:'37'},
+        45:{cateId:'45'},
+        43:{cateId:'43'}
+    },
+    //class_parse:'.swiper-wrapper&&li;a&&Text;a&&href;.*/(\\d+).html',
+    class_name:'动漫&电影&国产剧&美剧&日韩剧&港台剧',
+    class_url:'4&2&1&16&15&14',
+    detailUrl:'/index.php/vod/detail/id/fyid.html',
+    推荐:'.border-box .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
+    double: false, // 推荐内容是否双层定位
+    一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list.map(function(it){it.vod_pic=it.vod_pic.replace("mac:","https:");return it});',
 }
