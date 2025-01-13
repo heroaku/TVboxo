61bc67cbb7b9ea97b33b29dda5c3e193dad2844d
heroaku <108630267+heroaku@users.noreply.github.com>
2025-01-13 15:25:27 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,22 +1,3 @@
-var rule = {
-    模板: '海螺3',
-    title: '宝片视频',
-    host: 'https://ibaopian.pro',
-    searchUrl: '/vod/search/page/fypage/wd/**.html',
-    url: '/vod/show/id/fyclass/page/fypage.html',
-    class_name:'电视剧&电影&动漫&综艺&纪录片&短剧',
-    class_url:'2&1&4&3&5&8',
-    二级: {
-        title: '.hl-dc-title&&Text;.hl-dc-content&&li:eq(6)&&Text',
-        img: '.hl-lazy&&data-original',
-        desc: '.hl-dc-content&&li:eq(10)&&Text;.hl-dc-content&&li:eq(4)&&Text;.hl-dc-content&&li:eq(5)&&Text;.hl-dc-content&&li:eq(2)&&Text;.hl-dc-content&&li:eq(3)&&Text',
-        content: '.hl-content-text&&Text',
-        tabs: '.hl-tabs&&a',
-        lists: '.hl-plays-list:eq(#id)&&li',
-    },
-}
-
-
 var rule={
     title: '宝片视频',
     host: 'https://ibaopian.pro',
