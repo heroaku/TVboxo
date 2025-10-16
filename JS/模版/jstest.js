c918d354b3120d4751770ab6a933997048f6c4be
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-16 12:21:27 -0600

    Refactor推荐 and 搜索 properties in jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -16,9 +16,9 @@ class_url: '1&2&3&4&13&14&15&16',
 play_parse:true,
 lazy:'',
 limit:6,
-推荐:'.module-list;.module-items&&.module-item;a&&title;img&&data-original;.module-item-text&&Text;a&&href',
-double:true, // 推荐内容是否双层定位
-一级:'.module-items&&.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    推荐: 'body&&.scroll-box;.module-poster-item.module-item;.module-poster-item-title&&Text;img&&data-original;.module-item-note&&Text;a&&href',
+    double: true,
+    一级: 'body&&.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
     二级: {
         "title": "h1&&Text;.module-info-tag&&Text",
         "img": ".lazyload&&data-original",
@@ -27,6 +27,5 @@ double:true, // 推荐内容是否双层定位
         "tabs": ".module-tab-item",
         "lists": ".module-play-list:eq(#id) a"
     },
-//二级:{"title":"h1&&Text;.module-info-item&&div&&a:eq(0)&&Text","img":".module-item-pic&&img&&data-original","desc":".module-info-item&&a:eq(1)&&Text;.module-info-item&&a:eq(2)&&Text;.module-info-item:eq(1) a&&Text;.module-info-item:eq(0) a&&Text","content":".module-info-item&&Text","tabs":".module-tab-item:eq(#id)&&Text","lists":".module-list:eq(#id)&&.module-play-list-content a"},
-搜索:'.module&&.module-main;.lazy&&alt;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+    搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
 }
