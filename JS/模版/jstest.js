686aeb94afd024eca09725e3895cb32845a5e955
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-16 12:18:58 -0600

    Refactor 二级 structure in jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -19,6 +19,14 @@ limit:6,
 推荐:'.module-list;.module-items&&.module-item;a&&title;img&&data-original;.module-item-text&&Text;a&&href',
 double:true, // 推荐内容是否双层定位
 一级:'.module-items&&.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-二级:{"title":"h1&&Text;.module-info-item&&div&&a:eq(0)&&Text","img":".module-item-pic&&img&&data-original","desc":".module-info-item&&a:eq(1)&&Text;.module-info-item&&a:eq(2)&&Text;.module-info-item:eq(1) a&&Text;.module-info-item:eq(0) a&&Text","content":".module-info-item&&Text","tabs":".module-tab-item:eq(#id)&&Text","lists":".module-list:eq(#id)&&.module-play-list-content a"},
+    二级: {
+        "title": "h1&&Text;.module-info-tag&&Text",
+        "img": ".lazyload&&data-original",
+        "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
+        "content": ".module-info-introduction&&Text",
+        "tabs": ".module-tab-item",
+        "lists": ".module-play-list:eq(#id) a"
+    },
+//二级:{"title":"h1&&Text;.module-info-item&&div&&a:eq(0)&&Text","img":".module-item-pic&&img&&data-original","desc":".module-info-item&&a:eq(1)&&Text;.module-info-item&&a:eq(2)&&Text;.module-info-item:eq(1) a&&Text;.module-info-item:eq(0) a&&Text","content":".module-info-item&&Text","tabs":".module-tab-item:eq(#id)&&Text","lists":".module-list:eq(#id)&&.module-play-list-content a"},
 搜索:'.module&&.module-main;.lazy&&alt;.lazyload&&data-original;.module-item-note&&Text;a&&href',
 }
