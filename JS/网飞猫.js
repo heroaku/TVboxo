10cc7c7fafff872f7d3d954ddd874210528054cc
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-16 14:56:06 -0600

    Fix selectors and update URLs in 网飞猫.js

--- JS/网飞猫.js
+++ JS/网飞猫.js
@@ -1,7 +1,6 @@
 var rule = {
   title: '网飞猫',
   host: 'https://www.ncat2.com',
-  //https://www.ncat22.com/
   // url: '/show/fyclass-----2-fypage.html',
   url: '/show/fyclass-fyfilter-fypage.html',
   filter_url:'{{fl.类型}}-{{fl.地区}}-{{fl.语言}}-{{fl.年份}}-{{fl.排序}}',
@@ -20,7 +19,7 @@ var rule = {
   limit: 20,
   推荐: '.section-box:eq(2)&&.module-box-inner&&.module-item;*;*;*;*',
   double: false,
-  一级: '.module-box-inner&&.module-item;.v-item-title:eq(-1)&&Text;.v-item-cover&&img:eq(1)&&data-original;.v-item-bottom&&span&&Text;a&&href',
+  一级: '.module-box-inner&&.module-item;.v-item-title:eq(-1)&&Text;img&&data-original;.v-item-bottom&&span&&Text;a&&href',
   二级: {
     title: '.detail-title&&Text;.detail-tags&&a:eq(-2)&&Text',
     img: '.detail-pic&&img&&data-original',
