bc5875761b92cad0eea7373b008cbad498457d6e
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-17 17:18:32 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -22,7 +22,7 @@ double: true, // 推荐内容是否双层定位
     "img": ".module-item-pic&&img&&data-src",
     "desc": ".video-info-items:eq(0)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(3)&&Text",
     "content": "Text",
-    "tabs": ".module-tab-item .tab-item:eq(#id)",
+    "tabs": ".module-tab-item .tab-item",
     "lists": ".module-blocklist:eq(#id)&&.sort-item&&a"
 }, 搜索: '.module-items&&.module-search-item;a&&title;img&&data-src;.video-info&&a&&Text;a&&href',
 }
