5752ef6125e4c035d22369e347dab4e258d7d819
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-18 15:35:00 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,31 +1,37 @@
+
 var rule = {
-    title: 'EU影视',
-    host: 'https://euvod.tv',
-     //https://euvod.tv/vodshow/fyclass--time------fypage---.html
-    //https://euvod.tv/vodshow/fyclass--------fypage---.html
-    url: '/vodshow/fyclass--------fypage---.html',
-    searchUrl: '/vodshow/fyclass--------fypage---.html',
-    headers: {
-        'User-Agent': 'MOBILE_UA'
-    },
-    searchable: 2, //是否启用全局搜索,
-    quickSearch: 0, //是否启用快速搜索,
-    filterable: 0, //是否启用分类筛选,
-    //class_parse: '.nav&&ul&&li;a&&Text;a&&href;.*/(.*?).html',
-    class_name: '电影&电视剧&综艺&动漫&国产剧&港台剧&日韩剧&泰国剧&欧美剧&海外剧',
-    class_url: '1&2&4&3&14&15&16&30&20&13',
-play_parse: true,
-lazy: '',
-limit: 6, 
-推荐: '.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
-double: true, // 推荐内容是否双层定位
-一级: '.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href', 
-二级: {
-    "title": "h1&&Text;.video-info-item:eq(1)&&Text",
-    "img": ".module-item-pic&&img&&data-src",
-    "desc": ".video-info-items:eq(0)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(3)&&Text",
-    "content": "Text",
-    "tabs": ".module-tab-item .tab-item",
-    "lists": ".module-list:eq(#id)&&.sort-item&&a"
-}, 搜索: '.module-items&&.module-search-item;a&&title;img&&data-src;.video-info&&a&&Text;a&&href',
-}
+     title: 'CN影视',
+     host: 'https://ty1010.com',
+    //https://ty1010.com/index.php/vod/type/id/fyclass/page/fypage.html
+     模板:'短视2',
+     searchUrl: 'vodsearch/page/fypage/wd/**.html',
+     url: '/index.php/vod/type/id/fyclass/page/fypage.html',
+     searchable: 2,//是否启用全局搜索,
+     quickSearch: 1,//是否启用快速搜索,
+     filterable: 0,//是否启用分类筛选,
+     headers: {
+       'User-Agent': 'MOBILE_UA',
+     },
+     class_name:'电影&剧集&动漫&综艺',
+     class_url:'1&2&4&3',
+    //detailUrl:'/vodplay-fyid.html',
+     play_parse: true,
+
+
+     limit: 6,
+     推荐: '*',
+     double: true, // 推荐内容是否双层定位
+     一级: '.public-list-exp;a&&title;img&&data-src;.ft2&&Text;a&&href',
+  二级: {
+    title: 'h3&&Text;.hl-ma0&&Text',
+    img: '.mask-0&&data-src',
+    desc: '.detail-info .slide-info:eq(1)--strong&&Text;.deployment.none.cor5&&span&&Text;.deployment.none.cor5&&span:eq(2)&&Text;.detail-info .slide-info:eq(3)--strong&&Text;.detail-info .slide-info:eq(2)--strong&&Text',
+    content: '#height_limit&&Text',
+    tabs: '.anthology-tab a',
+    lists: '.anthology-list-play:eq(#id)&&li',
+    tab_text: 'body&&Text',
+    list_text: 'body&&Text',
+    list_url: 'a&&href',
+  },
+     搜索: '.public-list-box;.thumb-txt&&Text;.public-list-exp&&img&&data-src;.public-list-prb&&Text;a&&href',
+    }
