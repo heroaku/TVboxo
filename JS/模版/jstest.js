a4b0588aa5cbd327e59816fe41b8df9069fa80da
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-18 17:27:09 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,28 +1,51 @@
-var rule={
-    title: '耐看',
-    host: 'https://www.pipilv.cc',
-    //https://www.pipilv.cc/list/2--------2---.html
-    url: '/list/fyclass--------fypage---.html',
-    searchUrl: '/nk/-------------.html?wd=**',
-    searchable: 2,
-    quickSearch: 0,
-    filterable: 0,
-    headers: {
-    'User-Agent': 'MOBILE_UA',
-    },
-    class_parse: '.navbar&&ul&&li;a&&Text;a&&href;/(\\d+).html',
-    play_parse: false,
-    lazy: '',
-    limit: 6,
-    推荐: '.module-items;a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-    double: true,
-    一级: '.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-    二级: {
-    "title": "h1&&Text;.module-info-tag&&Text",
-    "img": ".lazyload&&data-original",
-    "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
-    "content": ".module-info-introduction&&Text",
-    "tabs": ".hisSwiper&&span",
-    "lists": ".his-tab-list:eq(#id) a"},
-    搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',}
-  }
+  },var rule = {
+     title: '狗番',
+     host: 'http://www.jumomo.fun',
+      //http://www.jumomo.fun/vodshow/2--------2---.html
+     模板:'首图2',
+     searchUrl: '/vodsearch/**----------fypage---',
+     url: '/vodshow/fyclass--------fypage---',
+     searchable: 2,//是否启用全局搜索,
+     quickSearch: 1,//是否启用快速搜索,
+     filterable: 0,//是否启用分类筛选,
+     class_name:'TV动画&剧场动画',
+     class_url:'1&2',
+     tab_rename:{'追番乐切':'LR',},
+     lazy:`js:
+        var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
+        var url = html.url;
+        var from = html.from;
+        var MacPlayerConfig={};
+        if (html.encrypt == '1') {
+            url = unescape(url)
+        } else if (html.encrypt == '2') {
+            url = unescape(base64Decode(url))
+        } else if (html.encrypt == '3') {
+            url = base64Decode(url);
+        }
+        if (/\\.m3u8|\\.mp4/.test(url)) {
+            input = url
+        } else {
+            eval(fetch(HOST + "/static/js/playerconfig.js").replace('var Mac','Mac'));
+            var list = MacPlayerConfig.player_list[from].parse;
+            input={
+                jx:0,
+                url:list+url,
+                parse:1,
+                header: JSON.stringify({
+                    'referer': HOST
+                })
+            }
+        }
+    `,
+    推荐: 'ul.stui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;.pic-tag-left&&Text;a&&href',
+    一级: '.stui-vodlist li;a&&title;a&&data-original;.pic-tag-left&&Text;a&&href',
+    二级:{
+           'title': '.h1&&Text',
+           'desc': '.stui-content__detail p:eq(3)&&Text;.stui-content__detail p:eq(0)&&Text;.stui-content__detail p:eq(2)&&Text;.stui-content__detail p:eq(1)&&Text;',
+           'content': '.stui-content__detail p:eq(4)&&Text;',
+           'tabs':'.nav-tabs li',
+           'lists':'.stui-content__playlist:eq(#id) li'
+         },
+    搜索: 'ul.stui-vodlist__media:eq(0) li,ul.stui-vodlist:eq(0) li,#searchList li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href;.text-muted:eq(-1)&&Text'
+    }
