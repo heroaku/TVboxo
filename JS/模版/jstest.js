9a0063a3a47195d4ab804cde5ad2cc485985c7d6
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-19 16:47:08 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,84 +1,30 @@
 var rule = {
-  title: '网飞猫',
-  host: 'https://www.ncat2.com',
-  // url: '/show/fyclass-----2-fypage.html',
-  url: '/show/fyclass-fyfilter-fypage.html',
-  filter_url:'{{fl.类型}}-{{fl.地区}}-{{fl.语言}}-{{fl.年份}}-{{fl.排序}}',
-  searchUrl: '/search?k=**&page=fypage',
-  searchable: 2,
-  quickSearch: 0,
-  filterable: 1,
-  headers: {
-    'User-Agent': 'MOBILE_UA',
-  },
-  class_parse: '#nav-swiper&&.nav-swiper-slide;a&&Text;a&&href;/(\\w+).html',
-  cate_exclude:'Netflix|今日更新|专题列表|排行榜',
-  tab_order:['超清', '蓝光', '极速蓝光'],
-  play_parse: true,
-  lazy: '',
-  limit: 20,
-  推荐: '.section-box:eq(2)&&.module-box-inner&&.module-item;*;*;*;*',
-  double: false,
-  一级: '.module-box-inner&&.module-item;.v-item-title:eq(-1)&&Text;img&&data-original;.v-item-bottom&&span&&Text;a&&href',
-  二级: {
-    title: '.detail-title&&Text;.detail-tags&&a:eq(-2)&&Text',
-    img: '.detail-pic&&img&&data-original',
-    desc: '.detail-info-row-main:eq(-2)&&Text;.detail-tags&&a&&Text;.detail-tags&&a:eq(1)&&Text;.detail-info-row-main:eq(1)&&Text;.detail-info-row-main&&Text',
-    content: '.detail-desc&&Text',
-    tabs: '.source-item-label',
-    lists: '.episode-list:eq(#id) a',
-  },
-  搜索: '.search-result-list&&a;.title&&Text;*;.search-result-item-header&&Text;a&&href;.desc&&Text',
-  图片替换:'https://www.ncat2.com=>https://vres.a357899.cn',
-  filter:'H4sIAAAAAAAAA+2Zz08bRxTH/xefOdgGtTi3HlqpUpVLe6hURREHV4qa0kN/qFWEZLANxhBsEDFx7AIpGEyCf0CQY9bY/md2Ztf/RWf95r0ZR+3LtqGRqviC+LzvzOzs7Nt531k/isQid755FPku+VvkTsS76In9jchMZHHh+6TNvyw8/Dk5briowiJbH6XrQVhBZGkGoneTP3378MGvOnz3068+++Lzr0kV66cyndWiBtJKVRVBDYC0fN3tV1EDQM3LXZgxNaAml4syVdKaBtLSebnyDDUAGjPf8vovcEwA0k63xXUPNQAac+XcK23jmAB0D7U1008Daas7o/IZagA0Zu6p66zjmADUb2tVFC6xHwBphWP/iNYagLRmRzgN1ABQc28O/WZbaxpoLo0Td3CIcwEw2qaf2ydtDLRmBw1vfQ3XDMB6tt5uzzzbAEjLDL2XNdQAUPOXN0XV0ZqGpXuBCmksqm2x6Zg0Jg6TxuL4dFRexUXoNkSlr0PYYnRSlt3WRAsdMg+gLa8Hk2NAiJZlsKWCuCwAtJx7NVk9x+UEoGsfnJl+GmhZNi6MpoHGfPXEaBpotoPXRtNA2uO2cE5QAzBjtu0x23Y/t3s9on4a6N6390W2i/cOQNd7feXXhng9AJMuh3JjqB4GZQwyXXWY8fp7skQPh5jmnCmoDiKHb5xhatHaVSj3OtiC2Np2RHtXZPFNMUxPojb0CirVy/gwiOkqg1dwXdehjcgO0T1nO24PtzENdqr7rYZfT5lUJw6V6pW+ao+DA9BNXh4bTYOVaEbTYCWv0TRYyWs0DVaCWv0Awi6CuL5ye33rfUcOswjxaHxOx8b/WvFZE5+143ETj9vxmInH7HjUxKMUj0XH0zwiLRa9r/4kqEH0zQbRoEHUNEhMNoglEtH76o9pMP9mg/mgwbxpICtXcg9fhUD7ODGxsHJrRzgFs7DEkwvr9W5EMYdXMaNXU7KEL0ncDntpfNJmXf1WRuRw350LpnFvRvX6b8yJpjDmhDMZnHHhjARrFrjizZghrrBzhoczJ0FhovvTEMYMceaEMzVBQaPraQhjQNSWZ56RhjBGkDNfo3RfdFdwLgC3bUAoHLllA8LZhH9rPTgLwVkP1l681ShxJoMzJ9PCOS2cH3DhnH1PhTNfl6llefNy4hxnQqFO/9wpkPsywGzebKHkCh5X1JgTqSzue+dU0AFCGQimaHMnZ5UH/vMUagCkVfKyjFVFQxjj4fUK1rEMgPodPhcV2mQAUPtk8YFKK5Dgf8rMYVGtPW53AHRUcy5Fo4g7PwBtd7/vyKe40hpMOd9Qa0jlfAxhLMJfWqDb+mbAlc23l/N3+1rwbmZg+s1g+s1gan1snlqf/7n1mbst6+M5L0T/CaYzAGnVA9dxvFOqwcQ0t2bb9NZAWuZCFI5Mb8OhDBP36Z8xTNznfa/SFUe0OQHQbPMrXqWJUwUwJffvP/1zBo2zBl8mF378YVFrGmie3M8zjGHiDJosP3NvaEcFmP6cMC0j0zLyIZeRj/55GTFh+ymPUqv+H5R6AJTqu1XRxBOLBppVuajON5jqANTvbOh38tgPgK639tjbPcDrAYQpK+yv28zJij3fMudw9tdt7myf7Yhm2rySAbz3zFEpsvQnK+G0IDwhAAA=',
-  一级f:`js:
-    let urls = [
-    ''https://www.ncat2.com/show/1-----1-1.html',
-    'https://www.ncat2.com/show/2-----1-1.html',
-    'https://www.ncat2.com/show/3-----1-1.html',
-    'https://www.ncat2.com/show/4-----1-1.html',
-    'https://www.ncat2.com/show/6-----1-1.html',
-    ];
-    let filters = {};
-    pdfa = jsp.pdfa;
-    pdfh = jsp.pdfh;
-    for(let url of urls){
-    let fclass = url.match(/show\\/(\\d+)-/)[1];
-    console.log(fclass);
-    let html = request(url);
-    let tabs = pdfa(html, '.filter-row');
-    let data = [];
-    for (let tab of tabs) {
-        let title = pdfh(tab, 'strong&&Text').replace(':','');
-        let lis = pdfa(tab, 'a');
-        let _map = {key: title, name: title};
-        let value = [];
-        for (let li of lis) {
-            let n = pdfh(li, 'a&&Text').trim();
-            let v=n;
-            if(/全部|地区|类型/.test(n)){
-                v = '';
-            }else if(/综合/.test(n)){
-                v = '1';
-            }else{
-                v = pdfh(li,'a&&href');
-                try {
-                    v = v.match(/-(.*?)1-1\.html/)[1].replace(/-/g,'');
-                }catch (e) {
-                    v = v.match(/-(.*?)-1\.html/)[1].replace(/-/g,'');
-                }
-                v = decodeURIComponent(v);
-            }
-            value.push({
-                'n': n, 'v': v
-            });
-        }
-        _map['value'] = value;
-        data.push(_map);
-    }
-    filters[fclass] = data;
-    }
-    VODS = [filters];
-    console.log(gzip(JSON.stringify(filters)));
-    `,
+    title: '老网影视',
+    host: 'https://www.laowang.co',
+     //https://www.laowang.co/vs/fyclass--------fypage---.html
+    url: '/vs/fyclassfyfilter.html.html',
+    searchUrl: '/vodsearch/**----------fypage---.html',
+    headers: {
+        'User-Agent': 'MOBILE_UA'
+    },
+    searchable: 2, //是否启用全局搜索,
+    quickSearch: 0, //是否启用快速搜索,
+    filterable: 1, //是否启用分类筛选,
+    filter: 'H4sIAAAAAAAAA+2ZWW8aVxTHvwvPruSxszVv2fd9T5WHPPipbd5aqYos2cYQwAvYckwoeGuMcVyDwXZdAwW+zNwZ+BYdc8/5n4sUj1DjVmo0b/z+Z+527py5f2behqzQ+e/ehr4f+SV0PuRW6mp5KjQQevP6xxGTf379w08j3QvfeLKKbHbCm0eyB6HRAVIXcypeIJWAY26s4oQjFCPgmDOecsYWKUaAPhObdiPHfWpAn4U5Va1znxrQLl6Q8QgwXuyDXYvzeBo41i5uqOktihFgvMSO2+AYgTFPd6Eu8zwCxPLvZJ4EmEtxw26u8lw0oF10vpP5xO00oN3yljdzbqehn3w6E9vu4hzHNCAWTjgTv3JMA9ZeT6rIIa9dA8c6S/POhzzFCNDn4rt2vMZ9asD6miV34Q/VqPASwbgiud7+iF3UgNhsVCV3OaYBu9hKeXvAu6hBsppzluaQ1S4gNtlyf+eVECADjTm3nuuZcI80+uroSl1GKldW0zUpI3BfZbRe6GSiPAkNSPVGxjnc4VRrkGSVnWoTyeoCJt+cVdkGT1sDtmjvvcQIkMipisQI0C6dd3Lb3E4D5rnySdoRyLb/KTECmUvZnEu5p91MWdU2uJ0GtJtMeplSMa4WYawk33KTRTee4cWApYRXnamW1wxVzIwrIgd2nQuLoGfbq/t2vWFsO3M/2z40OHSKtO5PQx8WfdjUh0QfMnVLdMvUB0UfNHTrW+jeT0M/J/o5Uz8r+llTPyP6GVM/LfppU5f1WuZ6LVmvZa7XkvVa5notWa9lrteS9Xo/zY1q7xTbm2OyUeC+6jPb8K7nu0CDUS8SI0AN7q5LjAD1km6ombSEhY2KMsIajEqUGIFR3UZMg1GJxko0GPe6KoXlXj+Cnnu9mHZ2Zox7nbmfFF4g4QKUi6RchHKJlEtQLpNyGcoVUq5AuUrKVSjXSLkG5Top16HcIOUGlJuk3IRyi5RbUG6TchvKHVLuQLlLyl0o90i5B+U+KfehPCDlAZSHpDyE8oiUR1Aek/IYyhNSnkB5SspTKM9IeQblOSnPobwg5QWUl6S8hDL4DT9GurX2aiA0dGLG0scG+Jk57UXU+IEKJ3vsCUn9GFdVOlA11IiGPg3hscbVzxD6GVc/y+Nn0OzqmlgeAjF9ESfDjoIA472PiskkMOyQ5Iyg73Pyi+xRNOJdD//WhX5sxz+1Tn42x99WHW9lfG3VYtlzHmppDaaZObAhgQ35v9qQwE4EduIL7cTwSdmJzljcLYzxPazBPA4nV4zj0ANUVanVLse4qjSg3XzRSfCbAwKpi4hzyEczgdTTnl1NoZ66YJwunY88FwLEaluqtMwxDRgvu2u8c9GAdgsrzj7emWlAu8NDJ5a0a/Py7qRHQh72f/MMBudBA/qoTLTHp7m1hv/k6PeOc+/gxrS7YDyYvEeVPJiOALHtgpdajmkIDtPgMA0O0+AwDX29h+mpkzpM/Q5Mvw80brjYXuNDmAB9zm66KX4DToBYatndxgcMDXI+HP/BpJ1aas/yewIC9Lm6prKoNQ3o0+d/vJOrGR9hNGA8n08Qfu8sVNlL0z6Pp8GM5feMmAfI53rT/os/3hCg3eyKimW5nQaO2bVdVWQDQoA+swknw0aCQPJSUa008tIF4/n0L/7/zzbsGl7WaDAemsf+V//sQd/vhAMTEJiAwAQEJuBrMgGjfwPB443K+yEAAA==',
+  filter_url: '-{{fl.地区}}--{{fl.类型}}-{{fl.语言}}-{{fl.字母}}---fypage---{{fl.年份}}',
+class_parse: '.nav&&ul&&li;a&&Text;a&&href;.*/(.*?).html',
+play_parse: true,
+lazy: '',
+limit: 6, 
+推荐: '.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+double: true, // 推荐内容是否双层定位
+一级: '.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href', 
+二级: {
+    "title": "h1&&Text;.video-info-item:eq(1)&&Text",
+    "img": ".module-item-pic&&img&&data-src",
+    "desc": ".video-info-items:eq(0)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(3)&&Text",
+    "content": "Text",
+    "tabs": ".module-tab-item",
+    "lists": ".module-list:eq(#id)&&.scroll-content&&a"
+}, 搜索: '.module-items&&.module-search-item;a&&title;img&&data-src;.video-info&&a&&Text;a&&href',
 }
