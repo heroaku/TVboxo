b01f23eb009fae1a1e154db68faabf086c3af9c4
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-19 16:23:24 -0600

    Create 老网影视.js

---
+++ JS/老网影视.js
@@ -0,0 +1,28 @@
+var rule = {
+    title: '老网影视',
+    host: 'https://www.laowang.co',
+     //https://www.laowang.co/vs/fyclass--------fypage---.html
+    url: '/vs/fyclass--------fypage---.html',
+    searchUrl: '/vodsearch/**----------fypage---.html',
+    headers: {
+        'User-Agent': 'MOBILE_UA'
+    },
+    searchable: 2, //是否启用全局搜索,
+    quickSearch: 0, //是否启用快速搜索,
+    filterable: 0, //是否启用分类筛选,
+class_parse: '.nav&&ul&&li;a&&Text;a&&href;.*/(.*?).html',
+play_parse: true,
+lazy: '',
+limit: 6, 
+推荐: '.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+double: true, // 推荐内容是否双层定位
+一级: '.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href', 
+二级: {
+    "title": "h1&&Text;.video-info-item:eq(1)&&Text",
+    "img": ".module-item-pic&&img&&data-src",
+    "desc": ".video-info-items:eq(0)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(3)&&Text",
+    "content": "Text",
+    "tabs": ".module-tab-item",
+    "lists": ".module-list:eq(#id)&&.scroll-content&&a"
+}, 搜索: '.module-items&&.module-search-item;a&&title;img&&data-src;.video-info&&a&&Text;a&&href',
+}
