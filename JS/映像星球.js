1ef76caf9a0b176c8fa382955f41150ffec67702
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-19 13:09:01 -0600

    Create 映像星球.js

---
+++ JS/映像星球.js
@@ -0,0 +1,28 @@
+var rule={
+    title: '映像星球',
+    host: 'https://yxxq1.cc',
+    url: '/top/fyclass--------fypage---.html',
+    searchUrl: '/search/**----------fypage---.html',
+  //https://yxxq1.cc/top/2--------2---.html
+    //https://yxxq1.cc/search/**----------fypage---.html
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 0,
+    headers: {
+    'User-Agent': 'MOBILE_UA',
+    },
+    class_parse: '.navbar&&ul&&li;a&&Text;a&&href;/(\\d+).html',
+    play_parse: false,
+    lazy: '',
+    limit: 6,
+    推荐: '.module-items;a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    double: true,
+    一级: 'a.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    二级: {
+    "title": "h1&&Text;.module-info-tag&&Text",
+    "img": ".lazyload&&data-original",
+    "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
+    "content": ".module-info-introduction&&Text",
+    "tabs": ".hisSwiper&&span",
+    "lists": ".his-tab-list:eq(#id) a"},
+    搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',}
