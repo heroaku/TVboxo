4bcf29416bb300cd6424b10da290e8de40495a08
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-19 15:40:31 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,38 +1,28 @@
 var rule = {
-     title: '海兔影视',
-     host: 'https://www.haituu.tv',
-     //https://www.haitu.xyz
-     模板:'短视2',
-    //https://www.haituu.tv/index.php/vod/show/id/fyclass/page/fypage.html
-     //https://www.haituu.tv/index.php/vod/search/page/2/wd/ai.html
-     searchUrl: '/index.php/vod/search/page/fypage/wd/**.html',
-     url: '/index.php/vod/show/id/fyclass/page/fypage.html',
-     searchable: 2,//是否启用全局搜索,
-     quickSearch: 1,//是否启用快速搜索,
-     filterable: 0,//是否启用分类筛选,
-     headers: {
-       'User-Agent': 'MOBILE_UA',
-     },
-     class_name: '电影&电视剧&综艺&动漫&短剧&国产剧&泰国剧&日韩剧&港台剧&欧美剧&海外剧',
-     class_url: '1&2&3&4&23&13&38&15&14&16&37',
-    //detailUrl:'/vodplay-fyid.html',
-     play_parse: true,
-
-
-     limit: 6,
-     推荐: '*',
-     double: true, // 推荐内容是否双层定位
-     一级: '.public-list-exp;a&&title;img&&data-src;.ft2&&Text;a&&href',
-  二级: {
-    title: 'h3&&Text;.hl-ma0&&Text',
-    img: '.mask-0&&data-src',
-    desc: '.detail-info .slide-info:eq(1)--strong&&Text;.deployment.none.cor5&&span&&Text;.deployment.none.cor5&&span:eq(2)&&Text;.detail-info .slide-info:eq(3)--strong&&Text;.detail-info .slide-info:eq(2)--strong&&Text',
-    content: '#height_limit&&Text',
-    tabs: '.anthology-tab a',
-    lists: '.anthology-list-play:eq(#id)&&li',
-    tab_text: 'body&&Text',
-    list_text: 'body&&Text',
-    list_url: 'a&&href',
-  },
-     搜索: '.public-list-box;.thumb-txt&&Text;.public-list-exp&&img&&data-src;.public-list-prb&&Text;a&&href',
-    }
+    title: '短剧网 | 星星',
+    host: 'https://www.laowang.co',
+     //https://www.laowang.co/vs/fyclass--------fypage---.html
+    url: '/vs/fyclass--------fypage---.html',
+    searchUrl: '/vodsearch/**----------fypage---.html',
+    headers: {
+        'User-Agent': 'MOBILE_UA'
+    },
+    searchable: 2, //是否启用全局搜索,
+    quickSearch: 0, //是否启用快速搜索,
+    filterable: 0, //是否启用分类筛选,
+class_parse: '.nav&&ul&&li;a&&Text;a&&href;.*/(.*?).html',
+play_parse: true,
+lazy: '',
+limit: 6, 
+推荐: '.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+double: true, // 推荐内容是否双层定位
+一级: '.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href', 
+二级: {
+    "title": "h1&&Text;.video-info-item:eq(1)&&Text",
+    "img": ".module-item-pic&&img&&data-src",
+    "desc": ".video-info-items:eq(0)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(3)&&Text",
+    "content": "Text",
+    "tabs": ".module-tab-content",
+    "lists": ".module-blocklist:eq(#id)&&.sort-item&&a"
+}, 搜索: '.module-items&&.module-search-item;a&&title;img&&data-src;.video-info&&a&&Text;a&&href',
+}
