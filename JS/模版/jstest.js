835e2777b9d10808e7c62ad45c3472807256816a
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-22 12:12:26 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -8,8 +8,8 @@ var rule = {
      searchable: 2,//是否启用全局搜索,
      quickSearch: 1,//是否启用快速搜索,
      filterable: 0,//是否启用分类筛选,
-     class_name:'TV动画&剧场动画',
-     class_url:'1&2',
+     //class_name:'TV动画&剧场动画',
+     //class_url:'1&2',
      tab_rename:{'追番乐切':'LR',},
      lazy:`js:
         var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
@@ -44,7 +44,7 @@ var rule = {
            'title': '.h1&&Text',
            'desc': '.stui-content__detail p:eq(3)&&Text;.stui-content__detail p:eq(0)&&Text;.stui-content__detail p:eq(2)&&Text;.stui-content__detail p:eq(1)&&Text;',
            'content': '.stui-content__detail p:eq(4)&&Text;',
-           'tabs':'.nav-tabs li',
+           'tabs':'.title H3',
            'lists':'.stui-content__playlist:eq(#id) li'
          },
     搜索: 'ul.stui-vodlist__media:eq(0) li,ul.stui-vodlist:eq(0) li,#searchList li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href;.text-muted:eq(-1)&&Text'
