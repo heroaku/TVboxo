e98e6d2a502e768707f97f674de55425ab55c783
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-22 18:22:38 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -7,10 +7,8 @@ var rule = {
             headers: {
                 'User-Agent': 'MOBILE_UA'
             },
-            timeout: 5000,
-            class_parse: 'body&&.hl-nav li:gt(0);a&&Text;a&&href;.*/(.*?).html',
+            class_parse: '.hl-nav li:gt(0):lt(4);a&&Text;a&&href;.*/(.*?).html',
             cate_exclude: '明星|专题|最新|排行',
-            limit: 40,
             play_parse: true,
             lazy: '',
             推荐: '.hl-vod-list;li;a&&title;a&&data-original;.remarks&&Text;a&&href',
@@ -23,7 +21,7 @@ var rule = {
                 "content": ".hl-content-text&&Text",
                 "tabs": ".hl-tabs&&a",
                 "lists": ".hl-plays-list:eq(#id)&&li"
-            },
+            }
             搜索: '.hl-list-item;a&&title;a&&data-original;.remarks&&Text;a&&href',
             searchable: 2,//是否启用全局搜索,
             quickSearch: 0,//是否启用快速搜索,
