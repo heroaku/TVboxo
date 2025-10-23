7a774e7736e4f6e958c73b4988355e04ab2ff16f
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-22 18:50:41 -0600

    Create 蘑菇影视.js

---
+++ JS/蘑菇影视.js
@@ -0,0 +1,19 @@
+var rule={     
+    title:'蘑菇影视',
+    host: 'https://www.moguvodw.com',
+    url: '/vodshow/fyclass--------fypage---.html',
+    //https://www.moguvodw.com/vodsearch/**----------fypage---.html
+    searchUrl:'/vodsearch/**----------fypage---.html',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    //class_parse:'.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
+    //cate_exclude:'演员',
+    class_name:'电视剧&电影&动漫&综艺',
+    class_url:'2&1&4&3',  
+    推荐:'body&&.hl-list-wrap;ul&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    一级:'.hl-vod-list&&li;a&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+    二级:{"title":".hl-dc-title&&Text;.hl-col-xs-12&&em&&.hl-text-muted:eq(-2)&&Text","img":".hl-lazy&&data-original","desc":";;.hl-col-xs-12:eq(5)&&Text;.hl-col-xs-12:eq(3)&&Text;.hl-col-xs-12:eq(4)&&Text","content":".hl-content-text&&Text","tabs":".hl-plays-from a","lists":".hl-plays-list:eq(#id) li"},
+    搜索:'.hl-list-wrap&&ul&&li;.hl-item-thumb&&title;.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+}
