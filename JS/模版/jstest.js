2414c48580034abafb9367fc0f7bef8e25f0a9c6
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-26 14:03:32 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,68 +1,24 @@
 var rule = {
-    title: '千神影视',
+    title:'老地影视',
     host:'https://www.laodifang.cc',
      //https://www.laodifang.cc/vodshow/fyclass--------fypage---.html
     //https://www.laodifang.cc/vodsearch/**----------fypage---.html
-    url: '/vodshow/fyfilter.html',
-    searchUrl: '/vodsearch/**----------fypage---.html',
-    searchable: 2,
-    quickSearch: 0,
-    filterable: 1,
-    filter: 'H4sIAAAAAAAAA+2aaU8bRxjHv4tfp9KubQjkXe77vlPlRVQhNWqbSk1aqYoiAcbEXDZGBMexgZBwJcVgjhCwa/vLeHbtb9E181zbLiurpJVQ9p3/v9nZmeeZGc9/Z/d5yAwd+/Z56Iee30PHQvZ6Wc2MhI6Enjz6qUfq3x79+GvP3oVPHKziy83Ycgs7wgy9OAJ4cKKZ/QA4bDAfXrYny8j5eqt/xZ5KIw8Tt/uqzTdV5BHijdWiqrxC3sH3Sbyul4aQd3K7Q0tWLI78KPHmuwGVHgYe4XbV6rYqFZBzuyo5qFIbyKPcbmzY6n+DnPujUvON99huhPtjL6XVLuahU6anXskD5l6qhZd8dRc32jdu9U4B7uarp/JOuDgknHs7sc45MDn39vCaXfmI3MnBw1aJngWUN5gFnMf9Z4F3R0B4dgaEZ1wgPDMEwjOpIDwnAAjPSQOCJlphUY1ickB4Jg6E50QH4TmcIKgvhcV69S32RQvPRQWC6s18dHqO9bRoJ5+uhQfCc1KDoNjLKRXfwdi1oEU1PWG9XoAyEHTPqZeNoRLeUwuKr7pqT35SlXUMkbTncgLhuTRB0CjWxp0xwFHUgrOat6bTlNU9QWUDNfsPjAQEZaCStst5V4ddyLWM8kU1WhLLCHVby2h+qZkdxE5oQalezFo7a5hqLThZRWu3SsnaE9T5alLlKthtLWiINl9xGQhK5Mg6l4GgepkFK7+C9bSgfs5+4HogeNg/cxkI7ktR9qXoqjdWVKVFrKcF1RtIOZlSCVwtrCmShZqdKthDWQyGNC/ht9ZIzalGqxg1XRHfrpdxYYGQw95YKzSWe3nYSbc17LmKcz3eXAsxDFwGgoZ2Y57LQNAwZCpqLMPFrMVAiWItxABzGQgxaUSZFmKARSRaiBSq1RinsCVcK2d3q16uiJWDup0Uho1wlDxIOCp5hHlE8jDzsOQmc1Nyg7khuNlN3OyWvIt5l+RHmR+VvJN5p+QdzDsk53hNGa/J8ZoyXpPjNWW8JsdrynhNjrflLORAFTLW2pgYKNR/GyjCcqCOAzhO5ASQE0ROAjlJ5BSQU0ROAzlN5AyQM0TOAjlL5ByQc0TOAzlP5AKQC0QuArlI5BKQS0QuA7lM5AqQK0SuArlK5BqQa0SuA7lO5AaQG0RuArlJ5BaQW0RuA7lN5A6QO0TuArlL5B6Qe0TuA7lP5AGQB0SMb3ANtH7JqWIlJ1QpxVOFtHuqWJntZmaLCx307LFTARuol0pWcdJV/v3jZ0/5T3JtQCUGXeVPv/v5l55WXx4eCYUP+LDDy8W18UX4b8BaWXK2VuTy4SjuGAA03LwcXZu5WL6uzdOUDxdig2/9PXwx4+5js/zMsvZ6qm9bxTDjLtTOg4HryQtEm4Z73wcDP8Pt92DgZyn9DHB9d44tJQg21XEri4MMgtp7NcgmHoSwm5wzEO36kIPZTzlbQbRj6/6tNfWzkf62dX+r6Gtbp4qOs1PTc1iVdGDz/mHzArsWOsR2jbjB8RoyXoPjNWS8BsdlyLgMjsuQcRkcl/MzsImhwCYeVpsYOaBNFGfH8hwnLI5xP31W87i9hLu/nI1r9g7ZS734J6+FtCEDs8KGOIIyslprFBO47WhB9SYK1jCeiIHgjSNu7aAlAsEbzmZ9d5w2nD0hdvXme+wLCCorfVSrM1imBbWX2xBniVpQvclZa4vOgrWgejs7ViJVL03wmaALUR623jnGDvOgBd1jvb/RN4q1tfhfLJdjoxzDRN3eE2LndvZy3rlbwvNhBERgYgITgz+/AhPD8Roy3sDchAJz83Wam6gwNweyGn52wu+1rB0rNObQooCgeyaX7XH0SyCobHzGXqHXllrw7rn/a9LG+HQjiadXIOieb+dUjrYMLeiePqdLVr4kXr1qQe35vHj0O0lTRSdNW9ieFrJsYVOUOYLyOV+t/4mvbEFQveSsSuSwnhY8jzZUAe0ZCLpnbtjKos0CwXlZV7UM5WVPiG32PzyVylXqJTpC1ELs/fueIHnaoDY7HFikwCIFFimwSIFFCn09FikcPeh7Ql7wKjXWqKzYiQq/yYnKr/w2m+/SVt+IncAPiqJdom7rNVy9nOXPw6Lim7/W/+97+0OtsY1fM3bwH0FjIG1tzbg+9eoQ3/8li07dZqyidvqxNBws2mDRHt5FG+2UDzbB7A1m7+GZvS/+AsG/5QyILwAA',
-    filter_url: '{{fl.类型}}-{{fl.地区}}-{{fl.排序}}-{{fl.剧情}}-{{fl.语言}}-{{fl.字母}}---fypage---{{fl.年份}}',
-    filter_def: {
-        "1": {"类型": "1"},
-        "2": {"类型": "2"},
-        "3": {"类型": "3"},
-        "4": {"类型": "4"},
-        "20": {"类型": "20"}
-        "5": {"类型": "5"}
+    url:'/vodshow/fyclass--------fypage---.html',
+    searchUrl:'/vodsearch/**----------fypage---.html',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{
+        'User-Agent':'UC_UA',
     },
-    headers: {
-        'User-Agent': 'PC_UA',
-    },
-    timeout: 10000,
-    //class_parse: '.top_nav&&li:lt(7);a&&Text;a&&href;/(\\w+).html',
-    //cate_exclude: 'Netflix|今日更新|专题列表|排行榜|热榜|文章',
+    // class_parse:'.fed-pops-navbar&&ul.fed-part-rows&&a.fed-part-eone:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
+    //class_parse:'.top_nav&&ul li:gt(0):lt(25);a&&Text;a&&href;.*/(.*?).html',
     class_name:'电影&电视剧&综艺&动漫&短剧&纪录片',
     class_url:'1&2&3&4&20&5',
-    play_parse: true,
-    lazy: $js.toString(() => {
-        let html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
-        let url = html.url;
-        if (html.encrypt == '1') {
-            url = unescape(url)
-        } else if (html.encrypt == '2') {
-            url = unescape(base64Decode(url))
-        }
-        if (/\.m3u8/.test(url)) {
-            let body = request(url);
-            let lines = body.split('\n');
-            let m3u8Url = null;
-            for (let line of lines) {
-                line = line.trim();
-                if (line.endsWith('.m3u8')) {
-                    m3u8Url = urljoin(url, line);
-                    console.log(m3u8Url);
-                    break;
-                }
-            }
-            input = {
-                jx: 0,
-                url: m3u8Url || url,
-                parse: 0
-            };
-        } else {
-            input = {
-                jx: tellIsJx(url),
-                url: url,
-                parse: 0
-            };
-        }
-    }),
-    double: true,
-    limit: 20,
-    推荐: '.vodlist_item;.vodlist_thumb;*;*;*;*',
+    play_parse:true,
+    lazy:'',
+    limit:6,
+        推荐: '.vodlist_item;.vodlist_thumb;*;*;*;*',
     一级: 'ul.vodlist&&li;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
     二级: {
         title: 'h2&&Text;.detail_list&&ul:eq(1)&&li&&a:eq(2)&&Text',
