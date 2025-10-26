3c60ae14fb147247c68efb84181904e2f8cc3072
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-26 16:15:54 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,42 +1,176 @@
 var rule = {
-    title:'老地影视',
-    host:'https://www.laodifang.cc',
-     //https://www.laodifang.cc/vodshow/fyclass--------fypage---.html
-    //https://www.laodifang.cc/vodsearch/**----------fypage---.html
-    //url: '/vodshow/fyfilter.html',
-    url:'/vodshow/fyclass--------fypage---.html',
-    searchUrl:'/vodsearch/**----------fypage---.html',
-    searchable:2,//是否启用全局搜索,
-    quickSearch:0,//是否启用快速搜索,
-    filterable:0,//是否启用分类筛选,
-   //filter: 'H4sIAAAAAAAAA+2aaU8bRxjHv4tfp9KubQjkXe77vlPlRVQhNWqbSk1aqYoiAcbEXDZGBMexgZBwJcVgjhCwa/vLeHbtb9E181zbLiurpJVQ9p3/v9nZmeeZGc9/Z/d5yAwd+/Z56Iee30PHQvZ6Wc2MhI6Enjz6qUfq3x79+GvP3oVPHKziy83Ycgs7wgy9OAJ4cKKZ/QA4bDAfXrYny8j5eqt/xZ5KIw8Tt/uqzTdV5BHijdWiqrxC3sH3Sbyul4aQd3K7Q0tWLI78KPHmuwGVHgYe4XbV6rYqFZBzuyo5qFIbyKPcbmzY6n+DnPujUvON99huhPtjL6XVLuahU6anXskD5l6qhZd8dRc32jdu9U4B7uarp/JOuDgknHs7sc45MDn39vCaXfmI3MnBw1aJngWUN5gFnMf9Z4F3R0B4dgaEZ1wgPDMEwjOpIDwnAAjPSQOCJlphUY1ickB4Jg6E50QH4TmcIKgvhcV69S32RQvPRQWC6s18dHqO9bRoJ5+uhQfCc1KDoNjLKRXfwdi1oEU1PWG9XoAyEHTPqZeNoRLeUwuKr7pqT35SlXUMkbTncgLhuTRB0CjWxp0xwFHUgrOat6bTlNU9QWUDNfsPjAQEZaCStst5V4ddyLWM8kU1WhLLCHVby2h+qZkdxE5oQalezFo7a5hqLThZRWu3SsnaE9T5alLlKthtLWiINl9xGQhK5Mg6l4GgepkFK7+C9bSgfs5+4HogeNg/cxkI7ktR9qXoqjdWVKVFrKcF1RtIOZlSCVwtrCmShZqdKthDWQyGNC/ht9ZIzalGqxg1XRHfrpdxYYGQw95YKzSWe3nYSbc17LmKcz3eXAsxDFwGgoZ2Y57LQNAwZCpqLMPFrMVAiWItxABzGQgxaUSZFmKARSRaiBSq1RinsCVcK2d3q16uiJWDup0Uho1wlDxIOCp5hHlE8jDzsOQmc1Nyg7khuNlN3OyWvIt5l+RHmR+VvJN5p+QdzDsk53hNGa/J8ZoyXpPjNWW8JsdrynhNjrflLORAFTLW2pgYKNR/GyjCcqCOAzhO5ASQE0ROAjlJ5BSQU0ROAzlN5AyQM0TOAjlL5ByQc0TOAzlP5AKQC0QuArlI5BKQS0QuA7lM5AqQK0SuArlK5BqQa0SuA7lO5AaQG0RuArlJ5BaQW0RuA7lN5A6QO0TuArlL5B6Qe0TuA7lP5AGQB0SMb3ANtH7JqWIlJ1QpxVOFtHuqWJntZmaLCx307LFTARuol0pWcdJV/v3jZ0/5T3JtQCUGXeVPv/v5l55WXx4eCYUP+LDDy8W18UX4b8BaWXK2VuTy4SjuGAA03LwcXZu5WL6uzdOUDxdig2/9PXwx4+5js/zMsvZ6qm9bxTDjLtTOg4HryQtEm4Z73wcDP8Pt92DgZyn9DHB9d44tJQg21XEri4MMgtp7NcgmHoSwm5wzEO36kIPZTzlbQbRj6/6tNfWzkf62dX+r6Gtbp4qOs1PTc1iVdGDz/mHzArsWOsR2jbjB8RoyXoPjNWS8BsdlyLgMjsuQcRkcl/MzsImhwCYeVpsYOaBNFGfH8hwnLI5xP31W87i9hLu/nI1r9g7ZS734J6+FtCEDs8KGOIIyslprFBO47WhB9SYK1jCeiIHgjSNu7aAlAsEbzmZ9d5w2nD0hdvXme+wLCCorfVSrM1imBbWX2xBniVpQvclZa4vOgrWgejs7ViJVL03wmaALUR623jnGDvOgBd1jvb/RN4q1tfhfLJdjoxzDRN3eE2LndvZy3rlbwvNhBERgYgITgz+/AhPD8Roy3sDchAJz83Wam6gwNweyGn52wu+1rB0rNObQooCgeyaX7XH0SyCobHzGXqHXllrw7rn/a9LG+HQjiadXIOieb+dUjrYMLeiePqdLVr4kXr1qQe35vHj0O0lTRSdNW9ieFrJsYVOUOYLyOV+t/4mvbEFQveSsSuSwnhY8jzZUAe0ZCLpnbtjKos0CwXlZV7UM5WVPiG32PzyVylXqJTpC1ELs/fueIHnaoDY7HFikwCIFFimwSIFFCn09FikcPeh7Ql7wKjXWqKzYiQq/yYnKr/w2m+/SVt+IncAPiqJdom7rNVy9nOXPw6Lim7/W/+97+0OtsY1fM3bwH0FjIG1tzbg+9eoQ3/8li07dZqyidvqxNBws2mDRHt5FG+2UDzbB7A1m7+GZvS/+AsG/5QyILwAA',
-   //filter_url: '{{fl.类型}}-{{fl.地区}}-{{fl.排序}}-{{fl.剧情}}-{{fl.语言}}-{{fl.字母}}---fypage---{{fl.年份}}',
-   // filter_def: {
-   //     "1": {"类型": "1"},
-   //     "2": {"类型": "2"},
-   //     "3": {"类型": "3"},
-   //     "4": {"类型": "4"},
-   //     "5": {"类型": "5"}
-   // },
-    headers:{
-        'User-Agent':'UC_UA',
-    },
-    // class_parse:'.fed-pops-navbar&&ul.fed-part-rows&&a.fed-part-eone:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
-    class_parse:'.top_nav&&ul li:gt(0):lt(25);a&&Text;a&&href;.*/(.*?).html',
-    //class_name:'电影&电视剧&综艺&动漫&短剧&纪录片',
-    //class_url:'1&2&3&4&20&5',
-    play_parse:true,
-    lazy:'',
-    limit:6,
-        推荐: '.vodlist_item;.vodlist_thumb;a&&title;*;*;*',
-    一级: 'ul.vodlist&&li;a&&title;.lazyload&&data-original;.pic_text&&Text;a&&href',
-    二级: {
-        title: 'h2&&Text;.detail_list&&ul:eq(1)&&li&&a:eq(2)&&Text',
-        img: '.lazyload&&data-original',
-        desc: 'content_detail&&li:eq(1)&&Text;.detail_list&&ul:eq(1)&&li&&a&&Text;.detail_list&&ul:eq(1)&&li&&a:eq(1)&&Text;.detail_list&&ul:eq(1)&&li:eq(2)&&Text;.detail_list&&ul:eq(1)&&li:eq(3)&&Text',
-        content: '.content_desc&&span&&Text',
-        tabs: '.play_source_tab--i&&a',
-        lists: 'ul.list_scroll:eq(#id)&&li'
-    },
-    搜索: '*',
+  类型: '影视',
+  title: '壹影视',
+  desc: '源动力出品',
+  host: 'https://light-ios.yiys04.com',
+    //https://light-ios.yiys04.com/vod/show/by/time/id/2
+  url: '/api/vod/v1/vod/list?pageNum=fypage&pageSize=12&tid=fyclass&by=time&area=&lang=&year=&class=',  
+  //url: '/api/vod/v1/vod/list?pageNum=fypage&pageSize=12&tid=fyclass&by=time&area=&lang=&year=&class=',
+  searchUrl: '/api/search?pageNum=fypage&pageSize=12&key=**',
+  searchable: 2,
+  quickSearch: 0,
+  timeout: 5000,
+  play_parse: true,
+  filterable: 0,
+  class_name: '电影&电视剧&综艺&动漫&短剧',
+  class_url: '1&2&3&4&5&22',
+  playMap: {
+    '官方专线1': 'y1',
+    '官方专线2': 'z1',
+    '全球蓝光': 'w',
+    '蓝光4': 't1',
+    '极速蓝光1': 'e',
+    '极速蓝光2': 'a',
+    '极速1': 'n',
+    '极速2': 'v',
+    '极速3': 'b',
+    '极速4': 'x',
+    '优质': 'i',
+    '优质1': 'l',
+  },
+  预处理: async () => {
+    return []
+  },
+  推荐: async function (tid, pg, filter, extend) {
+    const { input, pdfa, pdfh, pd } = this;
+    const url = `${rule.host}/api/topic/v1/list?tid=0&pageNum=1&pageSize=6`
+    const html = await request(url);
+    const response = JSON.parse(html);
+    const data = response.data.List;
+    let d = [];
+    data.forEach((it) => {
+      const itemData = it.vod_list;
+      itemData.forEach((it) => {
+        d.push({
+          vod_name: it.vod_name,
+          vod_pic: it.vod_pic,
+          vod_desc: it.vod_blurb,
+          vod_id: it.vod_id,
+          vod_remarks: it.vod_remarks
+        })
+      })
+
+    });
+    return d;
+  },
+  一级: async function (tid, pg, filter, extend) {
+    const { input, pdfa, pdfh, pd } = this;
+    const html = await request(input);
+    const response = JSON.parse(html);
+    const data = response.data.List;
+    let d = [];
+    data.forEach((it) => {
+      d.push({
+        vod_name: it.vod_name,
+        vod_pic: it.vod_pic,
+        vod_desc: it.vod_blurb,
+        vod_id: it.vod_id,
+        vod_remarks: it.vod_remarks
+      })
+    });
+    return d;
+  },
+  二级: async function (ids) {
+    const { input, pdfa, pdfh, pd } = this;
+    const d = [];
+    for (let id of ids) {
+      const url = `${rule.host}/vod/play/id/${id}/nid/1`;
+      let html = await request(url);
+      const detail = {
+        vod_id: id,
+      };
+      const $ = pq(html);
+
+      // 提取名称
+      const name = $('div.vod-episode-list span.text-lg.pr-5.line-clamp-2').text().trim();
+      detail.vod_name = name;
+      // 提取年份和类型
+      const yearAndGenres = $('div.vod-episode-list .text-sm.text-white\\/75.py-1.pt-3').first().text().trim();
+      const year = yearAndGenres.split(' ')[0];
+      detail.vod_year = year;
+      const genres = yearAndGenres.split(' ').slice(1).join(', ');
+      detail.type_name = genres;
+
+      // 提取更新日期
+      const updateDate = $('div.vod-episode-list .text-sm.text-white\\/75.py-1').text().split(' ')[0];
+      detail.vod_remarks = `更新:${updateDate}`;
+
+      // 提取播放源
+      const playSources = $('.whitespace-nowrap.text-sm').map((index, element) => {
+        return $(element).text().trim();
+      }).get();
+      detail.vod_play_from = playSources.join('$$$');
+
+      const episodes = [];
+      $('div.vod-episode-list ul > div > a').each((index, element) => {
+        const episode = {};
+        // 提取剧集名称
+        const name = $(element).find('li > label > div').text().trim();
+        episode.name = name;
+        // 提取 a 链接的 href
+        const href = $(element).attr('href');
+        episode.href = href;
+        // 将提取的信息添加到数组中
+        episodes.push(episode);
+      });
+      // console.log(episodes);
+
+      const play = [];
+      playSources.forEach((playSource, lineIndex) => {
+        const key = rule.playMap[playSource];
+        const lineEpisodes = episodes.map(episode => {
+          const hrefParts = episode.href.split('/');
+          hrefParts[hrefParts.length - 1] = key;
+          const newHref = `${rule.host}${hrefParts.join('/')}`;
+          return `${episode.name}$${newHref}`;
+        });
+        play[lineIndex] = lineEpisodes.join('#');
+      });
+      detail.vod_play_url = play.join('$$$');
+
+      // console.log(detail);
+      d.push(detail);
+    };
+    console.log(d)
+
+    return d[0];
+  },
+  搜索: async function (wd, quick, pg) {
+    const { input, pdfa, pdfh, pd } = this;
+    const html = await request(input);
+    const response = JSON.parse(html);
+    const data = response.data.List;
+    let d = [];
+    data.forEach((it) => {
+      d.push({
+        vod_name: it.vod_name,
+        vod_pic: it.vod_pic,
+        vod_desc: it.vod_blurb,
+        vod_id: it.vod_id,
+        vod_remarks: it.vod_remarks
+      })
+    });
+    return d;
+  },
+  // lazy: async function (flag, id, flags) {
+  //   const { input, pdfa, pdfh, pd } = this;
+  //   const url = `${rule.host}/api/playurl?`;
+  //   let html = await post(url, {
+  //     data: {
+  //       urlEncode: id,
+  //       sourceCode: flag
+  //     }
+  //   });
+  //   const response = JSON.parse(html);
+  //   const realUrl = response?.data?.url || "";
+  //   if (/m3u8|mp4/.test(realUrl)) {
+  //     return { parse: 0, url: realUrl }
+  //   } else {
+  //     if (/m3u8|mp4/.test(realUrl)) {
+  //       return { parse: 0, url: input }
+  //     } else {
+  //       return { parse: 1, url: input }
+  //     }
+  //   }
+  // }
 }
