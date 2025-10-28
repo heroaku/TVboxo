32864396bdbd7b1945240aa858f69b0be6e9017e
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-28 17:15:07 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,12 +1,36 @@
-var rule = Object.assign(muban.vfed,{
-title:'欧乐影院',
-host:'https://www.olehdtv.com',
-'User-Agent':'MOBILE_UA',
-class_parse:'ul.top_nav&&li;a&&Text;a&&href;./(\\d+).html',
-cate_exclude:'午夜影院|VIP蓝光影院|电视直播',
-推荐:'.cbox_list;ul&&li;a&&title;.lazyload&&data-original;.text_dy&&Text;a&&href',
-double:true, // 推荐内容是否双层定位
-一级:'.vodlist&&li;a&&title;.lazyload&&data-original;.text_dy&&Text;a&&href',
-二级:{"title":".hd_tit&&Text;.content_min&&ul&&li&&Text","img":".lazyload&&data-original","desc":";;;.content_min&&ul&&li:eq(2) a&&Text;.content_min&&ul&&li:eq(3) a&&Text","content":".context&&span&&Text","tabs":".play_source_tab&&.active","lists":".content_playlist:eq(#id) li"},
-搜索:'.vodlist&&li;*;*;*;*',
-});
+var rule = {
+     title: '海兔影视',
+     host: 'https://www.haituu.tv',
+     //https://www.haitu.xyz
+     模板:'短视2',
+    //https://www.haituu.tv/index.php/vod/show/id/fyclass/page/fypage.html
+     //https://www.haituu.tv/index.php/vod/search/page/2/wd/ai.html
+     searchUrl: '/index.php/vod/search/page/fypage/wd/**.html',
+     url: '/index.php/vod/show/id/fyclass/page/fypage.html',
+     searchable: 2,//是否启用全局搜索,
+     quickSearch: 1,//是否启用快速搜索,
+     filterable: 0,//是否启用分类筛选,
+     headers: {
+       'User-Agent': 'MOBILE_UA',
+     },
+     class_name: '电影&电视剧&综艺&动漫&短剧&国产剧&泰国剧&日韩剧&港台剧&欧美剧&海外剧',
+     class_url: '1&2&3&4&23&13&38&15&14&16&37',
+    //detailUrl:'/vodplay-fyid.html',
+     play_parse: true,
+     limit: 6,
+     推荐: '*',
+     double: true, // 推荐内容是否双层定位
+     一级: '.public-list-exp;a&&title;img&&data-src;.ft2&&Text;a&&href',
+  二级: {
+    title: 'h3&&Text;.hl-ma0&&Text',
+    img: '.mask-0&&data-src',
+    desc: '.detail-info .slide-info:eq(1)--strong&&Text;.deployment.none.cor5&&span&&Text;.deployment.none.cor5&&span:eq(2)&&Text;.detail-info .slide-info:eq(3)--strong&&Text;.detail-info .slide-info:eq(2)--strong&&Text',
+    content: '#height_limit&&Text',
+    tabs: '.anthology-tab a',
+    lists: '.anthology-list-play:eq(#id)&&li',
+    tab_text: 'body&&Text',
+    list_text: 'body&&Text',
+    list_url: 'a&&href',
+  },
+     搜索: '.public-list-box;.thumb-txt&&Text;.public-list-exp&&img&&data-src;.public-list-prb&&Text;a&&href',
+    }
