97f29c9237958583f64f2a2e0f2146c2e4e00b11
heroaku <108630267+heroaku@users.noreply.github.com>
2025-10-31 16:57:00 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,97 +1,520 @@
-var rule = {
-author: '小可乐/2503/第一版',
-title: '小红影视.',
-类型: '影视',
-host: 'https://www.xiaohys.com',
-hostJs: '',
-headers: {'User-Agent': MOBILE_UA},
-编码: 'utf-8',
-timeout: 5000,
-homeUrl: '/',
-url: '/index.php/api/vod',
-filter_url: '',
-searchUrl: '/index.php/ajax/suggest?mid=1&limit=60&wd=**',
-detailUrl: '/detail/fyid',
-limit: 9,
-double: false,
-class_name: '电影&剧集&综艺&动漫',
-class_url: '1&2&3&4',
-filter_def: {},
-推荐: '.public-list-exp;a&&title;img&&data-src;.ft2&&Text;a&&href',
-一级: $js.toString(() => {
-let t = new Date().getTime().toString();
-let key = md5(`DS${t}DCC147D11943AF75`);
-let fbody = {
-    "type": MY_CATE.toString(),
-    "class": (MY_FL.class || "").toString(),
-    "area": (MY_FL.area || "").toString(),
-    "year": (MY_FL.year || "").toString(),
-    "letter": (MY_FL.letter || "").toString(),
-    "by": (MY_FL.by || "").toString(),
-    "page": MY_PAGE,
-    "time": t,
-    "key": key
-};
-let khtml = fetch(input, {
-    headers: rule.headers,
-    body: fbody,
-    method: 'POST'
-});
-VODS = JSON.parse(khtml).list
-}),
-搜索: 'json:list;name;pic;en;id',
-二级: $js.toString(() => {
-let khtml = fetch(input);
-let kdetail = pdfh(khtml, '.search-show');
-let ktabs = pdfa(khtml, '.anthology-tab&&a').map((it) => { return '👶' + pdfh(it, 'body--span&&Text') });
-let kurls = pdfa(khtml, '.anthology-list-play').map((item) => {
-    let kurl = pdfa(item,'a').map((it) => { return pdfh(it, 'body&&Text') + '$' + pd(it, 'a&&href', input) });
-    return kurl.join('#')
-});
-VOD = {
-    vod_id: input,
-    vod_name: pdfh(khtml,'h3&&Text'),
-    vod_pic: pdfh(khtml,'.detail-pic&&img&&data-src'),
-    type_name: pdfh(kdetail,'li:contains(类型)--em&&Text'),
-    vod_remarks: pdfh(kdetail,'li:contains(状态)--em&&Text'),
-    vod_year: pdfh(kdetail,'li:contains(年份)--em&&Text'),
-    vod_area: pdfh(kdetail,'li:contains(地区)--em&&Text'),
-    vod_lang: pdfh(kdetail,'li:contains(语言)--em&&Text'),
-    vod_director: pdfh(kdetail,'li:contains(导演)--em&&Text'),
-    vod_actor: pdfh(kdetail,'li:contains(主演)--em&&Text'),
-    vod_content: pdfh(kdetail,'.top26--em&&Text'),
-    vod_play_from: ktabs.join('$$$'),
-    vod_play_url: kurls.join('$$$')
-}
-}),
 
-play_parse: true,
-lazy: $js.toString(() => {
-let kdata = fetch(input).split('aaaa=')[1].split('<')[0];
-let kurl = JSON.parse(kdata).url;
-if (/\.(m3u8|mp4)/.test(kurl)) {
-    input = { jx: 0, parse: 0, url: kurl, header: rule.headers }
-} else {
-    let furl = `${HOST}/static/player/artplayer/api.php?ac=getdate`;
-    let fbody = `url=${kurl}`;
-    let khtml = fetch(furl, {
-        headers: rule.headers,
-        body: fbody,
-        method: 'POST'
-    });
-    kdata = JSON.parse(khtml).data;
-    let iv = JSON.parse(khtml).iv;
-    iv = CryptoJS.enc.Utf8.parse(iv);
-    const key = CryptoJS.enc.Utf8.parse('d978a93ffb4d3a00');
-    let decrypted = CryptoJS.AES.decrypt(kdata, key, {
-        iv: iv,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7
-    });
-    kdata = decrypted.toString(CryptoJS.enc.Utf8);
-    kurl = JSON.parse(kdata).url;
-    input = { jx: 0, parse: 0, url: kurl, header: rule.headers }
-}
-}),
-filter: 'H4sIAAAAAAAAA+2ZWU8bVxTH3/kYfqbSDNAkzVv2fd8T5cGNrDYqpRLQSgghGYyJIWAbRCCuzdawhWIwSynYNf4yvjPmW+Ta92xuy8hqaV8yb/79z9w755w7M/fvmd6mgB04+6KpN/BdqCdwNvCqPdjVFWgOdAS/D2lUwytOJKr5p2D7j1p40RvoqMrR1aPIalXWEOhrBnUqo48HFQBjbmwLJmLAmNOfdMJTEAOgOUdWy8UMzmmA5lwZVwcFnNMAjaPEGeh8sffl/DCezwDGKtllNboGMQA638imW8QYgMjTnSxwnlWg2NIbzhOAcskulw/nMRcDNG5o4ij1EccZoHGzazpzHGeAYgPr7tQ4xgxQLDLiDPyMMQNUXyGhovtYnwGMHc1MOO+XIAZAc069qQzncU4DVMPhhjv5mypuYRnEdERisfKBVsoAxeJDKrGNMQO0UqWk7jOulAHuXMaZGafO1YBigyX3V6wEgDpQHHcLmbqE66S+l33NdKsEO0NBcadkcmo03+idsrhylBrCHAxQp5dTzv4mdtoA9yrnHBxSr2pAuR/GVbqIWRugFdp5xzEA6uPbLY4B0LjpJSezjuMMUJ5zH3kcAK/67xwD4FxyMpdc3bixnMov4zgDNG4woTulYnhDMFMlSyU3kXWHU1gMMd+l887bkh5GNyoyHRHdKxfwWQRQt+rtwY5veNUrm9nKarjRVU8X9fE4twGxChwDoJXdXuQYAK3CdFGNTXOYWayTCBsQ68sxAHHNiJgBsb6iEgOig2ojwh2sQl0He0LBTnHfHOyWC8UGO9hitXwJWu2n0NtYb5N6K+utUm9hvUXqNuu21C3WLaHbX5Gufwr9DOtnpH6a9dNSP8X6Kalzvbas1+Z6bVmvzfXasl6b67VlvTbXa8t6ba7XlvVaXK8l67W4XkvWa3G9lqzX4notWa/F9VqyXovr1T/r78hQd3dIXlHZaWdzrMEr6hwI50g5D8p5Ui6AcoGUi6BcJOUSKJdIuQzKZVKugHKFlKugXCXlGijXSLkOynVSboByg5SboNwk5RYot0i5DcptUu6AcoeUu6DcJeUeKPdIuQ/KfVIegPKAlIegPCTlESiPSHkMymNSnoDyhJSnoDwl5Rkoz0h5DspzUqwv8CKt/qq7Vr7u4evEiU+ofOIv14mTCTtTOZig+7U+lJ6ImbAbwafet6+7u/jpvTmoYrifd736oTNUPW3Ty+amQMuJuWsPn+TlaI1ZU/17KpKo828gNeLe1caeytPz3kCDrvhY9+7lir3cu5cn9HKw5YMF9oQA7IqjTgo9FwCd790QO20A4Re5ZwANO4l/4x+Hovp48rc1aMSX/VNv6eUDvX3n8V7P03dO5bQ1UzML9KcC2fdpf/Zpvt/y/Zbvt3y/5futmt9qPSm/dRQedlfC+Dw2IP3C4JzwCxoot41SJRfDHcIAjZvIOiP47gmAn/FRZx+9CwDvDTvlgyTtDTUQ++/RB8wFgGL5NbUxizEDdL70tnhrZ4DGTc45u/Rm1QCN2993YolyfoLfvtVJ1IfdX7QDwz4YoDm2Bir9ozjawP/hjbTf0c6Gsq6B2GP1rst7bBUotr6iO4sxA77b8N2G7zZ8t+G7jc/YbbSdlNvwchRe3zl1ypUFdCkANGd81U1i3gAUS8666/SN0ADvoMd/d6wkZypxfNMEQHPOL6g07RsGaE6PN0FOJi++ZRqg83l85fN666Vyuk27eD4DMra0I2IaqJ+Lh+U/8PsoAI2Lz6lYGscZwFg5v62y6NAAaM70iJNCpwXAfdlSpWnqSw3EXvvfvUHy+ALp9bbnb51Qo/n6Lsl3Sb5L8l1SwHdJn49Laur7BLEioANkJgAA'
+var rule={
+	title:'老王电影',
+
+	模板:'mxone5',
+  host: "https://laowang.co",
+  url: "/vs/fyclassfyfilter.html",
+  searchUrl: "/s**/page/fypage.html",
+  filterable: 1,
+  
+  filter_url: "-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}",
+
+	filter:{
+	        "5":[
+        {"key":"class","name":"剧情","value":[
+{"n":"全部剧情","v":""},
+{"n":"古装","v":"古装"},
+{"n":"虐恋","v":"虐恋"},
+{"n":"逆袭","v":"逆袭"},
+{"n":"悬疑","v":"悬疑"},
+{"n":"神袭","v":"神袭"},
+{"n":"重生","v":"重生"},
+{"n":"反转","v":"反转"},
+{"n":"复仇","v":"复仇"},
+{"n":"穿越","v":"穿越"},
+{"n":"总裁","v":"总裁"},
+{"n":"甜宠","v":"甜宠"},
+{"n":"强者","v":"强者"},
+{"n":"言情","v":"言情"},
+{"n":"爽文","v":"爽文"},
+{"n":"女恋","v":"女恋"},
+{"n":"离婚","v":"离婚"},
+{"n":"都市","v":"都市"},
+{"n":"脑洞","v":"脑洞"},
+{"n":"仙侠","v":"仙侠"},
+{"n":"民国","v":"民国"},
+{"n":"合集","v":"合集"}
+    ]
+    },
+    {"key":"year","name":"时间","value":[
+    {"n":"全部时间","v":""},
+    {"n":"2024","v":"2024"},
+    {"n":"2023","v":"2023"},
+    {"n":"2022","v":"2022"},
+    {"n":"2021","v":"2021"},
+    {"n":"2020","v":"2020"},
+    {"n":"2019","v":"2019"},
+    {"n":"2018","v":"2018"},
+    {"n":"2017","v":"2017"},
+    {"n":"2016","v":"2016"},
+    {"n":"2015","v":"2015"},
+    {"n":"2014","v":"2014"},
+    {"n":"2013","v":"2013"},
+    {"n":"2012","v":"2012"},
+    {"n":"2011","v":"2011"},
+    {"n":"2010","v":"2010"},
+    {"n":"2009","v":"2009"},
+    {"n":"2008","v":"2008"},
+    {"n":"2007","v":"2007"}
+    ]
+    },
+        {"key":"letter","name":"字母","value":[
+    {"n":"全部","v":""},
+    {"n":"A","v":"A"},
+    {"n":"B","v":"B"},
+    {"n":"C","v":"C"},
+    {"n":"D","v":"D"},
+    {"n":"E","v":"E"},
+    {"n":"F","v":"F"},
+    {"n":"G","v":"G"},
+    {"n":"H","v":"H"},
+    {"n":"I","v":"I"},
+    {"n":"J","v":"J"},
+    {"n":"K","v":"K"},
+    {"n":"L","v":"L"},
+    {"n":"M","v":"M"},
+    {"n":"N","v":"N"},
+    {"n":"O","v":"O"},
+    {"n":"P","v":"P"},
+    {"n":"Q","v":"Q"},
+    {"n":"R","v":"R"},
+    {"n":"S","v":"S"},
+    {"n":"T","v":"T"},
+    {"n":"U","v":"U"},
+    {"n":"V","v":"V"},
+    {"n":"W","v":"W"},
+    {"n":"X","v":"X"},
+    {"n":"Y","v":"Y"},
+    {"n":"Z","v":"Z"},
+    {"n":"0-9","v":"0-9"}
+    ]
+    },
+    {"key":"by","name":"排序","value":[
+    {"n":"全部排序","v":""},
+    {"n":"时间排序","v":"time"},
+    {"n":"人气排序","v":"hits"},
+    {"n":"评分排序","v":"score"}
+    ]
+    }
+    ],
+    "1":[
+        {"key":"class","name":"剧情","value":[
+{"n":"全部剧情","v":""},
+{"n":"喜剧","v":"喜剧"},
+{"n":"爱情","v":"爱情"},
+{"n":"恐怖","v":"恐怖"},
+{"n":"奇幻","v":"奇幻"},
+{"n":"动作","v":"动作"},
+{"n":"冒险","v":"冒险"},
+{"n":"科幻","v":"科幻"},
+{"n":"剧情","v":"剧情"},
+{"n":"犯罪","v":"犯罪"},
+{"n":"战争","v":"战争"},
+{"n":"悬疑","v":"悬疑"},
+{"n":"灾难","v":"灾难"},
+{"n":"武侠","v":"武侠"},
+{"n":"古装","v":"古装"},
+{"n":"邵氏","v":"邵氏"},
+{"n":"经典","v":"经典"},
+{"n":"4k电影","v":"4K"}
+    ]
+    },
+
+    {"key":"area","name":"地区","value":[
+    {"n":"全部地区","v":""},
+    {"n":"大陆","v":"大陆"},
+    {"n":"香港","v":"香港"},
+    {"n":"台湾","v":"台湾"},
+    {"n":"美国","v":"美国"},
+    {"n":"日本","v":"日本"},
+    {"n":"韩国","v":"韩国"},
+    {"n":"泰国","v":"泰马"},
+    {"n":"其他","v":"其他"}
+    ]
+    },
+    {"key":"year","name":"时间","value":[
+    {"n":"全部时间","v":""},
+    {"n":"2024","v":"2024"},
+    {"n":"2023","v":"2023"},
+    {"n":"2022","v":"2022"},
+    {"n":"2021","v":"2021"},
+    {"n":"2020","v":"2020"},
+    {"n":"2019","v":"2019"},
+    {"n":"2018","v":"2018"},
+    {"n":"2017","v":"2017"},
+    {"n":"2016","v":"2016"},
+    {"n":"2015","v":"2015"},
+    {"n":"2014","v":"2014"},
+    {"n":"2013","v":"2013"},
+    {"n":"2012","v":"2012"},
+    {"n":"2011","v":"2011"},
+    {"n":"2010","v":"2010"},
+    {"n":"2009","v":"2009"},
+    {"n":"2008","v":"2008"},
+    {"n":"2007","v":"2007"}
+    ]
+    },
+    {"key":"lang","name":"语言","value":[
+    {"n":"全部语言","v":""},
+    {"n":"国语","v":"国语"},
+    {"n":"英语","v":"英语"},
+    {"n":"粤语","v":"粤语"},
+    {"n":"闽南语","v":"闽南语"},
+    {"n":"韩语","v":"韩语"},
+    {"n":"日语","v":"日语"},
+    {"n":"法语","v":"法语"},
+    {"n":"德语","v":"德语"},
+    {"n":"其它","v":"其它"}
+    ]
+    },
+        {"key":"letter","name":"字母","value":[
+    {"n":"全部","v":""},
+    {"n":"A","v":"A"},
+    {"n":"B","v":"B"},
+    {"n":"C","v":"C"},
+    {"n":"D","v":"D"},
+    {"n":"E","v":"E"},
+    {"n":"F","v":"F"},
+    {"n":"G","v":"G"},
+    {"n":"H","v":"H"},
+    {"n":"I","v":"I"},
+    {"n":"J","v":"J"},
+    {"n":"K","v":"K"},
+    {"n":"L","v":"L"},
+    {"n":"M","v":"M"},
+    {"n":"N","v":"N"},
+    {"n":"O","v":"O"},
+    {"n":"P","v":"P"},
+    {"n":"Q","v":"Q"},
+    {"n":"R","v":"R"},
+    {"n":"S","v":"S"},
+    {"n":"T","v":"T"},
+    {"n":"U","v":"U"},
+    {"n":"V","v":"V"},
+    {"n":"W","v":"W"},
+    {"n":"X","v":"X"},
+    {"n":"Y","v":"Y"},
+    {"n":"Z","v":"Z"},
+    {"n":"0-9","v":"0-9"}
+    ]
+    },
+    {"key":"by","name":"排序","value":[
+    {"n":"全部排序","v":""},
+    {"n":"时间排序","v":"time"},
+    {"n":"人气排序","v":"hits"},
+    {"n":"评分排序","v":"score"}
+    ]
+    }
+    ],
+    "2":[
+        {"key":"class","name":"剧情","value":[
+{"n":"全部剧情","v":""},
+{"n":"国产","v":"国产"},
+{"n":"喜剧","v":"喜剧"},
+{"n":"韩剧","v":"韩剧"},
+{"n":"偶像","v":"偶像"},
+{"n":"奇幻","v":"奇幻"},
+{"n":"动作","v":"动作"},
+{"n":"战争","v":"战争"},
+{"n":"武侠","v":"武侠"},
+{"n":"家庭","v":"家庭"},
+{"n":"剧情","v":"剧情"},
+{"n":"犯罪","v":"犯罪"},
+{"n":"悬疑","v":"悬疑"},
+{"n":"都市","v":"都市"},
+{"n":"古装","v":"古装"},
+{"n":"历史","v":"历史"},
+{"n":"同性","v":"同性"}
+    ]
+    },
+
+    {"key":"area","name":"地区","value":[
+    {"n":"全部地区","v":""},
+    {"n":"大陆","v":"大陆"},
+    {"n":"香港","v":"香港"},
+    {"n":"韩国","v":"韩国"},
+    {"n":"香港","v":"香港"},
+    {"n":"台湾","v":"台湾"},
+    {"n":"日本","v":"日本"},
+    {"n":"美国","v":"美国"},
+    {"n":"泰国","v":"泰国"},
+    {"n":"英国","v":"英国"},
+    {"n":"法国","v":"法国"},
+    {"n":"其它","v":"其它"}
+    ]
+    },
+    {"key":"year","name":"时间","value":[
+    {"n":"全部时间","v":""},
+    {"n":"2024","v":"2024"},
+    {"n":"2023","v":"2023"},
+    {"n":"2022","v":"2022"},
+    {"n":"2021","v":"2021"},
+    {"n":"2020","v":"2020"},
+    {"n":"2019","v":"2019"},
+    {"n":"2018","v":"2018"},
+    {"n":"2017","v":"2017"},
+    {"n":"2016","v":"2016"},
+    {"n":"2015","v":"2015"},
+    {"n":"2014","v":"2014"},
+    {"n":"2013","v":"2013"},
+    {"n":"2012","v":"2012"},
+    {"n":"2011","v":"2011"},
+    {"n":"2010","v":"2010"},
+    {"n":"2009","v":"2009"},
+    {"n":"2008","v":"2008"},
+    {"n":"2007","v":"2007"}
+    ]
+    },
+    {"key":"lang","name":"语言","value":[
+    {"n":"全部语言","v":""},
+    {"n":"国语","v":"国语"},
+    {"n":"英语","v":"英语"},
+    {"n":"粤语","v":"粤语"},
+    {"n":"闽南语","v":"闽南语"},
+    {"n":"韩语","v":"韩语"},
+    {"n":"日语","v":"日语"},
+    {"n":"其它","v":"其它"}
+    ]
+    },
+        {"key":"letter","name":"字母","value":[
+    {"n":"全部","v":""},
+    {"n":"A","v":"A"},
+    {"n":"B","v":"B"},
+    {"n":"C","v":"C"},
+    {"n":"D","v":"D"},
+    {"n":"E","v":"E"},
+    {"n":"F","v":"F"},
+    {"n":"G","v":"G"},
+    {"n":"H","v":"H"},
+    {"n":"I","v":"I"},
+    {"n":"J","v":"J"},
+    {"n":"K","v":"K"},
+    {"n":"L","v":"L"},
+    {"n":"M","v":"M"},
+    {"n":"N","v":"N"},
+    {"n":"O","v":"O"},
+    {"n":"P","v":"P"},
+    {"n":"Q","v":"Q"},
+    {"n":"R","v":"R"},
+    {"n":"S","v":"S"},
+    {"n":"T","v":"T"},
+    {"n":"U","v":"U"},
+    {"n":"V","v":"V"},
+    {"n":"W","v":"W"},
+    {"n":"X","v":"X"},
+    {"n":"Y","v":"Y"},
+    {"n":"Z","v":"Z"},
+    {"n":"0-9","v":"0-9"}
+    ]
+    },
+    {"key":"by","name":"排序","value":[
+    {"n":"全部排序","v":""},
+    {"n":"时间排序","v":"time"},
+    {"n":"人气排序","v":"hits"},
+    {"n":"评分排序","v":"score"}
+    ]
+    }
+    ],
+    "3":[
+        {"key":"class","name":"剧情","value":[
+{"n":"全部剧情","v":""},
+{"n":"真人秀","v":"真人秀"},
+{"n":"脱口秀","v":"脱口秀"},
+{"n":"晚会","v":"晚会"},
+{"n":"选秀","v":"选秀"},
+{"n":"情感","v":"情感"},
+{"n":"音乐","v":"音乐"},
+{"n":"美食","v":"美食"}
+    ]
+    },
+    
+    {"key":"area","name":"地区","value":[
+    {"n":"全部地区","v":""},
+    {"n":"大陆","v":"大陆"},
+    {"n":"韩国","v":"韩国"},
+    {"n":"日本","v":"日本"},
+    {"n":"香港","v":"香港"},
+    {"n":"台湾","v":"台湾"},
+    {"n":"美国","v":"美国"}
+    ]
+    },
+    {"key":"year","name":"时间","value":[
+    {"n":"全部时间","v":""},
+    {"n":"2024","v":"2024"},
+    {"n":"2023","v":"2023"},
+    {"n":"2022","v":"2022"},
+    {"n":"2021","v":"2021"},
+    {"n":"2020","v":"2020"},
+    {"n":"2019","v":"2019"},
+    {"n":"2018","v":"2018"},
+    {"n":"2017","v":"2017"},
+    {"n":"2016","v":"2016"},
+    {"n":"2015","v":"2015"},
+    {"n":"2014","v":"2014"},
+    {"n":"2013","v":"2013"},
+    {"n":"2012","v":"2012"},
+    {"n":"2011","v":"2011"},
+    {"n":"2010","v":"2010"},
+    {"n":"2009","v":"2009"},
+    {"n":"2008","v":"2008"},
+    {"n":"2007","v":"2007"}
+    ]
+    },
+    {"key":"lang","name":"语言","value":[
+    {"n":"全部语言","v":""},
+    {"n":"国语","v":"国语"},
+    {"n":"英语","v":"英语"},
+    {"n":"粤语","v":"粤语"},
+    {"n":"闽南语","v":"闽南语"},
+    {"n":"韩语","v":"韩语"},
+    {"n":"日语","v":"日语"},
+    {"n":"其它","v":"其它"}
+    ]
+    },
+        {"key":"letter","name":"字母","value":[
+    {"n":"全部","v":""},
+    {"n":"A","v":"A"},
+    {"n":"B","v":"B"},
+    {"n":"C","v":"C"},
+    {"n":"D","v":"D"},
+    {"n":"E","v":"E"},
+    {"n":"F","v":"F"},
+    {"n":"G","v":"G"},
+    {"n":"H","v":"H"},
+    {"n":"I","v":"I"},
+    {"n":"J","v":"J"},
+    {"n":"K","v":"K"},
+    {"n":"L","v":"L"},
+    {"n":"M","v":"M"},
+    {"n":"N","v":"N"},
+    {"n":"O","v":"O"},
+    {"n":"P","v":"P"},
+    {"n":"Q","v":"Q"},
+    {"n":"R","v":"R"},
+    {"n":"S","v":"S"},
+    {"n":"T","v":"T"},
+    {"n":"U","v":"U"},
+    {"n":"V","v":"V"},
+    {"n":"W","v":"W"},
+    {"n":"X","v":"X"},
+    {"n":"Y","v":"Y"},
+    {"n":"Z","v":"Z"},
+    {"n":"0-9","v":"0-9"}
+    ]
+    },
+    {"key":"by","name":"排序","value":[
+    {"n":"全部排序","v":""},
+    {"n":"时间排序","v":"time"},
+    {"n":"人气排序","v":"hits"},
+    {"n":"评分排序","v":"score"}
+    ]
+    }
+    ],
+    "4":[
+        {"key":"class","name":"剧情","value":[
+{"n":"全部剧情","v":""},
+{"n":"科幻","v":"科幻"},
+{"n":"热血","v":"热血"},
+{"n":"推理","v":"推理"},
+{"n":"搞笑","v":"搞笑"},
+{"n":"冒险","v":"冒险"},
+{"n":"动作","v":"动作"},
+{"n":"运动","v":"运动"},
+{"n":"少女","v":"少女"},
+{"n":"励志","v":"励志"}
+    ]
+    },
+        {"key":"area","name":"地区","value":[
+    {"n":"全部地区","v":""},
+    {"n":"大陆","v":"大陆"},
+    {"n":"韩国","v":"韩国"},
+    {"n":"日本","v":"日本"},
+    {"n":"香港","v":"香港"},
+    {"n":"台湾","v":"台湾"},
+    {"n":"美国","v":"美国"}
+    ]
+    },
+    {"key":"lang","name":"语言","value":[
+    {"n":"全部语言","v":""},
+    {"n":"国语","v":"国语"},
+    {"n":"英语","v":"英语"},
+    {"n":"粤语","v":"粤语"},
+    {"n":"闽南语","v":"闽南语"},
+    {"n":"韩语","v":"韩语"},
+    {"n":"日语","v":"日语"},
+    {"n":"其它","v":"其它"}
+    ]
+    },
+        {"key":"year","name":"时间","value":[
+    {"n":"全部时间","v":""},
+    {"n":"2024","v":"2024"},
+    {"n":"2023","v":"2023"},
+    {"n":"2022","v":"2022"},
+    {"n":"2021","v":"2021"},
+    {"n":"2020","v":"2020"},
+    {"n":"2019","v":"2019"},
+    {"n":"2018","v":"2018"},
+    {"n":"2017","v":"2017"},
+    {"n":"2016","v":"2016"},
+    {"n":"2015","v":"2015"},
+    {"n":"2014","v":"2014"},
+    {"n":"2013","v":"2013"},
+    {"n":"2012","v":"2012"},
+    {"n":"2011","v":"2011"},
+    {"n":"2010","v":"2010"},
+    {"n":"2009","v":"2009"},
+    {"n":"2008","v":"2008"},
+    {"n":"2007","v":"2007"}
+    ]
+    },
+    {"key":"letter","name":"字母","value":[
+    {"n":"全部","v":""},
+    {"n":"A","v":"A"},
+    {"n":"B","v":"B"},
+    {"n":"C","v":"C"},
+    {"n":"D","v":"D"},
+    {"n":"E","v":"E"},
+    {"n":"F","v":"F"},
+    {"n":"G","v":"G"},
+    {"n":"H","v":"H"},
+    {"n":"I","v":"I"},
+    {"n":"J","v":"J"},
+    {"n":"K","v":"K"},
+    {"n":"L","v":"L"},
+    {"n":"M","v":"M"},
+    {"n":"N","v":"N"},
+    {"n":"O","v":"O"},
+    {"n":"P","v":"P"},
+    {"n":"Q","v":"Q"},
+    {"n":"R","v":"R"},
+    {"n":"S","v":"S"},
+    {"n":"T","v":"T"},
+    {"n":"U","v":"U"},
+    {"n":"V","v":"V"},
+    {"n":"W","v":"W"},
+    {"n":"X","v":"X"},
+    {"n":"Y","v":"Y"},
+    {"n":"Z","v":"Z"},
+    {"n":"0-9","v":"0-9"}
+    ]
+    },
+    {"key":"by","name":"排序","value":[
+    {"n":"全部排序","v":""},
+    {"n":"时间排序","v":"time"},
+    {"n":"人气排序","v":"hits"},
+    {"n":"评分排序","v":"score"}]}]
+	},
+  
+	filter_def:{
+    	5:{cateId:'5'},
+		1:{cateId:'1'},
+		2:{cateId:'2'},
+		3:{cateId:'3'},
+		4:{cateId:'4'}
+	},
+
+  class_parse: ".nav-menu-items&&li;a&&Text;a&&href;(\\d+)",
+	lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+	搜索: '.module-items .module-search-item;.video-serial&&title;img&&data-src;.video-serial&&Text;.video-serial&&href;.video-serial&&Text',
 }
