d85895983df51bb0b58e21b5ca6c8f312eb1f305
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-01 13:49:20 -0600

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -2,7 +2,7 @@ var rule = {
     title: '爱迪影视',
     host: 'https://bcvod.com',
     //host:'https://adys123.com',  //网址发布站
-    url:'/show/fyclass--------fypage---.html',
+    url:'/vodshow/fyclass--------fypage---.html',
 	//https://bcvod.com/vodshow/dianshiju--------2---.html
     //url: '/vodshow/fyfilter.html',
     filterable: 0,//是否启用分类筛选,
