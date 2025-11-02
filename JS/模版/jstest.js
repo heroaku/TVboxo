674a9abed36d2d2aacd01680fc28723c26df3f72
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-02 08:19:06 -0700

    Remove unused rule object from jstest.js
    Removed unused rule object and its properties.

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -29,25 +29,3 @@ double: true, // 推荐内容是否双层定位
     "lists": ".module-list:eq(#id)&&.sort-item&&a"
 }, 搜索: '.module-items&&.module-search-item;a&&title;img&&data-src;.video-info&&a&&Text;a&&href',
 }
-
-
-
-var rule={
-	title:'MX影院',
-
-	模板:'mxone5',
-  host: "https://mxvod.com",
-	//https://mxvod.com/vodshow/fyclass--------fypage---.html
-	url: "/vodshow/fyclass--------fypage---.html",
-  //url: "/vs/fyclassfyfilter.html",
-  searchUrl: "/vodsearch/**----------fypage---.html",
-//https://mxvod.com/vodsearch/**----------fypage---.html
-  //filterable: 1,
-	filterable: 0, 
-
-
-    class_parse: '.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
-	cate_exclude: '最新更新',
-	lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
-	搜索: '.module-items .module-search-item;.video-serial&&title;img&&data-src;.video-serial&&Text;.video-serial&&href;.video-serial&&Text',
-}
