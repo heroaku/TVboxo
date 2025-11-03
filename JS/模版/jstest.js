90759a4a33792abd40cbed22643b477a1e9561a3
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-02 18:58:27 -0700

    Fix selector for lists in jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -21,7 +21,7 @@ var rule = {
            'desc': '.stui-content__detail p:eq(3)&&Text;.stui-content__detail p:eq(0)&&Text;.stui-content__detail p:eq(2)&&Text;.stui-content__detail p:eq(1)&&Text;',
            'content': '.stui-content__detail p:eq(4)&&Text;',
            'tabs':'h3',
-           'lists':'.stui-pannel-box:eq(#id)&&.stui-content__playlist li'
+           'lists':'.stui-pannel_bd:eq(#id)&&.stui-content__playlist li'
          },
     搜索: 'ul.stui-vodlist__media:eq(0) li,ul.stui-vodlist:eq(0) li,#searchList li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href;.text-muted:eq(-1)&&Text'
     }
