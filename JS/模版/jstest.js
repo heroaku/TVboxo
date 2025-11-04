53aa6626c35b2f7b7ecd4f4ef5e93c01fd767a76
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-04 13:02:38 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,28 +1,30 @@
-var rule = {
-  title: '可视影院',
-  host: 'https://www.ketv.cc/',
-  url: '/s/fyclass--------fypage---.html',
-//https://www.ketv.cc/s/fyclass--------fypage---.html
-  searchUrl: '/vod-search-page-fypage-wd-**.html',
-  searchable: 2,
-  quickSearch: 0,
-  filterable: 0,
-  //class_parse: '.top_nav.clearfix li;a&&Text;a&&href;/.*-(.*?).html',
-  class_name:'电影&电视剧&综艺&动漫&短剧&体育&国产剧&香港剧&台湾剧&韩国剧&日本剧&欧美剧&海外剧&纪录片',
-  class_url:'movie&series&variety&anime&skit&sports&china&hongkong&taiwan&korea&japan&eus&overseas&documentary',
-  play_parse: true,
-  lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
-  limit: 6,
-  推荐: '.cbox_list li;.vodlist_item;a&&title;.lazyload&&data-original;.pic_text&&Text;a&&href',
-  double: true,
-  一级: '.vodlist_item;a&&title;.lazyload&&data-original;.pic_text&&Text;a&&href',
-  二级: {
-    title: '.hd_tit.fl&&Text',
-    img: '.lazyload&&data-original',
-    desc: '.text_muted hidden_xs:eq(0)&&Text;.text_muted hidden_xs:eq(1)&&Text;.text_muted hidden_xs:eq(2)&&Text;.text_muted hidden_xs:eq(3)&&Text',
-    content: '.left.text_muted&&Text',
-    tabs: '#NumTab&&a',
-    lists: '.play_list_box:eq(#id)&&.playlist_notfull&&a',
-  },
-  搜索: '.vodlist_item;a&&title;.lazyload&&data-original;.pic_text&&Text;a&&href',
-}
+var rule={
+    title: '耐看',
+    host: 'https://qxys.nbcys.eu.org',
+    url: '/vds/fyclass--------fypage---.html',
+  //https://qxys.nbcys.eu.org/vds/1--------2---.html
+    searchUrl: '/nk/-------------.html?wd=**',
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 0,
+    headers: {
+    'User-Agent': 'MOBILE_UA',
+    },
+    class_parse: '.navbar&&ul&&li;a&&Text;a&&href;/(\\d+).html',
+    play_parse: false,
+    lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+    limit: 6,
+    推荐: '.module-items;a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    double: true,
+    一级: 'a.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    二级: {
+    "title": "h1&&Text;.module-info-tag&&Text",
+    "img": ".lazyload&&data-original",
+    "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
+    "content": ".module-info-introduction&&Text",
+    "tabs": ".hisSwiper&&span",
+    "lists": ".his-tab-list:eq(#id) a"},
+    搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',}
+
+
+
