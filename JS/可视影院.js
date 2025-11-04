ae442bafb4de3a808742956c4e57dd6ac0fd5ccb
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-04 12:52:07 -0700

    Create 可视影院.js

---
+++ JS/可视影院.js
@@ -0,0 +1,28 @@
+var rule = {
+  title: '可视影院',
+  host: 'https://www.ketv.cc/',
+  url: '/s/fyclass--------fypage---.html',
+//https://www.ketv.cc/s/fyclass--------fypage---.html
+  searchUrl: '/vod-search-page-fypage-wd-**.html',
+  searchable: 2,
+  quickSearch: 0,
+  filterable: 0,
+  //class_parse: '.top_nav.clearfix li;a&&Text;a&&href;/.*-(.*?).html',
+  class_name:'电影&电视剧&综艺&动漫&短剧&体育&国产剧&香港剧&台湾剧&韩国剧&日本剧&欧美剧&海外剧&纪录片',
+  class_url:'movie&series&variety&anime&skit&sports&china&hongkong&taiwan&korea&japan&eus&overseas&documentary',
+  play_parse: true,
+  lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+  limit: 6,
+  推荐: '.cbox_list li;.vodlist_item;a&&title;.lazyload&&data-original;.pic_text&&Text;a&&href',
+  double: true,
+  一级: '.vodlist_item;a&&title;.lazyload&&data-original;.pic_text&&Text;a&&href',
+  二级: {
+    title: '.hd_tit.fl&&Text',
+    img: '.lazyload&&data-original',
+    desc: '.text_muted hidden_xs:eq(0)&&Text;.text_muted hidden_xs:eq(1)&&Text;.text_muted hidden_xs:eq(2)&&Text;.text_muted hidden_xs:eq(3)&&Text',
+    content: '.left.text_muted&&Text',
+    tabs: '#NumTab&&a',
+    lists: '.play_list_box:eq(#id)&&.playlist_notfull&&a',
+  },
+  搜索: '.vodlist_item;a&&title;.lazyload&&data-original;.pic_text&&Text;a&&href',
+}
