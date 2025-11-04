ba0b5e5bb21ffa498c78b4b53a69ba9a4d488f90
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-04 13:15:07 -0700

    Create 旗星影视.js

---
+++ JS/旗星影视.js
@@ -0,0 +1,30 @@
+var rule={
+    title: '旗星影视',
+    host: 'https://qxys.nbcys.eu.org',
+    url: '/vds/fyclass--------fypage---.html',
+    //https://qxys.nbcys.eu.org/vds/1--------2---.html
+    //https://qxys.nbcys.eu.org/vdh/**-/page/fypage.html
+    searchUrl: '/vdh/**-/page/fypage.html',
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 0,
+    headers: {
+    'User-Agent': 'MOBILE_UA',
+    },
+    class_parse: '.navbar&&ul&&li;a&&Text;a&&href;/(\\d+).html',
+    play_parse: false,
+    lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+    limit: 6,
+    推荐: '.module-items;a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    double: true,
+    一级: 'a.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    二级: {
+    "title": "h1&&Text;.module-info-tag&&Text",
+    "img": ".lazyload&&data-original",
+    "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
+    "content": ".module-info-introduction&&Text",
+    "tabs": ".hisSwiper&&span",
+    "lists": ".his-tab-list:eq(#id) a"},
+    搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',}
+
+
