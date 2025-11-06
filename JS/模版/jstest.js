fa57463e6861cef1c5d32cd54b4b8dbc631e48e1
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-06 11:11:06 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,74 +1,25 @@
-var rule = {
-author: '小可乐/240701/第一版',
-title: '2345one',
-类型: '影视',
-host: 'http://142.171.220.28',
-hostJs: '',
-headers: {'User-Agent': 'MOBILE_UA'},
-编码: 'utf-8',
-timeout: 5000,
-
-homeUrl: '/',
-url: '/vodshow/fyfilter',
-filter_url: '{{fl.cateId}}-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
-detailUrl: '',
-searchUrl: '/vodsearch/**----------fypage---',
-searchable: 1, 
-quickSearch: 1, 
-filterable: 1, 
-
-class_name: '电影天堂&电视剧集&樱花动漫&综艺娱乐',
-class_url: 'dianying&dianshiju&dongman&zongyi',
-filter_def: {
-dianying: {cateId: 'dianying'},
-dianshiju: {cateId: 'dianshiju'},
-dongman: {cateId: 'dongman'},
-zongyi: {cateId: 'zongyi'}
-},
-
-play_parse: true,
-lazy: `js:
-let kcode = JSON.parse(request(input).match(/var player_.*?=(.*?)</)[1]);
-let kurl = kcode.url;
-if (/\\.(m3u8|mp4)/.test(kurl)) {
-input = { jx: 0, parse: 0, url: kurl };
-} else {
-input = { jx: 0, parse: 1, url: kurl };
-}
-`,
-
-limit: 9,
-double: false,
-推荐: '*',
-一级: 'li.col-lg-2;a:eq(0)&&title;.lazyload&&data-original;p&&Text;a:eq(0)&&href',
-二级: `js:
-let html = request(input);
-VOD = {};
-VOD.vod_id = input;
-VOD.vod_name = pdfh(html, '.info&&h3&&Text');
-VOD.vod_pic = pd(html, 'img&&data-original', input);
-VOD.type_name = pdfh(html, 'p.row&&span:eq(0)&&Text').replace('分类：','');
-VOD.vod_remarks = pdfh(html, 'p.row&&span:eq(-1)&&Text');
-VOD.vod_year = pdfh(html, 'p.row&&span:eq(2)&&Text').replace('年份：','');
-VOD.vod_area = pdfh(html, 'p.row&&span:eq(1)&&Text').replace('地区：','');
-VOD.vod_actor = pdfh(html, 'p.row&&span:eq(-2)&&Text').replace('主演：','');
-VOD.vod_director = pdfh(html, 'p.row&&span:eq(-3)&&Text').replace('导演：','');
-VOD.vod_content = pdfh(html, '.pb-10&&Text');
-
-let ktabs = pdfa(html,'.ewave-tab').map((it) => { return pdfh(it, 'body&&Text') });
-VOD.vod_play_from = ktabs.join('$$$');
-
-let klists = [];
-pdfa(html, '.ewave-playlist-content').forEach((pl) => {
-    let klist = pdfa(pl, 'body&&a').map((it) => {
-    return pdfh(it, 'a&&Text') + '$' + pd(it, 'a&&href', input);
-    });
-    klist = klist.join('#');
-    klists.push(klist)
+Object.assign(muban.首图2.二级, {
+  title: '.stui-vodlist__thumb&&title;.stui-content__detail p&&Text',
+  desc: '.stui-content__detail p:eq(3)&&Text;.stui-content__detail a:eq(4)&&Text;.stui-content__detail a:eq(3)&&Text;.stui-content__detail p:eq(1)&&Text;.stui-content__detail p:eq(2)&&Text',
+  content: '.stui-content__detail p:eq(4)&&Text',
+  //tabs: '.stui-pannel__head h3',
 });
-VOD.vod_play_url = klists.join('$$$')
-`,
-搜索: '*',
-
-filter: 'H4sIAAAAAAAAA+2aW1MTSRTH3/kYeWarJrhe1jfv9/tdy4espiQsYpXAbrEWVWAAw8UELCQiMYCAgBJIwMUQSPgy9MzkW2yTPpce3JpMoSVl7bxY+f3PdE+f7p6Z/7F5VhN4GAk1tUWaHgUO36t5Fvgj3BY4HHjQGGpuDtQGmkKPwxJF76wZ7Zb8Z6ixVQr3ngWatuXuuXJ0bluWEGivBXUkJa8HFQBjViwHHTFQu765rWIK2ymgdrNDYm0d2ynQ2lnD69xuGzBmPl+wRoYgBkB99i1ZxY/YpwJqF+0zn7/Fdgrofj2vyqPzeD8FGCuPr2ytDUIMgNrFe0RiGdspoNjMC84PgMbSOWh2jOBYFFAs9mar0IsxBRjb2piwF7MQA6B2mQE7lsZ2Cjj2Yas0QbEK8Lx0273LNC8VoPnsLJXflnA+FWDMntnknQJA7QofRfG11fsCmxLTFeMZLayg/X57LW3X0NNwSNutqawYKHjdrS/j9lIGZ14BZbswa5XimK0CXul5MVakla4AtUvOmKkFbKeAViWfkZeK6dnyaA+ujS5RzqU49w/g7KP8YdTMLzn6AGnHvRJZc63kvJeSaHX6c3wvAMpl5TXHAGjuSl84BkDtuhIyIxHDB4VZ2xNWQq7lKG8LYF6brCh8oLWpAI8rq48r65ifzS6rmDRHaHKI+Z0xYfZvygHRawOZ+i+tqPFuFeg9oEvaG01kh0U3PmjMdMVQWnTnMayAxvHls0wbB6GAV2+tTNkDOHZ9Y0i+tGnXy41rz3V43fVjRW3XK9B2A8cAKJflaY4B0BORLIqXSQ4za8+MFlagPTMcA9D2oBZToO1BLRMFFOteFYtRjClwzGBbOPSUZ9BMrpaTnz3OYJ1R9ytolZ+avo/1fbpex3qdrgdZD+q6wbqh6cHfSJc/Nf0Q64d0/SDrB3X9AOsHdH0/6/t1nfMN6vkGOd+gnm+Q8w3q+QY536Ceb5DzlT+dOz3c0hLWVkpkkubSS48rdQSEI6QcBeUoKcdAOUbKcVCOk3IClBOknATlJCmnQDlFymlQTpNyBpQzpJwF5Swp50A5R8p5UM6TcgGUC6RcBOUiKZdAuUTKZVAuk3IFlCukXAXlKinXQLlGynVQrpNyA5QbpNwE5SYpt0C5RcptUG6TcgeUO6TcBeUuKcYv+BBs/3Lsld/btCc6/koUEl/tEzPVYW+iK2yJyEvpTZPqENP4vq+PtDQ7IjPyPdkFweYHT56Gt+9cc79Weejm+khD616baDfD62qUXUyfSEzbU3g/AIotrooCvX4VeDHYbobezfC6mWg3M+xWXLgZejcTzQvK4MnQuxQXbibazbT/Z+Gxxxa5YmpxHQF+lH32apHdbLa7LXaxnx5td3WLvFvzXd1aV7fI39W2ejD01S15datd1bT/L+2zb4N/YhtMusH5Gnq+Budr6PkanK+h52twvoaer8H5Gnq+Budr6PkanK+h52twvoaer8H5Gnq+Budr+Lbft/0/le1/0vTocajJYfpDLeEzD3ksVm5dpPu/Hstczu7LSSNqbnyCrrEzp123hv8RxZx2SX1ryJn5N5UZ8XERGyM7XQFKfeMTlwsA1C6bsJc+YzsF1C6fN2MJbKeAPoC9a+YQziUA9ZmP2u/7sU8F1C6asSc7sJ0Cis1v2qt9GFPgyYbH3pgjSS4XtoFig2lrgcooBdqnVuTRGAF4KWvswQFrBWMA1OfyutkVwz4VUGxiUozRp10Br8OQ6CIjpoDN42T53XsyjxXgEqtfuieMKdD7nFnR+pRAFiQzLG0oWhAFXsoo17ItvWrNF3AsCvjxK8iVwZgCmuuZtBwBzrUCut9U1oyTk1RA67A5KB8tXAcF/MzlxCbuCQDXEgvHGZ+zBrEgAaBxTpfEGK4DALUbLWh7UAGN5fmCuYLlM4BWtpSnxrFPBdSuc1VE8fkDIHO+NsprC0Dz0jkgUrgOAHtdQvqnLP4pi7fSzz9l+dYy0T9l8ctL/OmXl3556ZeXe11e/i3LvbbILqpLa33D7i2IuRwbVuiLPjHqj2zkv4t4otAQaWzl+HhGFpJWcYhPof6SH8IGvkJWpfZsjz07xYVgQyTcXN/65PuVp7v9czG30zO3UyLXPzNzOe1xPT1zKYvM9EetvFHAJZrLKVhp0fH/AsyeP8rfYuZ1W73DUDvs8g6j7LDIO8yxmxHfbRHgZl7Nkax0o+LdJIaJ6a7zC+a7GWkV2Zo6JC/2edeWv6px/h4nPFX3iG99A7719a2vb3196+tb3x9ofWva/wWzM9mblzAAAA=='
+var rule = {
+  模板: '首图2',
+  title: '234影视',
+  host: 'https://www.kan234.com',
+  url: '/show/fyclass-fyfilter.html',
+  searchUrl: '/search/**----------fypage---.html',
+  searchable: 2,
+  quickSearch: 0,
+  filterable: 1,
+  filter: {
+    "1": [{ "key": "剧情", "name": "按剧情", "value": [{ "n": "全部", "v": "" }, { "n": "喜剧", "v": "喜剧" }, { "n": "爱情", "v": "爱情" }, { "n": "恐怖", "v": "恐怖" }, { "n": "动作", "v": "动作" }, { "n": "科幻", "v": "科幻" }, { "n": "剧情", "v": "剧情" }, { "n": "战争", "v": "战争" }, { "n": "警匪", "v": "警匪" }, { "n": "犯罪", "v": "犯罪" }, { "n": "动画", "v": "动画" }, { "n": "奇幻", "v": "奇幻" }, { "n": "武侠", "v": "武侠" }, { "n": "冒险", "v": "冒险" }, { "n": "枪战", "v": "枪战" }, { "n": "恐怖", "v": "恐怖" }, { "n": "悬疑", "v": "悬疑" }, { "n": "惊悚", "v": "惊悚" }, { "n": "经典", "v": "经典" }, { "n": "青春", "v": "青春" }, { "n": "文艺", "v": "文艺" }, { "n": "情色", "v": "情色" }, { "n": "古装", "v": "古装" }, { "n": "历史", "v": "历史" }, { "n": "运动", "v": "运动" }, { "n": "农村", "v": "农村" }, { "n": "儿童", "v": "儿童" }, { "n": "同性", "v": "同性" }] }, { "key": "地区", "name": "按地区", "value": [{ "n": "全部", "v": "" }, { "n": "大陆", "v": "大陆" }, { "n": "香港", "v": "香港" }, { "n": "台湾", "v": "台湾" }, { "n": "美国", "v": "美国" }, { "n": "法国", "v": "法国" }, { "n": "英国", "v": "英国" }, { "n": "日本", "v": "日本" }, { "n": "韩国", "v": "韩国" }, { "n": "德国", "v": "德国" }, { "n": "泰国", "v": "泰国" }, { "n": "印度", "v": "印度" }, { "n": "意大利", "v": "意大利" }, { "n": "西班牙", "v": "西班牙" }, { "n": "加拿大", "v": "加拿大" }, { "n": "其他", "v": "其他" }] }, { "key": "年份", "name": "按年份", "value": [{ "n": "全部", "v": "" }, { "n": "2025", "v": "2025" }, { "n": "2024", "v": "2024" }, { "n": "2023", "v": "2023" }, { "n": "2022", "v": "2022" }, { "n": "2021", "v": "2021" }, { "n": "2020", "v": "2020" }, { "n": "2019", "v": "2019" }, { "n": "2018", "v": "2018" }, { "n": "2017", "v": "2017" }, { "n": "2016", "v": "2016" }, { "n": "2015", "v": "2015" }, { "n": "2014", "v": "2014" }, { "n": "2013", "v": "2013" }, { "n": "2012", "v": "2012" }, { "n": "2011", "v": "2011" }, { "n": "2010", "v": "2010" }] }, { "key": "排序", "name": "排序", "value": [{ "n": "按最新", "v": "time" }, { "n": "按最热", "v": "hits" }, { "n": "按评分", "v": "score" }] }],
+    "2": [{ "key": "剧情", "name": "按剧情", "value": [{ "n": "全部", "v": "" }, { "n": "剧情", "v": "剧情" }, { "n": "古装", "v": "古装" }, { "n": "武侠", "v": "武侠" }, { "n": "爱情", "v": "爱情" }, { "n": "喜剧", "v": "喜剧" }, { "n": "家庭", "v": "家庭" }, { "n": "犯罪", "v": "犯罪" }, { "n": "动作", "v": "动作" }, { "n": "奇幻", "v": "奇幻" }, { "n": "剧情", "v": "剧情" }, { "n": "历史", "v": "历史" }, { "n": "战争", "v": "战争" }, { "n": "经典", "v": "经典" }, { "n": "乡村", "v": "乡村" }, { "n": "情景", "v": "情景" }, { "n": "商战", "v": "商战" }, { "n": "网剧", "v": "网剧" }, { "n": "其他", "v": "其他" }] }, { "key": "地区", "name": "按地区", "value": [{ "n": "全部", "v": "" }, { "n": "大陆", "v": "大陆" }, { "n": "韩国", "v": "韩国" }, { "n": "香港", "v": "香港" }, { "n": "台湾", "v": "台湾" }, { "n": "日本", "v": "日本" }, { "n": "美国", "v": "美国" }, { "n": "泰国", "v": "泰国" }, { "n": "英国", "v": "英国" }, { "n": "新加坡", "v": "新加坡" }, { "n": "其他", "v": "其他" }] }, { "key": "年份", "name": "按年份", "value": [{ "n": "全部", "v": "" }, { "n": "2025", "v": "2025" }, { "n": "2024", "v": "2024" }, { "n": "2023", "v": "2023" }, { "n": "2022", "v": "2022" }, { "n": "2021", "v": "2021" }, { "n": "2020", "v": "2020" }, { "n": "2019", "v": "2019" }, { "n": "2018", "v": "2018" }, { "n": "2017", "v": "2017" }, { "n": "2016", "v": "2016" }, { "n": "2015", "v": "2015" }, { "n": "2014", "v": "2014" }, { "n": "2013", "v": "2013" }, { "n": "2012", "v": "2012" }, { "n": "2011", "v": "2011" }, { "n": "2010", "v": "2010" }] }, { "key": "排序", "name": "排序", "value": [{ "n": "按最新", "v": "time" }, { "n": "按最热", "v": "hits" }, { "n": "按评分", "v": "score" }] }],
+    "3": [{ "key": "剧情", "name": "按剧情", "value": [{ "n": "全部", "v": "" }, { "n": "真人秀", "v": "真人秀" }, { "n": "音乐", "v": "音乐" }, { "n": "爱情", "v": "爱情" }, { "n": "家庭", "v": "家庭" }, { "n": "悬疑", "v": "悬疑" }, { "n": "喜剧", "v": "喜剧" }, { "n": "旅游", "v": "旅游" }, { "n": "访谈", "v": "访谈" }, { "n": "情感", "v": "情感" }] }, { "key": "地区", "name": "按地区", "value": [{ "n": "全部", "v": "" }, { "n": "大陆", "v": "大陆" }, { "n": "香港", "v": "香港" }, { "n": "台湾", "v": "台湾" }, { "n": "韩国", "v": "韩国" }, { "n": "日本", "v": "日本" }, { "n": "美国", "v": "美国" }] }, { "key": "年份", "name": "按年份", "value": [{ "n": "全部", "v": "" }, { "n": "2025", "v": "2025" }, { "n": "2024", "v": "2024" }, { "n": "2023", "v": "2023" }, { "n": "2022", "v": "2022" }, { "n": "2021", "v": "2021" }, { "n": "2020", "v": "2020" }, { "n": "2019", "v": "2019" }, { "n": "2018", "v": "2018" }, { "n": "2017", "v": "2017" }, { "n": "2016", "v": "2016" }, { "n": "2015", "v": "2015" }, { "n": "2014", "v": "2014" }, { "n": "2013", "v": "2013" }, { "n": "2012", "v": "2012" }, { "n": "2011", "v": "2011" }, { "n": "2010", "v": "2010" }, { "n": "2009", "v": "2009" }, { "n": "2008", "v": "2008" }] }, { "key": "排序", "name": "排序", "value": [{ "n": "按最新", "v": "time" }, { "n": "按最热", "v": "hits" }, { "n": "按评分", "v": "score" }] }],
+    "4": [{ "key": "剧情", "name": "按剧情", "value": [{ "n": "全部", "v": "" }, { "n": "剧情", "v": "剧情" }, { "n": "奇幻", "v": "奇幻" }, { "n": "冒险", "v": "冒险" }, { "n": "爱情", "v": "爱情" }, { "n": "同性", "v": "同性" }, { "n": "肉番", "v": "肉番" }, { "n": "动作", "v": "动作" }, { "n": "古装", "v": "古装" }, { "n": "悬疑", "v": "悬疑" }, { "n": "惊悚", "v": "惊悚" }, { "n": "犯罪", "v": "犯罪" }, { "n": "未知", "v": "未知" }] }, { "key": "地区", "name": "按地区", "value": [{ "n": "全部", "v": "" }, { "n": "大陆", "v": "大陆" }, { "n": "日本", "v": "日本" }, { "n": "韩国", "v": "韩国" }, { "n": "美国", "v": "美国" }, { "n": "其他", "v": "其他" }] }, { "key": "年份", "name": "按年份", "value": [{ "n": "全部", "v": "" }, { "n": "2025", "v": "2025" }, { "n": "2024", "v": "2024" }, { "n": "2023", "v": "2023" }, { "n": "2022", "v": "2022" }, { "n": "2021", "v": "2021" }, { "n": "2020", "v": "2020" }, { "n": "2019", "v": "2019" }, { "n": "2018", "v": "2018" }, { "n": "2017", "v": "2017" }, { "n": "2016", "v": "2016" }, { "n": "2015", "v": "2015" }, { "n": "2014", "v": "2014" }, { "n": "2013", "v": "2013" }, { "n": "2012", "v": "2012" }, { "n": "2011", "v": "2011" }, { "n": "2010", "v": "2010" }, { "n": "2009", "v": "2009" }, { "n": "2008", "v": "2008" }] }, { "key": "排序", "name": "排序", "value": [{ "n": "按最新", "v": "time" }, { "n": "按最热", "v": "hits" }, { "n": "按评分", "v": "score" }] }],
+    "5": [{ "key": "剧情", "name": "按剧情", "value": [{ "n": "全部", "v": "" }, { "n": "剧情", "v": "剧情" }, { "n": "喜剧", "v": "喜剧" }, { "n": "爱情", "v": "爱情" }, { "n": "穿越", "v": "穿越" }] }, { "key": "地区", "name": "按地区", "value": [{ "n": "全部", "v": "" }, { "n": "大陆", "v": "大陆" }] }, { "key": "年份", "name": "按年份", "value": [{ "n": "全部", "v": "" }, { "n": "2025", "v": "2025" }, { "n": "2024", "v": "2024" }, { "n": "2023", "v": "2023" }, { "n": "2022", "v": "2022" }, { "n": "2021", "v": "2021" }, { "n": "2020", "v": "2020" }] }, { "key": "排序", "name": "排序", "value": [{ "n": "按最新", "v": "time" }, { "n": "按最热", "v": "hits" }, { "n": "按评分", "v": "score" }] }]
+  },
+  filter_url: '{{fl.地区}}-{{fl.排序}}-{{fl.剧情}}-----fypage---{{fl.年份}}',
+  tab_exclude: '排序|剧情简介|猜你喜欢|国产剧周榜单|国产剧月榜单',
 }
