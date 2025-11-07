ebf45019e39f9b6051a009f76c99fe111ad44999
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-07 14:21:26 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -2,7 +2,7 @@ var rule={
     title: '映像星球',
     host: 'https://www.937tv.vip',
     url: '/vodshow/fyclass--------fypage---.html',
-    searchUrl: '/search/**----------fypage---.html',
+    searchUrl: '/vodsearch/**----------fypage---.html',
   //https://www.937tv.vip/vodshow/lianxuju-----------.html
     //https://www.55yy7.com/vodsearch/**----------fypage---.html
     searchable: 2,
@@ -13,7 +13,7 @@ var rule={
     },
     //class_parse: '.head-nav&&ul&&li;a&&Text;a&&href;/(\\d+).html',
     class_name:'电影&电视剧&综艺&动漫&短剧',
-    class_url:'dianying&lianxuju&zongyi&dongman&duanju',
+    class_url:'dianying&lianxuju&zongyipian&dongman&duanju',
     play_parse: false,
     lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
     limit: 6,
@@ -21,7 +21,7 @@ var rule={
     double: true,
     一级: '.public-list-box;a&&title;img&&data-src;.public-list-prb&&Text;a&&href',
     二级: {
-    "title": "h1&&Text;.module-info-tag&&Text",
+    "title": ".this-desc-title&&Text;.focus-item-label-original&&Text",
     "img": ".lazyload&&data-original",
     "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
     "content": ".module-info-introduction&&Text",
