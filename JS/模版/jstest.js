88fe3abc4c7bf32f425ad0a399a7dd941677cf9c
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-09 07:32:39 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -23,7 +23,7 @@ var rule= {
         "img": "img&&data-original",
         "desc": ';;;.video-info-actor:eq(1)&&Text;.video-info-actor:eq(0)&&Text',
         "content": ".stui-pannel:eq(2)&&p&&Text",
-        "tabs": ".stui-pannel__head.bottom-line h3",
+        "tabs": ".stui-pannel:gt(0) h3",
         "lists": ".stui-pannel_bd:eq(#id)&&.stui-content__playlist li"
     },
     搜索:'.stui-vodlist__media li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
