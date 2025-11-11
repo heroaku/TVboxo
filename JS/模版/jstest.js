dd904b9ab0cb1fea97d5e37d3faf7ddf936ef8be
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-11 15:38:11 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,30 +1,33 @@
 var rule={
-    title: '热播影视',
-    host: 'https://www.rebovod.com',
-    url: '/index.php/vod/show/id/fyclass/page/fypage.html',
-    searchUrl: '/index.php/vod/search/page/fypage/wd/**.html',
-  //https://www.rebovod.com/index.php/vod/show/id/fyclass/page/fypage.html
-    //https://www.rebovod.com/index.php/vod/search/page/fypage/wd/**.html
-    searchable: 2,
-    quickSearch: 0,
-    filterable: 0,
-    headers: {
-    'User-Agent': 'MOBILE_UA',
-    },
-    //class_parse: '.head-nav&&ul&&li;a&&Text;a&&href;/(\\d+).html',
-    class_name:'电影&电视剧&综艺&动漫&短剧',
-    class_url:'1&2&3&4&5',
-    play_parse: false,
-    lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
-    limit: 6,
-    推荐: '.public-list-box;a;a&&title;img&&data-src;.public-list-prb&&Text;a&&href',
-    double: true,
-    一级: '.public-list-box;a&&title;img&&data-src;.public-list-prb&&Text;a&&href',
-    二级: {
-    "title": ".this-desc-title&&Text;.focus-item-label-original&&Text",
-    "img": ".lazyload&&data-original",
-    "desc": ".this-desc-info&&span:eq(1)&&Text;.this-desc-info&&span:eq(2)&&Text;.this-info:eq(1)&&Text;.this-info&&Text",
-    "content": ".module-info-introduction&&Text",
-    "tabs": ".anthology-tab&&.swiper-wrapper a",
-    "lists": ".anthology-list-box:eq(#id) ul li"},
-    搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',}
+		title: '黑夜影视',
+		host: 'https://m.sdjinhengli.com',
+		url: '/kkersw/fyclass--------fypage---.html',
+		searchUrl: '/tag/page/fypage/wd/**/',
+        //https://m.sdjinhengli.com/kkersw/fyclass--------fypage---.html
+		//https://darkvod.com/tag/page/fypage/wd/**/
+		searchable: 2,//是否启用全局搜索,
+		quickSearch: 0,//是否启用快速搜索,
+		filterable: 0,//是否启用分类筛选,
+		headers: {//网站的请求头,完整支持所有的,常带ua和cookies
+		'User-Agent': 'MOBILE_UA',
+		// "Cookie": "searchneed=ok"
+		},
+		//class_parse: '.myui-header__menu li.hidden-sm:gt(0):lt(5);a&&Text;a&&href',
+        class_name:'电影&电视剧&综艺&动漫&短剧',
+        class_url:'1&2&3&4&shuangwenduanju',
+		play_parse: true,
+        lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+		limit: 6,
+		推荐: 'ul.myui-vodlist.clearfix;li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
+		double: true, // 推荐内容是否双层定位
+		一级: '.myui-vodlist li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
+		二级: {
+		"title": ".myui-content__detail .title&&Text;.myui-content__detail p:eq(-2)&&Text",
+		"img": ".myui-content__thumb .lazyload&&data-original",
+		"desc": ".myui-content__detail p:eq(0)&&Text;.myui-content__detail p:eq(1)&&Text;.myui-content__detail p:eq(2)&&Text",
+		"content": ".content&&Text",
+		"tabs": ".nav-tabs:eq(0) li",
+		"lists": ".myui-content__list:eq(#id) li"
+		},
+		搜索: '#searchList li;a&&title;.lazyload&&data-original;.text-muted&&Text;a&&href;.text-muted:eq(-1)&&Text',
+		}
