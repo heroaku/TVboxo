d22991f6f6319fd257598525b4a506f199c5d020
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-11 11:20:30 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,26 +1,30 @@
-var rule = {
-     title: '淘剧影视',
-     host: 'http://www.taoju.tv',
-	//http://www.taoju.tv/list/fyclass_fypage.html
-	//https://www.55kan.com/show/fyclass--------fypage---.html
-     //模板:'首图2',
-     searchUrl: '/vodsearch/**----------fypage---',
-     url: '/list/fyclass_fypage.html',
-     searchable: 2,//是否启用全局搜索,
-     quickSearch: 1,//是否启用快速搜索,
-     filterable: 0,//是否启用分类筛选,
-     class_name:'电视剧&电影&综艺&动漫',
-     class_url:'2&1&3&4',
-     //tab_rename:{'追番乐切':'LR',},
+var rule={
+    title: '937影院',
+    host: 'https://www.rebovod.com',
+    url: '/index.php/vod/show/id/fyclass/page/fypage.html',
+    searchUrl: '/index.php/vod/search/page/fypage/wd/**.html',
+  //https://www.rebovod.com/index.php/vod/show/id/fyclass/page/fypage.html
+    //https://www.rebovod.com/index.php/vod/search/page/fypage/wd/**.html
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 0,
+    headers: {
+    'User-Agent': 'MOBILE_UA',
+    },
+    //class_parse: '.head-nav&&ul&&li;a&&Text;a&&href;/(\\d+).html',
+    class_name:'电影&电视剧&综艺&动漫&短剧',
+    class_url:'1&2&z3&4&5',
+    play_parse: false,
     lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
-    推荐: 'ul.stui-vodlist.clearfix;li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
-    一级: '.stui-vodlist li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-    二级:{
-           'title': '.h1&&Text',
-           'desc': '.stui-content__detail p:eq(3)&&Text;.stui-content__detail p:eq(0)&&Text;.stui-content__detail p:eq(2)&&Text;.stui-content__detail p:eq(1)&&Text;',
-           'content': '.stui-content__detail p:eq(4)&&Text;',
-           'tabs':'.nav-tabs li',
-           'lists':'.stui-content__playlist:eq(#id) li'
-         },
-    搜索: 'ul.stui-vodlist__media:eq(0) li,ul.stui-vodlist:eq(0) li,#searchList li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href;.text-muted:eq(-1)&&Text'
-    }
+    limit: 6,
+    推荐: '.public-list-box;a;a&&title;img&&data-src;.public-list-prb&&Text;a&&href',
+    double: true,
+    一级: '.public-list-box;a&&title;img&&data-src;.public-list-prb&&Text;a&&href',
+    二级: {
+    "title": ".this-desc-title&&Text;.focus-item-label-original&&Text",
+    "img": ".lazyload&&data-original",
+    "desc": ".this-desc-info&&span:eq(1)&&Text;.this-desc-info&&span:eq(2)&&Text;.this-info:eq(1)&&Text;.this-info&&Text",
+    "content": ".module-info-introduction&&Text",
+    "tabs": ".anthology-tab&&.swiper-wrapper a",
+    "lists": ".anthology-list-box:eq(#id) ul li"},
+    搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',}
