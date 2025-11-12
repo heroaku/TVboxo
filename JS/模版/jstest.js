baf8478373f20fb252e609fd6a5d98ada51d36d6
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-12 15:22:39 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,10 +1,10 @@
 var rule={
-    title: '游子视频',
+    title: '夕云影视',
     host: 'https://www.xyxy.live',
     url: '/index.php/vod/show/id/fyclass/page/fypage.html',
-    searchUrl: '/vodsearch/**----------fypage---/',
+    searchUrl: '/index.php/vod/search/page/fypage/wd/**.html',
     //https://www.dam7rpv.wiki/vodshow/21--------2---.html
-    //https://www.xyxy.live/index.php/vod/show/id/fyclass/page/fypage.html
+    //https://www.xyxy.live/index.php/vod/show/id/fyclass/page/fypage.html  https://www.xyxy.live/index.php/vod/search/page/fypage/wd/**.html
     searchable: 2,
     quickSearch: 0,
     filterable: 0,
@@ -12,8 +12,8 @@ var rule={
     'User-Agent': 'MOBILE_UA',
     },
     //class_parse: '.navbar&&ul&&li;a&&Text;a&&href;/(\\d+).html',
-    class_name: '电影&电视剧&综艺&动漫&短剧',
-    class_url: '20&39&47&46&remenduanju',
+    class_name: '电影&电视剧&综艺&动漫',
+    class_url: '20&39&47&46',
     play_parse: false,
     lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
     limit: 6,
@@ -26,5 +26,5 @@ var rule={
     "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
     "content": ".module-info-introduction&&Text",
     "tabs": ".hisSwiper&&span",
-    "lists": ".his-tab-list:eq(#id) a"},
+    "lists": ".module-play-list-content:eq(#id) a"},
     搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',}
