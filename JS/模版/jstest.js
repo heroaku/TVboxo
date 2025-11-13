4790e5e987c58fe78a3330b9d7b7f9f8d413e170
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-13 14:36:03 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,30 +1,69 @@
-var rule= {
-            title: '',
-            host: 'https://6080z.com',
-            url: '/vodshow/fyclass--------fypage---.html',
-            //https://6080z.com/vodshow/2--------2---.html
-            searchUrl: '/search/**----------fypage---.html',
-            searchable: 2,//是否启用全局搜索,
-            quickSearch: 0,//是否启用快速搜索,
-            filterable: 0,//是否启用分类筛选,
-            class_parse: '.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
-            //class_name:'电影&电视剧&综艺&动漫&纪录片&国产剧&日剧&韩剧&港剧&欧美剧&泰剧&台剧',
-            //class_url:'1&2&3&4&17&18&20&22&19&21&24&23',  
-            play_parse: true,
-            lazy: '',            
-            //lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
-            limit: 6,
-            推荐: '.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
-            double: true, // 推荐内容是否双层定位
-            一级: '.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
-            二级: {
-                "title": "h1&&Text;.tag-link&&Text",
-                "img": ".module-item-pic&&img&&data-src",
-                "desc": ".video-info-items:eq(0)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(3)&&Text",
-                "content": ".vod_content&&Text",
-                "tabs": ".module-tab-item",
-                "lists": ".module-player-list:eq(#id)&&.scroll-content&&a"
-                        }, 
-            搜索: '.module-items&&.module-search-item;a&&title;img&&data-src;.video-info&&a&&Text;a&&href',
-            } 
+var rule = {
+    author: '小可乐/240701/第一版',
+    title: '天龙影院',
+    类型: '影视',
+    host: 'https://m.82mao.com',
+    hostJs: '',
+    headers: {'User-Agent': 'MOBILE_UA'},
+    编码: 'utf-8',
+    timeout: 5000,
+    homeUrl: '/',
+    url: '/List/fyfilter.html',
+    filter_url: '{{fl.cateId}}-{{fl.area}}-{{fl.by}}-{{fl.class}}--{{fl.letter}}---fypage---{{fl.year}}',
+    detailUrl: '',
+    searchUrl: '/Search/**----------fypage---.html',
+    searchable: 1,
+    quickSearch: 0,
+    filterable: 1,
 
+    class_name: '电影&剧集&综艺&动漫&短剧',
+    class_url: '17&18&20&48&53',
+    filter_def: {
+        17: {cateId: '17'},
+        18: {cateId: '18'},
+        20: {cateId: '20'},
+        48: {cateId: '48'},
+        53: {cateId: '53'}
+    },
+
+    play_parse: true,
+    parse_url: 'http://cdn.113276.xyz/cs1/?id=',
+    lazy: `js:
+var kcode = JSON.parse(request(input).match(/var player_.*?=(.*?)</)[1]);
+var kurl = kcode.url;
+if (/m3u8|mp4/.test(kurl)) {
+input = { jx: 0, parse: 0, url: kurl }
+} else {
+input = { jx: 0, parse: 1, url: rule.parse_url+kurl }
+}`,
+
+    limit: 9,
+    double: false,
+    推荐: '*',
+    一级: '.list-item;h3&&Text;img&&src;span:eq(0)&&Text;a&&href',
+    二级: {
+//名称;类型
+        title: '.bread&&Text;.info--label:eq(2)&&Text',
+//图片
+        img: '.pic&&img&&src',
+//主要描述;年份;地区;演员;导演
+        desc: '.info--label:eq(-2)&&Text;.info--label:eq(-1)&&Text;.info--label:eq(2)&&a:eq(-1)&&Text;.info--label:eq(1)&&Text;.info--label:eq(0)&&Text',
+//简介
+        content: '.detail-intro:eq(-1)&&p&&Text',
+//线路数组
+        tabs: 'li.tab-item',
+//线路标题
+        tab_text: 'font--i:eq(0)&&Text',
+//播放数组 选集列表
+        lists: '.series-list:eq(#id)&&a',
+//选集标题
+        list_text: 'body&&Text',
+//选集链接
+        list_url: 'a&&href',
+//链接处理
+        list_url_prefix: ''
+    },
+    搜索: '*',
+
+    filter: {}
+}
