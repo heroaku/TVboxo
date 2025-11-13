764a43d08fcf6a581673f5acd52a565d93bded02
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-13 13:39:37 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,30 +1,24 @@
 var rule= {
-    title:'小镇影视',
-    host:'http://dyxz.tv',
-    // http://dyxz.tv/list/fyclass_fypage.html
-    //http://www.dyxz4.com
-    url:'/list/fyclass_fypage.html',
-    searchable: 2,//是否启用全局搜索,
-    quickSearch: 0,//是否启用快速搜索,
-    filterable:1,//是否启用分类筛选,
-    class_parse: '.stui-header__menu&&li:gt(0):lt(6);a&&Text;a&&href;.*/(.*?).html',
-    //class_name:'电影&电视剧&综艺&动漫&日韩剧&国产剧&欧美剧&港台剧',
-    //class_url:'dianying&dianshiju&3&4&16&13&15&14',
-    play_parse: true,
-    lazy:'js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=="1"){url=unescape(url)}else if(html.encrypt=="2"){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}',
-    limit: 6,
-    searchUrl:'/vodsearch**/page/fypage.html',
-    detailUrl:'/detail/fyid.html', //非必填,二级详情拼接链接
-    推荐: '.stui-pannel:eq(2);.stui-vodlist&&li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-    double: true, // 推荐内容是否双层定位
-    一级: '.stui-vodlist li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-    二级: {
-        "title": "h1&&Text;.data&&Text",
-        "img": "img&&data-original",
-        "desc": ';;;.video-info-actor:eq(1)&&Text;.video-info-actor:eq(0)&&Text',
-        "content": ".stui-pannel:eq(2)&&p&&Text",
-        "tabs": ".nav-tabs li",
-        "lists": ".stui-content__playlist:eq(#id) li"
-    },
-    搜索:'.stui-vodlist__media li;a&&title;a&&data-original;.pic-text&&Text;a&&href',
-}
+            title: '',
+            host: 'https://6080z.com',
+            url: '/vodshow/fyclass--------fypage---.html',
+            //https://6080z.com/vodshow/2--------2---.html
+            searchUrl: '/search/**----------fypage---.html',
+            searchable: 2,//是否启用全局搜索,
+            quickSearch: 0,//是否启用快速搜索,
+            filterable: 0,//是否启用分类筛选,
+            class_parse: '.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
+            play_parse: true,
+            lazy: '',
+            limit: 6,
+            推荐: '.module-list;.module-items&&.module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+            double: true, // 推荐内容是否双层定位
+            一级: '.module-items .module-item;a&&title;img&&data-src;.module-item-text&&Text;a&&href',
+            二级: {
+                "title": "h1&&Text;.tag-link&&Text",
+                "img": ".module-item-pic&&img&&data-src",
+                "desc": ".video-info-items:eq(0)&&Text;.video-info-items:eq(1)&&Text;.video-info-items:eq(2)&&Text;.video-info-items:eq(3)&&Text",
+                "content": ".vod_content&&Text",
+                "tabs": ".module-tab-item",
+                "lists": ".module-player-list:eq(#id)&&.scroll-content&&a"
+            }
