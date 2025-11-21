184d6e86f934211ac2cc5bc6abc2a5574ab95dd0
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-21 11:26:25 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,29 +1,29 @@
 var rule = {
-    title: '剧多多',
-    host: 'https://www.jddzx.cc',
-    url: '/type/fyclass/page/fypage.html',
-    detailUrl: '/vod/fyid.html',
-    searchUrl: '/vodsearch.html?wd=**',
+    title: '糖豆影视',
+    host: 'https://tdys.cc',
+    // 启用翻页：MacCMS 标准格式
+    url: '/show/fyclass--------fypage---.html',
+    detailUrl: '/vod-detail/fyid.html',
+    searchUrl: '/so/-.html?wd=**',
     searchable: 2,
     quickSearch: 1,
     filterable: 0,
     headers: {
-        'User-Agent': 'MOBILE_UA'
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36'
     },
-    class_name: '电影&剧集&动漫&综艺&短剧',
-    class_url: 'dianying&juji&dongman&zongyi&duanju',
+    class_name: '电影&剧集&动漫&综艺&纪录片',
+    class_url: 'dianying&juji&dongman&zongyi&jilupian',
     lazy: '',
-    limit: 20,
-    double: false,
+    limit: 30,
     推荐: 'a.module-poster-item.module-item;a&&title;.module-item-pic&&img&&data-original;.module-item-note&&Text;a&&href',
     一级: 'a.module-poster-item.module-item;a&&title;.module-item-pic&&img&&data-original;.module-item-note&&Text;a&&href',
     二级: {
-        "title": "h1&&Text;.module-info-content&&p:eq(0)&&Text",
+        "title": "h1&&Text;.module-info-tag&&Text",
         "img": ".module-info-poster&&img&&data-original",
-        "desc": ".module-info-content&&p:eq(1)&&Text",
+        "desc": ".module-info-content&&p:eq(1)&&Text", // 主演
         "content": ".module-info-introduction&&p&&Text",
-        "tabs": ".module-player-tab-item",
-        "lists": ".module-player-list:eq(#id)&&a"
+        "tabs": "div.module-player-tab-item",
+        "lists": "div.module-player-list:eq(#id) a"
     },
     搜索: 'a.module-poster-item.module-item;a&&title;.module-item-pic&&img&&data-original;.module-item-note&&Text;a&&href'
 };
