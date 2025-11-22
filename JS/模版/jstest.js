3eb8cbe540c2718d0dcb30fb035247f1d6275af8
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-22 07:37:47 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -6,7 +6,6 @@ var rule = {
     host: 'https://bfzyapi.com',
     // homeTid: '13',
     homeTid: '',
-    //cate_exclude: '电影片|连续剧|综艺片|动漫片|电影解说|体育|演员|新闻资讯',
-    cate_exclude: '电影解说|体育|演员|新闻资讯',
+    cate_exclude: '电影片|连续剧|综艺片|动漫片|电影解说|体育|演员|新闻资讯',
     parse_url: '',
 }
