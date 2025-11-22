23964d03b859d60a31f3c5f6d1ad7d8b132e4508
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-22 07:43:41 -0700

    Create 暴风资源JS.js

---
+++ JS/暴风资源JS.js
@@ -0,0 +1,11 @@
+// https://bfzyapi.com/api.php/provide/vod/?ac=list
+
+var rule = {
+    模板: '采集1',
+    title: '暴风资源',
+    host: 'https://bfzyapi.com',
+    // homeTid: '13',
+    homeTid: '',
+    cate_exclude: '电影片|连续剧|综艺片|动漫片|电影解说|体育|演员|新闻资讯',
+    parse_url: '',
+}
