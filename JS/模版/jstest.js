f5c08dc52221ab647c3cfc40b73cbc27cccc8b29
heroaku <108630267+heroaku@users.noreply.github.com>
2025-11-27 13:27:54 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,117 +1,125 @@
-// 地址发布页 https://www.czzy.site
-// 地址发布页 https://cz01.vip
+// 搜索验证 -> drpy_ocr自动过搜索验证失败
 var rule = {
-    title: '厂长资源',
-    //host: 'https://www.czzy88.com',
-    host:'https://www.czzy.site',
-    //hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});HOST = html.match(/推荐访问<a href="(.*)"/)[1];print("厂长跳转地址 =====> " + HOST)',
-	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(3)&&a&&href");print(src);HOST=src',
-    url: '/fyclassfyfilter',
+    title: '烧火影视[优]',
+    host:'https://saohuody.com',
+     //host:'https://saohuo.tv',
+    //host: 'http://shapp.us',
+    //hostJs: 'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"a:eq(2)&&href");print(src);HOST=src',
+    // url:'/list/fyclass-fypage.html',
+    url: '/list/fyfilter-fypage.html',
     filterable: 1,//是否启用分类筛选,
-    filter_url: '{{fl.cateId}}{{fl.class}}{{fl.area}}/page/fypage',
-    filter: 'H4sIAAAAAAAAA42V3U7iQBTH36XXJt77KhtjBql0EFqlHbUYE79AQEXcVfwiarJi8QOVNborUniZTluufIXtiO1U7LRe9KLT3/+cOWfO/LvIpaU5yE/EFG7sxyI3zavcGAcyPOBGOBGkeecNF/JW69V5nwMpxL9jc84yAcjXXKO/1uCWRgaro264CZnPQF4ezQpAnHSehIJgEgLxQ2bdHPX3e2a5YW9X2OK4qrr83hPWWyGkE1oWYBJR3tbyuKiFSCQBAbeKUsPae2WzCSSRIrzw+EQ32lpo+HTS3Uq3HApmXNA8qIeCggv2z67CwXkv5NNffFF14LdOAeeejdfqW6cYIkRARaSVKhQTbqnbZfu+GdX/NA+dHg1JzVvNKT5KmoExXhxWHtTN2m2UkozVl6SkOSd6lJQIvmrtzdY3tFMgoNTH/e9ljX/t7wNuX0Yp+RSSZDgkNXrrln5gVu+j1OTigTgYzlw6Nzd7+EKL7DOCKgJi1hkPcda9AkbnGO8eGv9+4a02tzROxAP3mEwBWab24XgHPt38ZB8+3xgZeAlRD4Ke23cPH6t0HwpIyKOTJH0Seixe71k39WCWV3iFgvmf/aOrYDANpAXiSh5b1My1XDCbRLOkeR5aahh6LRiNS1kk+Umzc8Mi0/70Ax8KJgWBcuU83vkTzKWgY4SU3LmwfzMKSqD3I/WVVNkyl7VgWJHExMKn+qs14kKB8AJxYg+sb+AXRlGzkOyAonfPuN1ktB8CxZ/fXKmYy9VgdhrFaH5z9daq7jI2isiVoOhazi4y+koOX+Z9aMlcPWakJ6jk32vh0GgXg2Hyh8wKPG2C2dyyC6eMI5v3YZdG9zwYm0fOVHuktdLtH3cZ2QEUfQdgFVrM+Qfw0/xbpXtLvw5Gp5ySEG2rdda0ihuMsZ6hmLbLHJRpOvxW+xrr+8yAydQMDWnXe9Rmhs8+RrFexbl6wZga97D+2aPxUmFgKnHB8aX/CTefD1IJAAA=',
-    searchUrl: '/daoyongjiekoshibushiyoubing?q=**&f=_all&p=fypage',
-    
-    searchable: 2,
-    filterable: 0,
-    headers: {
-        'User-Agent': 'PC_UA',
-        // 'Cookie': 'esc_search_captcha=1'
+    filter_url: '{{fl.cateId}}',
+    filter: {
+        "1": [{
+            "key": "cateId",
+            "name": "类型",
+            "value": [{"v": "1", "n": "全部"}, {"v": "6", "n": "喜剧"}, {"v": "7", "n": "爱情"}, {
+                "v": "8",
+                "n": "恐怖"
+            }, {"v": "9", "n": "动作"}, {"v": "10", "n": "科幻"}, {"v": "11", "n": "战争"}, {
+                "v": "12",
+                "n": "犯罪"
+            }, {"v": "13", "n": "动画"}, {"v": "14", "n": "奇幻"}, {"v": "15", "n": "剧情"}, {
+                "v": "16",
+                "n": "冒险"
+            }, {"v": "17", "n": "悬疑"}, {"v": "18", "n": "惊悚"}, {"v": "19", "n": "其它"}]
+        }],
+        "2": [{
+            "key": "cateId",
+            "name": "类型",
+            "value": [{"v": "2", "n": "全部"}, {"v": "20", "n": "大陆"}, {"v": "21", "n": "TVB"}, {
+                "v": "22",
+                "n": "韩剧"
+            }, {"v": "23", "n": "美剧"}, {"v": "24", "n": "日剧"}, {"v": "25", "n": "英剧"}, {
+                "v": "26",
+                "n": "台剧"
+            }, {"v": "27", "n": "其它"}]
+        }],
+        "4": [{
+            "key": "cateId",
+            "name": "类型",
+            "value": [{"v": "4", "n": "全部"}, {"v": "38", "n": "搞笑"}, {"v": "39", "n": "恋爱"}, {
+                "v": "40",
+                "n": "热血"
+            }, {"v": "41", "n": "格斗"}, {"v": "42", "n": "美少女"}, {"v": "43", "n": "魔法"}, {
+                "v": "44",
+                "n": "机战"
+            }, {"v": "45", "n": "校园"}, {"v": "46", "n": "亲子"}, {"v": "47", "n": "童话"}, {
+                "v": "48",
+                "n": "冒险"
+            }, {"v": "49", "n": "真人"}, {"v": "50", "n": "LOLI"}, {"v": "51", "n": "其它"}]
+        }],
+        // "28":[{"key":"cateId","name":"综艺","value":[{"v":"28","n":"脱口秀"},{"v":"29","n":"真人秀"},{"v":"30","n":"选秀"},{"v":"31","n":"美食"},{"v":"32","n":"旅游"},{"v":"33","n":"汽车"},{"v":"34","n":"访谈"},{"v":"35","n":"纪实"},{"v":"36","n":"搞笑"},{"v":"37","n":"其它"}]}]
     },
-    class_name: '全部&高分影视&最新电影&电影&电视剧&动画&国产剧&日剧&韩剧&美剧&海外剧&俄罗斯电影&加拿大电影&华语电影&印度电影&日本电影&欧美电影&韩国电影',
-    class_url: 'movie_bt&gaofenyingshi&zuixindianying&/movie_bt_series/dyy&/movie_bt_series/dianshiju&/movie_bt_series/dohua&/movie_bt_series/guochanju&/movie_bt_series/rj&/movie_bt_series/hj&/movie_bt_series/mj&/movie_bt_series/hwj&/movie_bt_series/eluosidianying&/movie_bt_series/jianadadianying&/movie_bt_series/huayudianying&/movie_bt_series/yindudianying&/movie_bt_series/ribendianying&/movie_bt_series/meiguodianying&/movie_bt_series/hanguodianying',
+    filter_def: {
+        1: {cateId: '1'},
+        2: {cateId: '2'},
+        4: {cateId: '4'}
+        // 28:{cateId:'28'}
+    },
+    //searchUrl: '/search.php?page=fypage&searchword=**&searchtype=',
+    searchUrl: '/search/**----------fypage---.html',    
+    searchable: 2,
+    quickSearch: 0,
+    headers: {'User-Agent': 'MOBILE_UA',},
+    timeout: 5000,//网站的全局请求超时,默认是3000毫秒
+    class_name: '电影&电视剧&动漫',
+    class_url: '1&2&4',
     play_parse: true,
-
-    // lazy代码:源于海阔香雅情大佬 / 小程序：香情影视 https://pastebin.com/L4tHdvFn
     lazy: `js:
-        pdfh = jsp.pdfh;
-        var html = request(input);
-        var ohtml = pdfh(html, '.videoplay&&Html');
-        var url = pdfh(ohtml, "body&&iframe&&src");
-        if (url) {
-            var _obj={};
-            eval(pdfh(request(url),'body&&script&&Html')+'\\n_obj.player=player;_obj.rand=rand');
-            function js_decrypt(str, tokenkey, tokeniv) {
-                eval(getCryptoJS());
-                var key = CryptoJS.enc.Utf8.parse(tokenkey);
-                var iv = CryptoJS.enc.Utf8.parse(tokeniv);
-                return CryptoJS.AES.decrypt(str, key, {iv: iv,padding: CryptoJS.pad.Pkcs7}).toString(CryptoJS.enc.Utf8);
-            };
-            let config = JSON.parse(js_decrypt(_obj.player,'VFBTzdujpR9FWBhe', _obj.rand));
+        pdfh = jsp.pdfh;pdfa = jsp.pdfa;pd = jsp.pd;
+        var html = pd(request(input), 'iframe&&src');
+        var apiurl = '';
+        if (/api\\.hhplayer/.test(html)) {
+            apiurl = 'https://api.hhplayer.com/api.php';
+        } else if (/hkjx\\.hhplayer/.test(html)) {
+            apiurl = 'https://hkjx.hhplayer.com/api.php';
+        } else if (/hhjx\\.hhplayer/.test(html)) {
+            apiurl = 'https://hhjx.hhplayer.com/api.php';
+        } else if (/play\\.hhplayer/.test(html)) {
+            apiurl = 'https://play.hhplayer.com/hhjx/api.php';
+        }
+        var url = '';
+        var t = '';
+        var key = '';
+        eval(pdfh(request(html), 'body&&script,0&&Html').split('var act')[0].replaceAll('var ', ''));
+        var purl = JSON.parse(request(apiurl, {
+            headers: {
+                'User-Agent': MOBILE_UA,
+                'Referer': html
+            },
+            body: 'url=' + url + '&t=' + t + '&key=' + key + '&act=0&play=1',
+            method: 'POST'
+        })).url;
+        if(!purl){
             input = {
-                 jx: 0,
-                 url: config.url,
-                 parse: 0
-            };
-        }else if (/decrypted/.test(ohtml)) {
-            var phtml = pdfh(ohtml, "body&&script:not([src])&&Html");
-            eval(getCryptoJS());
-            var script = phtml.match(/var.*?\\)\\);/g)[0];
-            var data = [];
-            eval(script.replace(/md5/g, 'CryptoJS').replace('eval', 'data = '));
+            parse:1,
+            url:html
+            }
+        }
+        else if (/obj\\/tos/.test(purl)) {
             input = {
                 jx: 0,
-                url: data.match(/url:.*?['"](.*?)['"]/)[1],
+                url: purl,
                 parse: 0
             }
-
-
-        } 
-    `,
-    推荐: '.bt_img;ul&&li;*;*;*;*',
-    double: true,
-    一级: '.bt_img&&ul&&li;h3.dytit&&Text;img.lazy&&data-original;.jidi&&Text;a&&href',
-    二级: {
-        "title": "h1&&Text;.moviedteail_list li&&a&&Text",
-        "img": "div.dyimg img&&src",
-        "desc": ".moviedteail_list li:eq(3) a&&Text;.moviedteail_list li:eq(2) a&&Text;.moviedteail_list li:eq(1) a&&Text;.moviedteail_list li:eq(7)&&Text;.moviedteail_list li:eq(5)&&Text",
-        "content": ".yp_context&&Text",
-        "tabs": ".mi_paly_box span",
-        "lists": ".paly_list_btn:eq(#id) a"
-    },
-    搜索: `js:
-    let cookie = getItem(RULE_CK,'');
-    // let cookie = '';
-    log('储存的cookie:'+cookie);
-    let hhtml=request(input,{withHeaders:true,headers:{Cookie:cookie}});
-    let json = JSON.parse(hhtml);
-    let html = json.body;
-    let setCk = Object.keys(json).find(it=>it.toLowerCase()==='set-cookie');
-    cookie = setCk ? json[setCk] : cookie;
-    // 3个set-Cookie
-    if (Array.isArray(cookie)) {
-        cookie = cookie.join(';');
-    }
-    cookie = cookie.split(';')[0];
-    log('set-cookie:'+cookie);
-    let code='';
-    if(/erphp-search-captcha/.test(html)){
-        code = jsp.pdfh(html,'.erphp-search-captcha--button&&Text');
-        if(code.includes('=')){
-            let a = code.replace('=','').replace(/ /g,'');
-            code = eval(a);
-            log('回答验证码:'+a+' 答案:'+code);
+        } else {
+            input = {
+                jx: 0,
+                url: /http/.test(purl) ? purl: 'https://api.hhplayer.com' + purl,
+                parse: 0
+            }
         }
-        let key = jsp.pdfh(html,'.erphp-search-captcha&&input&&name');
-        let body = key+'='+code;
-        post(input,{body:body,headers:{Cookie:cookie}});
-        setItem(RULE_CK,cookie);
-        html = getHtml(input);
-    }
-    // log(html);
-    VODS = [];
-    let lis=pdfa(html,'.search_list&&ul&&li');
-    log(lis.length);
-    lis.forEach(function(it){
-        VODS.push({
-            vod_id: pd(it,'a&&href',input),
-			vod_name: pdfh(it,'h3.dytit&&Text'),
-			vod_pic: pd(it,'img.lazy&&data-original',input),
-			vod_remarks: pdfh(html,'.jidi&&Text')
-        });
-    
-    });
-    
     `,
-
+    推荐: '.v_list,0&&li;*;*;*;*',
+    一级: '.v_list li;a&&title;.lazyload&&data-original;.v_note&&Text;a&&href',
+    二级: {
+        "title": "h1&&Text;",
+        "img": ".m_background&&style",
+        "desc": ";;;.v_info_box&&p&&Text",
+        "content": ".p_txt.show_part&&Text",
+        "tabs": ".from_list&&li",
+        "lists": "#play_link&&li:eq(#id)&&a"
+    },
+    搜索: '*',
 }
