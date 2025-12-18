1f6750c8982000b08d90d03abfb87f8645c47940
heroaku <108630267+heroaku@users.noreply.github.com>
2025-12-18 16:56:34 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,30 +1,77 @@
-var rule={
-    title: '八天影视',
-    host: 'https://dy.8ttv.cn',
-    url: '/index.php/vod/show/id/fyclass/page/fypage.html',
-    searchUrl: '/index.php/vod/search/page/fypage/wd/**.html',
-    //https://dy.8ttv.cn/index.php/vod/show/id/2/page/2.html
-    //https://dy.8ttv.cn/index.php/vod/search/page/2/wd/ai.html
+var rule = {
+    title: '柏彩影视',
+    host: 'https://bcvod.com',
+    url: '/vodshow/fyfilter.html',
+    filterable: 1,
+    filter_url:'{{fl.cateId}}-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
+    filter: {
+        // ... 这里省略了filter的具体内容，您原来的filter很长，不需要改动
+    },
+    filter_def:{
+        dianying:{cateId:'dianying',by:'time'},
+        dianshiju:{cateId:'dianshiju',by:'time'},
+        zongyi:{cateId:'zongyi',by:'time'},
+        dongman:{cateId:'dongman',by:'time'},
+        duanju:{cateId:'duanju',by:'time'}
+    },
+    searchUrl: '/vodsearch/**----------fypage---.html',
     searchable: 2,
     quickSearch: 0,
-    filterable: 0,
     headers: {
-    'User-Agent': 'MOBILE_UA',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Cache-Control': 'max-age=0'
     },
-    //class_parse: '.navbar&&ul&&li;a&&Text;a&&href;/(\\d+).html',
-    class_name: '电影&电视剧&综艺&动漫&短剧&纪录片',
-    class_url: '1&2&3&4&5&20',
-    play_parse: false,
-    lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
-    limit: 6,
-    推荐: '.module-items;a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-    double: true,
-    一级: 'a.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    class_name:'电影&电视剧&综艺&动漫&短剧',
+    class_url:'dianying&dianshiju&zongyi&dongman&duanju',
+    play_parse: true,
+    lazy: `js:
+        var headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1'
+        };
+        var html = request(input, {headers: headers});
+        if (html.includes('Just a moment')) {
+            // 如果遇到Cloudflare验证，尝试使用一个反代服务
+            // 注意：这个反代服务需要您自己搭建或寻找可用的
+            var proxy_url = 'https://your-proxy-service.com/?url=' + encodeURIComponent(input);
+            html = request(proxy_url, {headers: headers});
+        }
+        var match = html.match(/r player_.*?=(.*?)</);
+        if (match) {
+            var url = JSON.parse(match[1]).url;
+            if (url.indexOf('http') == -1) {
+                var player_html = request('https://adys.tv/player/?url=' + url, {headers: headers});
+                var player_match = player_html.match(/url":.*?['"](.*?)['"]/);
+                if (player_match) {
+                    input = player_match[1];
+                } else {
+                    input = '';
+                }
+            } else {
+                input = url;
+            }
+        } else {
+            input = '';
+        }
+    `,
+    推荐: '*',
+    一级:'.vodlist.vodlist_wi li;a&&title;.lazyload&&data-original;.pic_text&&Text;a&&href',
     二级: {
-    "title": "h1&&Text;.module-info-tag&&Text",
-    "img": ".lazyload&&data-original",
-    "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
-    "content": ".module-info-introduction&&Text",
-    "tabs": ".hisSwiper&&span",
-    "lists": ".module-play-list-content:eq(#id) a"},
-    搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',}
+        "title": "h1&&Text;li.data--span:eq(0)&&Text",
+        "img": ".bgi.lazyload&&data-background-image",
+        "desc": "li.data:eq(4)&&Text;;;li.data--span:eq(3)&&Text;li.data--span:eq(2)&&Text",
+        "content": ".full_text&&span&&Text",
+        "tabs": ".play_source_tab&&a",
+        "lists": ".play_list_box:eq(#id)&&.content_playlist li"
+    },
+    搜索: '.vodlist.clearfix li.vodlist_item;*;*;*;*',
+}
