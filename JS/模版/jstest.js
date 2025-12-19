81cdace161c18cc6593bee1acf4d3840af8e4c78
heroaku <108630267+heroaku@users.noreply.github.com>
2025-12-19 09:50:41 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,10 +1,10 @@
 var rule={
-    title: '飞快影视JS',
-    host: 'https://feikuai.tv',
-    url: '/vodshow/fyclass--------fypage---.html',
-    searchUrl: '/vodsearch/**----------fypage---.html',
-  //https://feikuai.tv/vodshow/2--------2---.html
-    //https://www.55yy7.com/vodsearch/**----------fypage---.html
+    title: 'FROK影视',
+    host: 'https://www.freeokk.pro',
+    url: '/show/fyfilter--------fypage---.html',
+    searchUrl: '/search/**----------fypage---.html',
+  //https://www.freeokk.pro/show/2--------2---.html
+    //https://www.freeokk.pro/search/ai----------2---.html
     searchable: 2,
     quickSearch: 0,
     filterable: 0,
@@ -12,8 +12,10 @@ var rule={
     'User-Agent': 'MOBILE_UA',
     },
     class_parse: '.navbar&&ul&&li;a&&Text;a&&href;/(\\d+).html',
-    play_parse: false,
-        //lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+	  //class_name:'电影&电视剧&综艺&动漫',
+    //class_url:'1&2&3&4',  
+	  play_parse: true,
+    lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
     limit: 6,
     推荐: '.module-items;a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
     double: true,
@@ -23,6 +25,6 @@ var rule={
     "img": ".lazyload&&data-original",
     "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
     "content": ".module-info-introduction&&Text",
-    "tabs": ".module-tab-items-box&&.module-tab-item span",
-    "lists": ".module-list:eq(#id) a"},
+    "tabs": ".hisSwiper&&span",
+    "lists": ".his-tab-list:eq(#id) a"},
     搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',}
