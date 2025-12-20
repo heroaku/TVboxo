9ce5e4015751cb5f01fa676220062fc1b3f0abef
heroaku <108630267+heroaku@users.noreply.github.com>
2025-12-20 12:47:57 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -36,11 +36,7 @@ var rule={
 	cate_exclude: '纪录',
     tab_remove:['VIP线路'], // VIP线路 需登录会员
     play_parse:true,
-    lazy: $js.toString(() => {
-        var url = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]).url;
-        url.indexOf('http') == -1 ? input = request('https://adys.tv/player/?url=' + url, {}).match(/url":.*?['"](.*?)['"]/)[1] :input = url
-
-    }),
+    //lazy: $js.toString(() => {var url = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]).url;url.indexOf('http') == -1 ? input = request('https://laodifang.tv/player/?url=' + url, {}).match(/url":.*?['"](.*?)['"]/)[1] :input = url    }),
     推荐:'*',
     // 一级:'.vodlist.vodlist_wi li;a&&title;.lazyload&&data-original;.pic_text&&Text;a&&href',
     一级:'.vodlist.vodlist_wi li;a&&title;.lazyload&&data-background-image;.xszxj&&Text;a&&href',
