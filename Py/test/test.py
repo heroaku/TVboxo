1b345f350409fddc7e6a8ca31c53db4ff5934bfa
heroaku <108630267+heroaku@users.noreply.github.com>
2025-12-20 11:26:59 -0700

    Update test.py

--- Py/test/test.py
+++ Py/test/test.py
@@ -1,27 +1,143 @@
-# coding=utf-8
-# !/usr/bin/python
-import sys
-import requests
-from bs4 import BeautifulSoup
-import re
+# -*- coding: utf-8 -*-
+# 本资源来源于互联网公开渠道，仅可用于个人学习爬虫技术。
+# 严禁将其用于任何商业用途，下载后请于 24 小时内删除，搜索结果均来自源站，本人不承担任何责任。
+
 from base.spider import Spider
-from urllib.parse import unquote
-
+import re,sys,time,uuid,hmac,urllib3,hashlib
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 sys.path.append('..')
-xurl = "https://www.youzisp.tv"
-headerx = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36'
-}
-
 
 class Spider(Spider):
-    global xurl
-    global headerx
+    host,player_host,dkey,player = 'https://app.laodifang.tv','https://www.laodifang.tv',{bs.decode('gbk') for bs in [b'\xc2\xd7\xc0\xed',b'\xb8\xa3\xc0\xfb',b'\xc7\xe9\xc9\xab']},{}
+
+    def init(self, extend=''):
+        pass
+
+    def headers(self,path,json_type=1):
+        timestamp = str(int(time.time()))
+        random_uuid = str(uuid.uuid4()).replace('-', '')
+        sign = self.hmac_sha256(f'PV-HMAC-SHA256\n{timestamp}\nPOST\n{path}\n\nUNSIGNED-PAYLOAD\n{random_uuid}')
+        headers = {
+            'User-Agent': 'okhttp/4.11.0',
+            'Accept-Encoding': 'gzip',
+            'Content-Type': 'application/json',
+            'access-token': '',
+            'authorization': f"PV-HMAC-SHA256 Credential=5gc4r3ic, SignedHeaders=host;content-type;x-pv-timestamp;x-pv-nonce, Signature={sign}",
+            'x-pv-timestamp': timestamp,
+            'x-pv-nonce': random_uuid
+        }
+        if json_type == 1: headers['content-type'] = 'application/json; charset=UTF-8'
+        return headers
+
+    def homeContent(self, filter):
+        if not self.host: return None
+        path = '/api/v1/type/list'
+        response = self.post(f'{self.host}{path}', data='{}', headers=self.headers(path), verify=False).json()
+        classes = []
+        for item in response['data']:
+            if isinstance(item, dict):
+                type_name = item.get('typeName')
+                if not(isinstance(type_name, str) and any(kw in type_name for kw in self.dkey)):
+                    classes.append({'type_id': item['typeId'],'type_name': type_name})
+        return {'class': classes}
+
+    def homeVideoContent(self):
+        path = '/api/v1/video/index'
+        response = self.post(f'{self.host}{path}', data='{"isFilterAd":false}', headers=self.headers(path), verify=False).json()
+        videos = []
+        for i in response['data'].values():
+            if isinstance(i,list):
+                videos.extend(self.json2vods(i))
+        return {'list': videos}
+
+    def categoryContent(self, tid, pg, filter, extend):
+        path = '/api/v1/video/list'
+        response = self.post(f'{self.host}{path}', data=f'{{"area":"","claxx":"","lang":"","page":{pg},"pageSize":12,"sort":2,"typeId":{tid},"typeTopId":0,"year":"","isFilterAd":false}}', headers=self.headers(path), verify=False).json()
+        return {'list': self.json2vods(response['data']['list']), 'page': pg}
+
+    def searchContent(self, key, quick, pg='1'):
+        path = '/api/v1/video/search'
+        response = self.post(f'{self.host}{path}', data=f'{{"keyWord":"{key}","isFilterAd":false}}', headers=self.headers(path), verify=False).json()
+        videos = self.json2vods(response['data'])
+        return {'list': videos, 'page': pg}
+
+    def detailContent(self, ids):
+        path = '/api/v1/video/detail'
+        response = self.post(f'{self.host}{path}', data=f'{{"videoId": {ids[0]}}}', headers=self.headers(path), verify=False).json()
+        data = response['data']
+        path2 = '/app/config.json'
+        if not self.player:
+            response2 = self.fetch(f'{self.player_host}{path2}', headers=self.headers(path2), verify=False).json()
+            self.player = {k['code']: (k['name'], k['needParse']) for k in response2['data']['player']}
+        type_name = data['vodClass']
+        if isinstance(type_name, str) and any(kw in type_name for kw in self.dkey): return
+        shows,play_urls = [],[]
+        raw_shows = data['vodPlayFrom'].split('$$$')
+        raw_urls_list = data['vodPlayUrl'].split('$$$')
+        for show_code, urls_str in zip(raw_shows, raw_urls_list):
+            name, need_parse = self.player.get(show_code, ('', 0))
+            if name and name != show_code:
+                formatted_show = f"{name}({show_code})"
+            else:
+                formatted_show = show_code
+            urls = []
+            for url_item in urls_str.split('#'):
+                episode, url = url_item.split('$',1)
+                urls.append(f"{episode}${show_code}@{int(need_parse)}@{url}")
+            play_urls.append('#'.join(urls))
+            shows.append(formatted_show)
+        video = {
+            'vod_id': data['vodId'],
+            'vod_name': data['vodName'],
+            'vod_pic': data['vodPic'],
+            'vod_remarks': data['vodRemarks'],
+            'vod_year': data['vodYear'],
+            'vod_area': data['vodArea'],
+            'vod_actor': data['vodActor'],
+            'vod_director': data['vodDirector'],
+            'vod_content': data['vodContent'],
+            'vod_play_from': '$$$'.join(shows),
+            'vod_play_url': '$$$'.join(play_urls),
+            'type_name': data['vodClass']
+        }
+        return {'list': [video]}
+
+    def playerContent(self, flag, video_id, vip_flags):
+        play_from, need_parse, raw_url = video_id.split('@',2)
+        jx,url = 0,''
+        if need_parse == '1':
+            try:
+                path = '/api/v1/video/realPlayUrl'
+                response = self.post(f'{self.host}{path}', data=f'{{"playerCode":"{play_from}","url":"{raw_url}"}}', headers=self.headers(path), verify=False).json()
+                play_url = response['data']['playUrl']
+                if play_url.startswith('http') and play_url != raw_url: url = play_url
+            except Exception:
+                pass
+        if not url:
+            url = raw_url
+            if re.search(r'(?:www\.iqiyi|v\.qq|v\.youku|www\.mgtv|www\.bilibili)\.com', video_id):
+                jx = 1
+        return { 'jx': jx, 'parse': 0, 'url': url, 'header': {'User-Agent': 'okhttp/4.11.0','Accept-Encoding': 'gzip'}}
+
+    def json2vods(self,arr):
+        videos = []
+        for i in arr:
+            vod_class = i.get('vodClass')
+            if not(isinstance(vod_class, str) and any(kw in vod_class for kw in self.dkey)):
+                videos.append({
+                    'vod_id': i['vodId'],
+                    'vod_name': i['vodName'],
+                    'vod_pic': i['vodPic'],
+                    'vod_remarks': i['vodRemarks'],
+                    'vod_year': i['vodYear']
+                })
+        return videos
+
+    def hmac_sha256(self, data):
+        key = "4ssnwkd3qoz4tuqoyeddhe65667qo4oc"
+        return hmac.new(key.encode('utf-8'), data.encode('utf-8'), hashlib.sha256).hexdigest()
 
     def getName(self):
-        return "首页"
-
-    def init(self, extend):
         pass
 
     def isVideoFormat(self, url):
@@ -29,249 +145,9 @@ class Spider(Spider):
 
     def manualVideoCheck(self):
         pass
-    def fl(self,key):
-        videos = []
-        doc = BeautifulSoup(key, "html.parser")
-        soup = doc.find_all('li', class_="hl-list-item hl-col-xs-4 hl-col-sm-3 hl-col-md-20w hl-col-lg-2")
 
-        for vod in soup:
-            name = vod.select_one("a")['title']
-            id = xurl + vod.select_one("a")["href"]
-            pic = vod.select_one("a")['data-original']
-            remark = vod.find('span', class_="hl-lc-1 remarks").text
-            video = {
-                "vod_id": id,
-                "vod_name": name,
-                "vod_pic": pic,
-                "vod_remarks": remark
-            }
-            videos.append(video)
-        return videos
-    def homeContent(self, filter):
-        result = {}
-        result = {
-            "class": [{"type_id": "dianying", "type_name": "电影"}, {"type_id": "dianshiju", "type_name": "电视剧"},
-                      {"type_id": "zongyi", "type_name": "综艺"}, {"type_id": "dongman", "type_name": "动漫"},
-                      {"type_id": "jilupian", "type_name": "纪录片"}], "list": [], "filters": {"dianying": [
-                {"key": "类型", "name": "类型",
-                 "value": [{"n": "全部", "v": ""}, {"n": "动作", "v": "dongzuo"}, {"n": "冒险", "v": "maoxian"},
-                           {"n": "喜剧", "v": "xiju"}, {"n": "奇幻", "v": "qihuan"}, {"n": "科幻", "v": "kehuan"},
-                           {"n": "悬疑", "v": "xuanyi"}, {"n": "惊悚", "v": "jingsong"},
-                           {"n": "战争", "v": "zhanzheng"},
-                           {"n": "爱情", "v": "aiqing"}, {"n": "歌舞", "v": "gewu"}, {"n": "犯罪", "v": "fanzui"},
-                           {"n": "灾难", "v": "zainan"}, {"n": "同性", "v": "tongxing"}, {"n": "剧情", "v": "juqing"},
-                           {"n": "网络", "v": "wangluo"}]}], "dianshiju": [{"key": "类型", "name": "类型",
-                                                                            "value": [{"n": "全部", "v": ""},
-                                                                                      {"n": "国产", "v": "guochan"},
-                                                                                      {"n": "港台", "v": "gangtai"},
-                                                                                      {"n": "欧美", "v": "oumei"},
-                                                                                      {"n": "日韩", "v": "rihan"},
-                                                                                      {"n": "东南亚",
-                                                                                       "v": "dongnanya"}]}],
-                "zongyi": [
-                    {"key": "类型",
-                     "name": "类型",
-                     "value": [
-                         {"n": "全部",
-                          "v": ""},
-                         {"n": "国产",
-                          "v": "guochan"},
-                         {"n": "欧美",
-                          "v": "oumei"},
-                         {"n": "日韩",
-                          "v": "rihan"},
-                         {
-                             "n": "东南亚",
-                             "v": "dongnanya"}]}],
-                "dongman": [
-                    {"key": "类型",
-                     "name": "类型",
-                     "value": [
-                         {"n": "全部",
-                          "v": ""},
-                         {"n": "大陆",
-                          "v": "dalu"},
-                         {"n": "日韩",
-                          "v": "rihan"},
-                         {"n": "欧美",
-                          "v": "oumei"},
-                         {
-                             "n": "东南亚",
-                             "v": "dongnanya"},
-                         {
-                             "n": "动漫剧场",
-                             "v": "dongmanjuchang"}]}],
-                "jilupian": [
-                    {"key": "类型",
-                     "name": "类型",
-                     "value": [
-                         {"n": "全部",
-                          "v": ""}]}]}}
-        return result
+    def destroy(self):
+        pass
 
-    def homeVideoContent(self):
-        try:
-            detail = requests.get(url=xurl + '/vodtype/dianshiju.html', headers=headerx)
-            detail.encoding = "utf-8"
-            res = detail.text
-            videos = self.fl(res)
-
-            result = {'list': videos}
-            return result
-        except:
-            pass
-
-    def categoryContent(self, cid, pg, filter, ext):
-        result = {}
-        print(ext)
-        area = ""
-        if pg:
-            page = int(pg)
-        else:
-            page = 1
-        page = int(pg)
-        videos = []
-        lxType1 = ""
-        if cid == 'dianying':
-            if '类型' in ext.keys():
-                lxType = ext['类型']
-            else:
-                lxType = cid
-        if cid == 'dianshiju':
-            if '类型' in ext.keys():
-                lxType = ext['类型']
-            else:
-                lxType = cid
-        if cid == 'zongyi':
-            if '类型' in ext.keys():
-                lxType = ext['类型']
-            else:
-                lxType = cid
-        if cid == 'dongman':
-            if '类型' in ext.keys():
-                lxType = ext['类型']
-            else:
-                lxType = cid
-        if cid == 'jilupian':
-            if '类型' in ext.keys():
-                lxType = ext['类型']
-            else:
-                lxType = cid
-
-        videos = []
-        url = xurl + "/vodshow/" + lxType + "--------" + str(page) + "---.html"
-
-        try:
-            detail = requests.get(url=url, headers=headerx)
-            detail.encoding = "utf-8"
-            res = detail.text
-            videos =self.fl(res)
-        except:
-            pass
-        result = {'list': videos}
-        result['page'] = pg
-        result['pagecount'] = 9999
-        result['limit'] = 90
-        result['total'] = 999999
-        return result
-
-    def detailContent(self, ids):
-        did = ids[0]
-        result = {}
-        videos = []
-        playurl = ''
-        res = requests.get(url=did, headers=headerx)
-        res = res.text
-        match = re.search(r'content-text"><em>(.*?)</em>', res)
-        if match:
-            vod_content = match.group(1)
-        match = re.search(r'年份：</em>(.*?)</li>', res)
-        if match:
-            vod_year = match.group(1)
-        match = re.search(r'地区：</em>(.*?)</li>', res)
-        if match:
-            vod_area = match.group(1)
-        soup = BeautifulSoup(res, 'html.parser')
-        ul = soup.find('div', class_="hl-list-wrap")
-        vods = ul.find_all('li')
-        for vod in vods:
-            purl = xurl + vod.select_one('a')['href']
-            plform = vod.select_one('a').text
-            playurl += plform + '$' + purl + '#'
-
-        playurl = playurl[:-1]
-
-        videos.append({
-            "vod_id": did,
-            "vod_name": '',
-            "vod_pic": "",
-            "type_name": '',
-            "vod_year": vod_year,
-            "vod_area": vod_area,
-            "vod_remarks": "",
-            "vod_actor": '',
-            "vod_director": '',
-            "vod_content": vod_content,
-            "vod_play_from": '1080P',
-            "vod_play_url": playurl
-        })
-
-        result['list'] = videos
-
-        return result
-
-    def playerContent(self, flag, id, vipFlags):
-        result = {}
-        res = requests.get(id, headers=headerx)
-        res.encoding = "utf-8"
-        match = re.search(r'\},"url":"(.*?)"', res.text)
-        if match:
-            purl = match.group(1).replace('\\', '')
-        result["parse"] = 0
-        result["playUrl"] = ''
-        result["url"] = purl
-        result["header"] = headerx
-        return result
-
-    def searchContentPage(self, key, quick, page):
-        result = {}
-        videos = []
-        if not page:
-            page = 1
-        detail = requests.get(xurl + '/vodsearch/' + key + '----------' + str(page) + '---.html', headers=headerx)
-        detail.encoding = "utf-8"
-        res = detail.text
-        doc = BeautifulSoup(res, "html.parser")
-        soup = doc.find_all('li', class_="hl-list-item hl-col-xs-12")
-
-        for vod in soup:
-            name = vod.select_one("a")['title']
-            id = xurl + vod.select_one("a")["href"]
-            pic = vod.select_one("a")['data-original']
-            remark = vod.find('span', class_="hl-lc-1 remarks").text
-            video = {
-                "vod_id": id,
-                "vod_name": name,
-                "vod_pic": pic,
-                "vod_remarks": remark
-            }
-            videos.append(video)
-
-        result['list'] = videos
-        result['page'] = page
-        result['pagecount'] = 9999
-        result['limit'] = 90
-        result['total'] = 999999
-        return result
-
-    def searchContent(self, key, quick):
-        return self.searchContentPage(key, quick, '1')
-
-    def localProxy(self, params):
-        if params['type'] == "m3u8":
-            return self.proxyM3u8(params)
-        elif params['type'] == "media":
-            return self.proxyMedia(params)
-        elif params['type'] == "ts":
-            return self.proxyTs(params)
-        return None
+    def localProxy(self, param):
+        pass
