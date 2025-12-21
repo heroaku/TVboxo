ce162fffb2becc63f90809780b7e96cbe0b161e0
heroaku <108630267+heroaku@users.noreply.github.com>
2025-12-21 10:03:28 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,30 +1,26 @@
-var rule={
-    title: '937影院',
-    host: 'https://www.937tv.vip',
+var rule={     
+    title:'蘑菇影视',
+    host: 'https://www.moguvodw.com',
     url: '/vodshow/fyclass--------fypage---.html',
-    searchUrl: '/vodsearch/**----------fypage---.html',
-  //https://www.937tv.vip/vodshow/lianxuju-----------.html
-    //https://www.55yy7.com/vodsearch/**----------fypage---.html
-    searchable: 2,
-    quickSearch: 0,
-    filterable: 0,
-    headers: {
-    'User-Agent': 'MOBILE_UA',
-    },
-    //class_parse: '.head-nav&&ul&&li;a&&Text;a&&href;/(\\d+).html',
-    class_name:'电影&电视剧&综艺&动漫&短剧',
-    class_url:'dianying&lianxuju&zongyipian&dongman&duanju',
-    play_parse: false,
-    lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
-    limit: 6,
-    推荐: '.module-items;a;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    //https://www.moguvodw.com/vodsearch/**----------fypage---.html
+    searchUrl:'/vodsearch/**----------fypage---.html',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    //class_parse:'.nav-menu-items&&li;a&&Text;a&&href;.*/(.*?).html',
+    //cate_exclude:'演员',
+    class_name:'电视剧&电影&动漫&综艺&短剧&纪录片',
+    class_url:'2&1&4&3&21&23',  
+    推荐:'ul.hl-vod-list;li;a&&title;.hl-item-thumb.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
     double: true,
-    一级: 'a.module-poster-item.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-    二级: {
-    "title": "h1&&Text;.module-info-tag&&Text",
-    "img": ".lazyload&&data-original",
-    "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
-    "content": ".module-info-introduction&&Text",
-    "tabs": ".hisSwiper&&span",
-    "lists": ".his-tab-list:eq(#id) a"},
-    搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',}
+     一级: '.hl-vod-list li;a&&title;a&&data-original;.hl-lc-1&&Text;a&&href',
+     二级:{
+            'title':'.h1&&Text;.data:eq(0)&&Text',
+            'img':'.hl-item-thumb&&data-original',
+            'desc':'.hl-infos-content&&.hl-text-conch&&Text',
+            'content':'.hl-content-text&&Text',
+            'tabs':'.hl-tabs&&a',
+            'lists':'.hl-plays-list:eq(#id)&&li'
+         },
+     搜索: '.hl-list-item;a&&title;a&&data-original;.remarks&&Text;a&&href'
+    }
