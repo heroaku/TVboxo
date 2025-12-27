791084b776444af081606ac6d76cd7fbf7aa8e56
heroaku <108630267+heroaku@users.noreply.github.com>
2025-12-27 16:52:40 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -27,17 +27,49 @@ var rule = {
         //"tabs": ".stui-pannel:gt(0) h3",
         "lists": ".stui-pannel:eq(#id)&&.stui-content__playlist li"
     },
-    */
+
     二级:  {
     "title": ".stui-content__detail .title&&Text",
     "img": ".stui-content__thumb .lazyload&&data-original",
     "desc": ".stui-content__detail p.data:eq(0)&&Text;.stui-content__detail p.data:eq(1)&&Text;.stui-content__detail p:eq(2)&&Text",
     "content": "#desc .stui-content__desc&&Text",
-     //"tabs": ".stui-pannel h4.title",
-         "tabs": "js:['南亞線路', '優質線路', '光速線路', '索尼線路', '速播線路', '大洋線路']",
+     "tabs": ".stui-pannel h4.title",
      "lists": ".stui-pannel:eq(#id) .stui-content__playlist li"
    // "tabs": ".stui-pannel__head .title",
    // "lists": ".stui-pannel:eq(#id)&&.stui-content__playlist li"
+},
+    */
+    二级: {
+  title: ".stui-content__detail .title&&Text",
+  img: ".stui-content__thumb .lazyload&&data-original",
+  desc: ".stui-content__detail p:eq(0)&&Text;.stui-content__detail p:eq(1)&&Text;.stui-content__detail p:eq(2)&&Text",
+  content: "#desc .stui-content__desc&&Text",
+  tabs: (html) => {
+    let panels = pdfa(html, '.stui-pannel');
+    let tabs = [];
+    panels.forEach(pan => {
+      // 判断是否有播放列表
+      let hasList = pdfa(pan, '.stui-content__playlist li').length > 0;
+      if (hasList) {
+        let title = pdfh(pan, 'h4.title&&Text');
+        if (title && title.trim() !== '') {
+          tabs.push(title);
+        }
+      }
+    });
+    return tabs;
+  },
+  lists: (html, tabs) => {
+    let panels = pdfa(html, '.stui-pannel');
+    let lists = [];
+    panels.forEach(pan => {
+      let episodes = pdfa(pan, '.stui-content__playlist li');
+      if (episodes.length > 0) {
+        lists.push(episodes);
+      }
+    });
+    return lists;
+  }
 },
     搜索: 'ul.stui-vodlist&&li;a&&title;.lazyload&&data-original;.text-muted&&Text;a&&href;.text-muted:eq(-1)&&Text',
 
