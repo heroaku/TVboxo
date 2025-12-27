dd60d6cb9ae90c62d3f00fa9d100acef358d5ecf
heroaku <108630267+heroaku@users.noreply.github.com>
2025-12-27 16:37:56 -0700

    Update jstest.js

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -33,8 +33,10 @@ var rule = {
     "img": ".stui-content__thumb .lazyload&&data-original",
     "desc": ".stui-content__detail p.data:eq(0)&&Text;.stui-content__detail p.data:eq(1)&&Text;.stui-content__detail p:eq(2)&&Text",
     "content": "#desc .stui-content__desc&&Text",
-    "tabs": ".stui-pannel__head .title",
-    "lists": ".stui-pannel:eq(#id)&&.stui-content__playlist li"
+     "tabs": ".stui-pannel h4.title",
+     "lists": ".stui-pannel:eq(#id) .stui-content__playlist li"
+   // "tabs": ".stui-pannel__head .title",
+   // "lists": ".stui-pannel:eq(#id)&&.stui-content__playlist li"
 },
     搜索: 'ul.stui-vodlist&&li;a&&title;.lazyload&&data-original;.text-muted&&Text;a&&href;.text-muted:eq(-1)&&Text',
 
