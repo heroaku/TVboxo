4822ff35654424c7ad35515e23b595f69168e3ab
heroaku <108630267+heroaku@users.noreply.github.com>
2026-01-10 11:05:21 -0700

    Update jstest.js with new host and search parameters

--- JS/模版/jstest.js
+++ JS/模版/jstest.js
@@ -1,52 +1,188 @@
+//发布页https://www.ddys.site
 var rule = {
-    title: '3Q影视',
-    host: 'https://qqqys.com/',
-    url: 'https://qqqys.com/',
-    searchUrl: 'https://qqqys.com/index.php?m=vod-search&wd=**----------&page=fypage',
-    searchable: 2, //是否启用全局搜索
-    quickSearch: 0, //是否启用快速搜索
-    filterable: 0, //是否启用分类筛选
+    title: '低端影视',
+    //host: 'https://ddys.pro',
+    host: 'https://ddys.io',
+    url: '/fyclass$fypage',
+    searchUrl: '/?s=**&post_type=post',
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 0,
+
     headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'MOBILE_UA',
+        "Referer": "https://ddys.pro/"
     },
-    class_parse: '.nav-item:gt(0):lt(5);a&&Text;a&&href;.*/(.*?)\\.html',
+    class_name: '动漫&电影&剧集&综艺&记录',
+    class_url: "category/anime/&category/movie/&category/airing/&category/variety/&category/documentary/",
     play_parse: true,
-    lazy: `js:
-            let html = request(input);
-            let hconf = html.match(/r player_.*?=(.*?)</)[1];
-            let json = JSON5.parse(hconf);
-            let url = json.url;
-            if (json.encrypt == '1') {
-                url = unescape(url);
-            } else if (json.encrypt == '2') {
-                url = unescape(base64Decode(url));
-            }
-            if (/\\.(m3u8|mp4|m4a|mp3)/.test(url)) {
-                input = {
-                    parse: 0,
-                    jx: 0,
-                    url: url,
-                };
-            } else {
-                input;
-            }`,
+    lazy: `
+    
+    if (/(pan.quark.cn|www.aliyundrive.com|www.alipan.com)/.test(input)){
+ input= getProxyUrl().replace('js',"quark")+'&type=push&url='+encodeURIComponent(input);
+}else if(/¥/.test(input)){
+    
+    var jxurl= base64Decode(input.split("¥")[0]);
+    
+    
+    var url=JSON.parse(fetch(jxurl,{headers:{"Referer": input.split("¥")[1]}})).url
+    
+            
+        input = {
+               url: url,
+               parse: 0,
+               jx:0
+           }
+    
+     
+         
+     }else if(/mp4/.test(input)){
+        input = {
+             url: input,
+             parse:0,
+             jx:0,
+             header:{
+                 "Referer":"https://ddys.pro/"
+             }
+         }
+         
+     }`,
     limit: 6,
-    推荐: '.module-item;li;a&&title;.lazyload&&data-original;.module-item-desc&&Text;a&&href',
-    double: true, // 推荐内容是否双层定位
-    一级: '.module-item li;a&&title;a&&data-original;.module-item-desc&&Text;a&&href',
+    推荐: '.tab-list.active;a.module-poster-item.module-item;.module-poster-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+    double: true,
+    一级: `js: var M=input.split("$");
+var page= M[1];
+if(page==1){
+var html=fetch(M[0])
+}else{
+html=fetch(M[0]+"page/"+page+"/")
+}
+var list= pdfa(html,"body&&article");
+var d=[];
+for(var it of list){
+d.push({
+title: pdfh(it,"h2&&Text"),
+desc: pdfh(it,".post-box-meta&&Text"),
+img: pdfh(it,".post-box-image&&style"),
+url: pdfh(it,"h2&&a&&href")
+})
+}
+setResult(d)
+`,
+
     二级: {
         "title": "h1&&Text",
-        "img": ".poster-img .lazyload&&data-original",
-        "desc": ".info-item:eq(0)&&Text;.info-item:eq(1)&&Text;.info-item:eq(2)&&Text;.info-item:eq(3)&&Text;.info-item:eq(4)&&Text",
-        "content": ".intro-content&&Text",
-        "tabs": ".play-tabs li",
-        "lists": ".play-list:eq(#id) li"
+        //图片
+
+        //主要描述;年份;地区;演员;导演
+        "desc": "",
+        //简介
+        "content": "",
+        //线路数组  
+
+
+        "tabs": `js:
+       pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+             TABS = ['主'];
+             if (pdfa(html, '.page-links&&a').length > 0) {
+             	TABS =['第1季']
+            var as = pdfa(html, '.page-links&&a')
+            var k=2
+            for(var i in as){
+                                           
+                                           
+                 TABS.push("第"+k+"季")   
+                 k++                       
+                                        
+        }
+            
+            }
+            if (/夸克网盘|视频下载/.test(html)) {
+            TABS.push('夸克网盘');
+            
+        }
+           `,
+
+
+
+        lists: $js.toString(() => {
+            pdfh = jsp.pdfh;
+            pdfa = jsp.pdfa;
+            pd = jsp.pd;
+
+            var json = JSON.parse(pdfh(html, ".wp-playlist-script&&Html"))
+            var plays = json["tracks"].map(x => {
+
+                var url = 'https://v.ddys.pro' + x.src0;
+                var f = false;
+                /*if (x.src1 != "") {
+                    var r = x.srctype == '1' ? '2' : '3';
+
+                    url = base64Encode('https://ddys.pro/getvddr' + r + '/video?id=' + x.src1 + '&type=json') + "¥" + input;
+                    f = true;
+                }*/
+                return x.caption + "$" + url
+
+            })
+
+
+
+            LISTS = [];
+            LISTS.push(plays);
+            if (pdfa(html, '.page-links&&a')
+                .length > 0) {
+
+                var as = pdfa(html, '.page-links&&a')
+                    .map(h => {
+                        return pdfh(h, 'a&&href')
+
+                    });
+
+                for (var i in as) {
+
+                    var temp = [];
+                    var htm = fetch(as[i]);
+                    json = JSON.parse(pdfh(htm, ".wp-playlist-script&&Html"))
+                    plays = json["tracks"].map(x => {
+
+                        var url = 'https://v.ddys.pro' + x.src0;
+                        var f = false;
+                        if (x.src1 != "") {
+                            var r = x.srctype == '1' ? '2' : '3';
+
+                            url = base64Encode('https://ddys.pro/getvddr' + r + '/video?id=' + x.src1 + '&type=json') + "¥" + input;
+                            f = true;
+                        }
+                        return x.caption + "$" + url
+
+                    })
+
+                    LISTS.push(plays)
+
+                }
+            }
+
+            if (/夸克网盘|视频下载/.test(html)) {
+
+                var b = [];
+                var quark = pdfa(html, 'body&&a[href*="pan.quark.cn"]');
+                var uc = pdfa(html, 'body&&a[href*="drive.uc"]');
+                log(quark)
+                if (quark.length >= 1) {
+                    b.push("夸克网盘$" + pdfh(quark[0], "a&&href"))
+                }
+                if (uc.length >= 1) {
+                    b.push("UC网盘$" + pdfh(uc[0], "a&&href"))
+                }
+
+
+
+
+                LISTS.push(b);
+            }
+
+        })
     },
-    搜索: '.module-item;a&&title;.lazyload&&data-original;.module-item-desc&&Text;a&&href;.info&&p:eq(0) p&&Text',
-    // 分类映射
-    电影: 'https://qqqys.com/index.php?m=vod-type-id-1.html',
-    剧集: 'https://qqqys.com/index.php?m=vod-type-id-2.html',
-    动漫: 'https://qqqys.com/index.php?m=vod-type-id-3.html',
-    综艺: 'https://qqqys.com/index.php?m=vod-type-id-4.html',
-    短剧: 'https://qqqys.com/index.php?m=vod-type-id-5.html'
+
+    搜索: '#main&&article;h2&&Text;;.cat-links&&a&&Text;a&&href'
 }
